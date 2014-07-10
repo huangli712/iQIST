8caeecaca3a28f77fd389990e73a2f67f9aa912c
lihuang <li.huang@unifr.ch>
2014-07-10 12:48:09 +0200

    try to implement s_error.f90, not finished

--- src/common/s_error.f90
+++ src/common/s_error.f90
@@ -8,19 +8,75 @@
 !!! author  : li huang (email:huangli712@gmail.com)
 !!! history : 09/15/2009 by li huang
 !!!           07/08/2014 by li huang
+!!!           07/10/2014 by li huang
 !!! purpose :
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
 
-!!>>>
-  subroutine s_print_error()
+!!>>> print the error information and STOP the program
+  subroutine s_print_error(sub, msg)
+     use constants
+
+     implicit none
+
+! external arguments
+! subroutine name
+     character(len=*), intent(in) :: sub
+
+! error message
+     character(len=*), intent(in) :: msg
+
+! print error information
+     write(mystd,'(2X,4a)') 'fatal error occurred in ', sub, ': ', msg
+
+! TERMINATE THE PROGRAM
+!-------------------------------------------------------------------------
+     STOP
+!-------------------------------------------------------------------------
+
+     return
   end subroutine s_print_error
 
-!!>>>
-  subroutine s_print_exception()
+!!>>> print normal runtime exceptional information, and continue
+  subroutine s_print_exception(sub, msg)
+     use constants
+
+     implicit none
+
+! external arguments
+! subroutine name
+     character(len=*), intent(in) :: sub
+
+! exception message
+     character(len=*), intent(in) :: msg
+
+! print error information
+     write(mystd,'(2X,4a)') 'runtime exception occurred in ', sub, ': ', msg
+
+! CONTINUE/PAUSE THE PROGRAM
+!-------------------------------------------------------------------------
+     CONTINUE ! OR PAUSE
+!-------------------------------------------------------------------------
+
+     return
   end subroutine s_print_exception
 
-!!>>>
-  subroutine s_print_message()
+!!>>> print normal runtime message to the console
+  subroutine s_print_message(sub, msg)
+     use constants
+
+     implicit none
+
+! external arguments
+! subroutine name
+     character(len=*), intent(in) :: sub
+
+! runtime message
+     character(len=*), intent(in) :: msg
+
+! print error information
+     write(mystd,'(2X,4a)') 'instant message from ', sub, ': ', msg
+
+     return
   end subroutine s_print_message
