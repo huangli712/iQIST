1c62139e794f652b11977f1978fd5ce1ca69d1e4
lihuang <li.huang@unifr.ch>
2014-07-10 17:28:31 +0200

    try to implement s_util.f90

--- src/common/s_util.f90
+++ src/common/s_util.f90
@@ -25,3 +25,19 @@
 
      return
   end subroutine s_assert
+
+pure function count_chars(str,chars) result(n)
+  character(len=*), intent(in) :: str, chars
+  integer :: n
+  integer :: pos,inc
+  n = 0
+  pos = 0
+  do
+     inc = scan(str(pos + 1:),chars)
+     if (inc == 0) exit
+     pos = pos + inc
+     n = n + 1
+  end do
+end function count_chars
+
+
