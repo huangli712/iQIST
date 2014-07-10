b34de9296649c57579979898405fb757f747fa84
lihuang <li.huang@unifr.ch>
2014-07-10 12:55:35 +0200

    modify s_util.f90, try to implement it

--- src/common/s_util.f90
+++ src/common/s_util.f90
@@ -1,4 +1,12 @@
-
-
-   subroutine util()
-   end subroutine util
+!!!-----------------------------------------------------------------------
+!!! project : CSSL (Common Service Subroutines Library)
+!!! program :
+!!! source  : s_util.f90
+!!! type    : subroutines
+!!! author  : li huang (email:huangli712@gmail.com)
+!!! history : 07/10/2014 by li huang
+!!!           07/10/2014 by li huang
+!!! purpose : these subroutines are used to
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
