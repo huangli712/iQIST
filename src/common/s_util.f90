e3166e958e6ca8650fda155cb838a64a3bf19449
lihuang <li.huang@unifr.ch>
2014-07-10 12:59:10 +0200

    implement s_assert() in s_util.f90

--- src/common/s_util.f90
+++ src/common/s_util.f90
@@ -1,6 +1,6 @@
 !!!-----------------------------------------------------------------------
 !!! project : CSSL (Common Service Subroutines Library)
-!!! program :
+!!! program : s_assert
 !!! source  : s_util.f90
 !!! type    : subroutines
 !!! author  : li huang (email:huangli712@gmail.com)
@@ -10,3 +10,18 @@
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
+
+!!>>> s_assert: fortran version of assert
+  subroutine s_assert(condition)
+     implicit none
+
+! external arguments
+     logical, intent(in) :: condition
+
+! if condition == .false., it aborts the program.
+     if ( .not. condition ) then
+         call s_print_error("s_assert", "assert failed.")
+     endif
+
+     return
+  end subroutine s_assert
