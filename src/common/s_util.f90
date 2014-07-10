ca76123d5f1587e4b72f06d202fa5a16171b4097
lihuang <li.huang@unifr.ch>
2014-07-10 17:36:54 +0200

    try to implement s_util.f90, not finished

--- src/common/s_util.f90
+++ src/common/s_util.f90
@@ -40,9 +40,37 @@ pure function count_chars(str,chars) result(n)
   end do
 end function count_chars
 
+! string_count_substring --
+!     Return the number of times a substring occurs
+! Arguments:
+!     string     String to be examined
+!     substr     Substring in question
+! Result:
+!     Number of occurrences
+! Note:
+!     Trailing blanks _are_ taken into account.
+!     Possible overlaps are ignored:
+!     string = 'ababab' and substr = 'abab'
+!     will give the answer 1, not 2
+!
+function string_count_substring( string, substr ) result( count )
+    character(len=*)           :: string
+    character(len=*)           :: substr
+
+    integer                    :: start
+    integer                    :: count
+
+    count  = 0
+    start  = 0
+    do
+        start = index( string(start+1:), substr )
+        count = count + 1
+        if ( start == 0 ) exit
+    enddo
+end function string_count_substring
 
 function real2str(num) result(str)
-  integer, parameter, private :: max_real_len = 16
+  integer, parameter :: max_real_len = 16
   real, intent(in) :: num
   character(len=:) ,allocatable :: str
   character(len=max_real_len) :: base_str
@@ -51,7 +79,7 @@ function real2str(num) result(str)
 end function real2str
 
 function dble2str(num) result(str)
-  integer, parameter, private :: max_dble_len = 28
+  integer, parameter :: max_dble_len = 28
   double precision, intent(in) :: num
   character(len=:) ,allocatable :: str
   character(len=max_dble_len) :: base_str
@@ -60,10 +88,38 @@ function dble2str(num) result(str)
 end function dble2str
 
 function int2str(num) result(str)
-  integer, parameter, private :: max_int_len = 12
+  integer, parameter :: max_int_len = 12
   integer, intent(in) :: num
   character(len=:) ,allocatable :: str
   character(len=max_int_len) :: base_str
   write(base_str,*) num
   str = trim(adjustl(base_str))
 end function int2str
+
+function upcase(s) result(t)
+! Returns string 's' in uppercase
+character(*), intent(in) :: s
+character(len(s)) :: t
+integer :: i, diff
+t = s; diff = ichar('A')-ichar('a')
+do i = 1, len(t)
+    if (ichar(t(i:i)) >= ichar('a') .and. ichar(t(i:i)) <= ichar('z')) then
+        ! if lowercase, make uppercase
+        t(i:i) = char(ichar(t(i:i)) + diff)
+    end if
+end do
+end function
+
+function lowcase(s) result(t)
+! Returns string 's' in lowercase
+character(*), intent(in) :: s
+character(len(s)) :: t
+integer :: i, diff
+t = s; diff = ichar('A')-ichar('a')
+do i = 1, len(t)
+    if (ichar(t(i:i)) >= ichar('A') .and. ichar(t(i:i)) <= ichar('Z')) then
+        ! if uppercase, make lowercase
+        t(i:i) = char(ichar(t(i:i)) - diff)
+    end if
+end do
+end function
