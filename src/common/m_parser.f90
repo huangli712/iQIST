2fcc8fb5a6a290296f7913dad0797f31134685f6
lihuang <li.huang@unifr.ch>
2014-07-10 12:41:24 +0200

    modify m_parser.f90, add header comment

--- src/common/m_parser.f90
+++ src/common/m_parser.f90
@@ -1,3 +1,18 @@
+!!!-----------------------------------------------------------------------
+!!! project : CSML (Common Service Modules Library)
+!!! program : parser
+!!! source  : m_parser.f90
+!!! type    : module
+!!! author  : li huang (email:huangli712@gmail.com)
+!!! history : 07/10/2014 by li huang
+!!! purpose : this purpose of this module is to implement a generic and
+!!!           flexible config/input file reader and analyzer.
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
 
-   module parser
-   end module parser
+  module parser
+     implicit none
+
+  contains
+  end module parser
