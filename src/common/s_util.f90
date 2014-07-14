e1ae2b021fcedd5c6ff09f33bfd74f756c21d81b
lihuang <li.huang@unifr.ch>
2014-07-14 11:43:47 +0200

    modify s_util.f90, add s_str_compress(), try to implement it

--- src/common/s_util.f90
+++ src/common/s_util.f90
@@ -11,6 +11,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:huangli712@gmail.com)
 !!! history : 07/10/2014 by li huang
+!!!           07/14/2014 by li huang
 !!! purpose : these subroutines are used to
 !!! status  : unstable
 !!! comment :
