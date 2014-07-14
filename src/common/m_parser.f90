2605968942f75ab7db13ae59131425af1963ea42
lihuang <li.huang@unifr.ch>
2014-07-14 11:58:27 +0200

    try to implement m_parser.f90, not implemented

--- src/common/m_parser.f90
+++ src/common/m_parser.f90
@@ -1,6 +1,13 @@
 !!!-----------------------------------------------------------------------
 !!! project : CSML (Common Service Modules Library)
 !!! program : parser
+!!!           p_create
+!!!           p_destroy
+!!!           p_parse
+!!!           p_get_integer
+!!!           p_get_real
+!!!           p_get_logical
+!!!           p_get_character
 !!! source  : m_parser.f90
 !!! type    : module
 !!! author  : li huang (email:huangli712@gmail.com)
@@ -18,4 +25,26 @@
      implicit none
 
   contains
+
+  subroutine p_create()
+  end subroutine p_create
+
+  subroutine p_destroy()
+  end subroutine p_destroy
+
+  subroutine p_parse()
+  end subroutine p_parse
+
+  subroutine p_get_integer()
+  end subroutine p_get_integer
+
+  subroutine p_get_real()
+  end subroutine p_get_real
+
+  subroutine p_get_logical()
+  end subroutine p_get_logical
+
+  subroutine p_get_character()
+  subroutine p_get_character()
+
   end module parser
