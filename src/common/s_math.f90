58f2fe1c5a40eb501cd5b2bc9e90a8d661220cbf
lihuang <li.huang@unifr.ch>
2014-07-24 17:39:26 +0200

    modify s_math.f90, fix the header comment

--- src/common/s_math.f90
+++ src/common/s_math.f90
@@ -7,7 +7,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:huangli712@gmail.com)
 !!! history : 07/10/2014 by li huang
-!!! history : 07/24/2014 by li huang
+!!!           07/24/2014 by li huang
 !!! purpose : these subroutines are used to
 !!! status  : unstable
 !!! comment :
