ad767f2d8f5deca5054af750411e5de04f5515bc
Yilin Wang <qhwyl2006@126.com>
2014-08-04 08:43:34 +0800

    improve skip lists algorithm

--- src/ctqmc/manjushaka/ctqmc_skiplists.f90
+++ src/ctqmc/manjushaka/ctqmc_skiplists.f90
@@ -21,7 +21,7 @@
          integer :: indx(2)
          type(t_pnode), dimension(:,:), pointer     :: forward => null()
          logical, dimension(:,:,:), pointer         :: lsave   => null() 
-         logical, dimension(:,:), pointer           :: lcopy => null()
+         integer, dimension(:,:), pointer           :: lcopy => null()
          type(t_subprod), dimension(:,:), pointer   :: subprod => null()
      end type t_node
 
@@ -34,9 +34,6 @@
      type :: t_subprod
          real(dp), dimension(:,:,:), pointer :: submat => null()
      end type t_subprod
-
-!>>> a very large integer
-     integer, private, parameter :: max_int = 100000000
 
 !>>> global variables, we use two skip lists
      type(t_skiplists), public, save, pointer :: skip_lists
@@ -67,11 +64,9 @@
             head%forward(i,1)%p => tail
             head%forward(i,2)%p => tail
         enddo
-        head%lsave = .false. 
-        head%lcopy = .false.
 
 ! set tail node
-        tail%indx = max_int
+        tail%indx = 1
         tail%forward(1,1)%p => null()
         tail%forward(1,2)%p => null()
  
@@ -127,7 +122,7 @@
                 enddo
             enddo
             node%lsave = .false.
-            node%lcopy = .false.
+            node%lcopy = 2
         endif
      
         return
@@ -229,7 +224,7 @@
         enddo
 
         x%p => node%forward(1,1)%p
-        do while( .not. associated(x%p, skiplists%tail) )
+        do while( associated(x%p) )
             x%p%indx(1) = x%p%indx(1) + 1
             x%p => x%p%forward(1,1)%p
         enddo
@@ -263,7 +258,7 @@
         node => x%p%forward(1,1)%p
 
         x%p => node%forward(1,1)%p
-        do while(.not. associated(x%p, skiplists%tail))
+        do while( associated(x%p) )
             x%p%indx(1) = x%p%indx(1) - 1
             x%p => x%p%forward(1,1)%p
         enddo
@@ -342,14 +337,14 @@
             skiplists%list_level(2) = skiplists%list_level(1)
             skiplists%num_node(2)   = skiplists%num_node(1)
             x%p => skiplists%head
-            do while(.not. associated(x%p, skiplists%tail))
+            do while( associated(x%p) )
                 x%p%node_level(2) = x%p%node_level(1)
                 x%p%indx(2) = x%p%indx(1)
                 x%p%forward(:,2) = x%p%forward(:,1)
                 x%p%lsave(:,:,2) = x%p%lsave(:,:,1)
                 do i=1, x%p%node_level(1)
                     do j=1, nsectors
-                        if (x%p%lcopy(j,i)) then
+                        if (x%p%lcopy(j,i) == 1) then
                             x%p%subprod(j,i)%submat(:,:,2) = x%p%subprod(j,i)%submat(:,:,1)
                         endif                 
                     enddo
@@ -360,12 +355,12 @@
             skiplists%list_level(1) = skiplists%list_level(2)
             skiplists%num_node(1)   = skiplists%num_node(2)
             x%p => skiplists%head
-            do while(.not. associated(x%p, skiplists%tail))
+            do while( associated(x%p) )
                 x%p%node_level(1) = x%p%node_level(2)
                 x%p%indx(1) = x%p%indx(2)
                 x%p%forward(:,1) = x%p%forward(:,2)
                 x%p%lsave(:,:,1) = x%p%lsave(:,:,2)
-                x%p%lcopy(:,:) = .false.
+                x%p%lcopy(:,:) = 2
                 x%p => x%p%forward(1,1)%p
             enddo
         endif
@@ -395,7 +390,7 @@
             endif
             call random_level(level)
             call insert_node(skiplists, level, ib-1, node_b) 
-            if (ib < csize) then
+            if (ib < csize .and. ib /= ia .and. ib /= ia+1 ) then
                 call change_one_node(skiplists,ib+1)
             endif
 ! remove two operators
@@ -405,7 +400,7 @@
                 call change_one_node(skiplists,ia)
             endif
             call remove_node(skiplists, ib, node_b) 
-            if (ib < csize+1) then
+            if (ib < csize+1 .and. ib /= ia-1) then
                 call change_one_node(skiplists,ib)
             endif
 ! remove one, then insert one
@@ -416,7 +411,7 @@
             endif
             call random_level(level)
             call insert_node(skiplists, level, ib-1, node_b) 
-            if (ib < csize) then
+            if (ib < csize .and. ib /= ia) then
                 call change_one_node(skiplists,ib+1)
             endif
 ! change all the node
@@ -459,7 +454,7 @@
      
         type(t_skiplists), pointer :: skiplists
         integer, intent(in) :: csize
-        integer, intent(in) :: string(csize+1)
+        integer, intent(in) :: string(0:csize+1)
         integer, intent(in) :: index_t_loc(mkink)
         real(dp), intent(in) :: expt_t_loc(ncfgs)
         real(dp), intent(out) :: trace
@@ -472,23 +467,9 @@
         integer :: indx
         integer :: dim1
         integer :: i,j
-     
-        level = skiplists%list_level(1)
-        head => skiplists%head
-        tail => skiplists%tail
-     
+
         isect = string(1)
         dim1 = sectors(isect)%ndim
-
-! if we don't save its result, allocate memory and calculate it
-        !if (.not. head%lsave(isect, level, 1)) then
-        !    call cat_subprod(skiplists, level, head, tail, csize, string, index_t_loc, right_mat, head%subprod(isect,level)%submat(:,:,1))
-        !    head%lsave(isect, level, 1) = .true.
-        !    head%lcopy(isect, level) = .true.
-        !    right_mat = head%subprod(isect, level)%submat(:,:,1)
-        !else
-        !    right_mat = head%subprod(isect, level)%submat(:,:,2)
-        !endif
 
         call cat_subprod2(skiplists, csize, string, index_t_loc, right_mat)
 
@@ -512,180 +493,17 @@
 
         return
      end subroutine ctqmc_sector_ztrace
-     
-     recursive &
-     subroutine cat_subprod(skiplists, level, head, tail, csize, string, index_t_loc, tmp_mat, total_prod)
-        implicit none
-     
-        type(t_skiplists), pointer :: skiplists
-        integer, intent(in)   :: level
-        type(t_node), pointer :: head
-        type(t_node), pointer :: tail
-        integer, intent(in)   :: csize
-        integer, intent(in)   :: string(csize+1)
-        integer, intent(in)   :: index_t_loc(mkink)
-        real(dp), intent(inout) :: total_prod(max_dim_sect, max_dim_sect)
-        real(dp), intent(out) :: tmp_mat(max_dim_sect, max_dim_sect)
-
-        type(t_pnode) :: x1, x2
-        integer :: this_level
-        integer :: counter
-        integer :: isect, jsect
-        integer :: dim1, dim2, dim3
-        integer :: istart
-        integer :: vt, vf
-        integer :: i,j
-     
-        this_level = level - 1
-        total_prod = zero
-
-        if (head%indx(1) == 0) then
-            dim1 = sectors(string(1))%ndim
-        else
-            dim1 = sectors(string(head%indx(1)))%ndim
-        endif
- 
-        counter = 0 
-        x1%p => head
-        x2%p => head%forward(this_level,1)%p
-
-        if (this_level == 1) then
-            do while(.not. associated(x1%p, tail))
-                counter = counter + 1
-                if (x1%p%indx(1) == 0) then
-                    isect = string(1) 
-                    jsect = string(1) 
-                else
-                    isect = string(x1%p%indx(1))
-                    jsect = string(x1%p%indx(1)+1)
-                endif
-
-                dim2 = sectors(isect)%ndim
-                dim3 = sectors(jsect)%ndim
-
-                if (.not. x1%p%lsave(isect,1,1)) then
-                    x1%p%subprod(isect,1)%submat(:,:,1) = zero
-                    if (x1%p%indx(1) == 0) then
-                        do i=1, dim2
-                            x1%p%subprod(isect,1)%submat(i,i,1) = one 
-                        enddo
-                    else
-                        istart = sectors(isect)%istart
-                        vt = type_v( index_t_loc(x1%p%indx(1)) )
-                        vf = flvr_v( index_t_loc(x1%p%indx(1)) ) 
-                        do i=1, dim2
-                            do j=1, dim3
-                                x1%p%subprod(isect,1)%submat(j,i,1) = sectors(isect)%myfmat(vf,vt)%item(j,i) * &
-                                expt_v( istart+i-1, index_t_loc(x1%p%indx(1)) )
-                            enddo 
-                        enddo
-                        num_prod = num_prod + 1
-                    endif
-                    x1%p%lsave(isect,1,1) = .true.
-                    x1%p%lcopy(isect,1) = .true.
-
-                    if (counter == 1) then
-                        total_prod = x1%p%subprod(isect,1)%submat(:,:,1)
-                    else
-                        call dgemm( 'N','N', dim3, dim1, dim2, one,                    &
-                                    x1%p%subprod(isect,1)%submat(:,:,1), max_dim_sect, &
-                                    total_prod,                          max_dim_sect, &
-                                    zero, tmp_mat,                       max_dim_sect   )
-                        total_prod = tmp_mat
-                        num_prod = num_prod + 1
-                    endif
-
-                else
-                    if (counter == 1) then
-                        total_prod = x1%p%subprod(isect,1)%submat(:,:,2)
-                    else
-                        call dgemm( 'N','N', dim3, dim1, dim2, one,                  &
-                                    x1%p%subprod(isect,1)%submat(:,:,2), max_dim_sect, &
-                                    total_prod,                        max_dim_sect, &
-                                    zero, tmp_mat,                     max_dim_sect   )
-                        total_prod = tmp_mat
-                        num_prod = num_prod + 1
-                    endif
-                endif
-
-                x1%p => x1%p%forward(1,1)%p
-            enddo
-
-        else
-            do while(.not. associated(x1%p, tail) )
-                counter =  counter + 1
-                if (x1%p%indx(1) == 0) then
-                    isect = string(1) 
-                    if (associated(x2%p, skiplists%tail)) then
-                        jsect = string(1)
-                    else
-                        jsect = string(x2%p%indx(1)) 
-                    endif
-                else
-                    isect = string(x1%p%indx(1))
-                    if (associated(x2%p, skiplists%tail)) then
-                        jsect = string(1)
-                    else
-                        jsect = string(x2%p%indx(1)) 
-                    endif
-                endif
-
-                dim2 = sectors(isect)%ndim
-                dim3 = sectors(jsect)%ndim
-
-                if ( .not. x1%p%lsave(isect,this_level,1) ) then
-                    call cat_subprod(skiplists,this_level, x1%p, x2%p, csize, string, &
-                              index_t_loc, tmp_mat, x1%p%subprod(isect,this_level)%submat(:,:,1))
-                    x1%p%lsave(isect, this_level,1) = .true.
-                    x1%p%lcopy(isect, this_level) = .true.
-
-                    if (counter == 1) then
-                        total_prod = x1%p%subprod(isect,this_level)%submat(:,:,1)
-                    else
-                        call dgemm( 'N','N', dim3, dim1, dim2, one,                             &
-                                    x1%p%subprod(isect,this_level)%submat(:,:,1), max_dim_sect, &
-                                    total_prod,                                   max_dim_sect, &
-                                    zero, tmp_mat,                                max_dim_sect   )
-                        total_prod = tmp_mat
-                        num_prod = num_prod + 1
-                    endif
-
-                else
-
-                    if (counter == 1) then
-                        total_prod = x1%p%subprod(isect,this_level)%submat(:,:,2)
-                    else
-                        call dgemm( 'N','N', dim3, dim1, dim2, one,                             &
-                                    x1%p%subprod(isect,this_level)%submat(:,:,2), max_dim_sect, &
-                                    total_prod,                                   max_dim_sect, &
-                                    zero, tmp_mat,                                max_dim_sect   )
-                        total_prod = tmp_mat
-                        num_prod = num_prod + 1
-                    endif
-
-                endif
-
-                if (associated(x2%p, tail)) EXIT
-
-                x1%p => x2%p
-                x2%p => x2%p%forward(this_level,1)%p
-
-            enddo  
-        endif
-     
-        return
-     end subroutine cat_subprod
 
      subroutine cat_subprod2(skiplists, csize, string, index_t_loc, total_prod)
         type(t_skiplists), pointer :: skiplists
         integer, intent(in)   :: csize
-        integer, intent(in)   :: string(csize+1)
+        integer, intent(in)   :: string(0:csize+1)
         integer, intent(in)   :: index_t_loc(mkink)
         real(dp), intent(inout) :: total_prod(max_dim_sect, max_dim_sect)
 
         real(dp) :: tmp_mat(max_dim_sect, max_dim_sect)
         type(t_pnode) :: x1, x2, x3, x4
-        integer :: this_level
+        integer :: icopy
         integer :: counter
         integer :: isect, jsect
         integer :: msect, nsect
@@ -698,15 +516,8 @@
         dim1 = sectors(string(1))%ndim
         x1%p => skiplists%head
         do while(.not. associated(x1%p, skiplists%tail))
-            
-            if (x1%p%indx(1) == 0) then
-                isect = string(1) 
-                jsect = string(1) 
-            else
-                isect = string(x1%p%indx(1))
-                jsect = string(x1%p%indx(1)+1)
-            endif
-
+            isect = string(x1%p%indx(1))
+            jsect = string(x1%p%indx(1)+1)
             dim2 = sectors(isect)%ndim
             dim3 = sectors(jsect)%ndim
 
@@ -729,8 +540,9 @@
                     num_prod = num_prod + 1
                 endif
                 x1%p%lsave(isect,1,1) = .true.
-                x1%p%lcopy(isect,1) = .true.
+                x1%p%lcopy(isect,1) = 1
             endif 
+
             x1%p => x1%p%forward(1,1)%p
         enddo
  
@@ -738,22 +550,8 @@
             x1%p => skiplists%head
             x2%p => skiplists%head%forward(i,1)%p 
             do while(.not. associated(x1%p, skiplists%tail))
-                if (x1%p%indx(1) == 0) then
-                    isect = string(1) 
-                    if (associated(x2%p, skiplists%tail)) then
-                        jsect = string(1)
-                    else
-                        jsect = string(x2%p%indx(1)) 
-                    endif
-                else
-                    isect = string(x1%p%indx(1))
-                    if (associated(x2%p, skiplists%tail)) then
-                        jsect = string(1)
-                    else
-                        jsect = string(x2%p%indx(1)) 
-                    endif
-                endif
-
+                isect = string(x1%p%indx(1))
+                jsect = string(x2%p%indx(1)) 
                 dim2 = sectors(isect)%ndim
                 dim3 = sectors(jsect)%ndim
 
@@ -764,44 +562,20 @@
                     dim4 = dim2 
                     do while(.not. associated(x3%p, x2%p)) 
                         counter = counter + 1
-                        if (x3%p%indx(1) == 0) then
-                            msect = string(1) 
-                            if (associated(x4%p, skiplists%tail)) then
-                                nsect = string(1)
-                            else
-                                nsect = string(x4%p%indx(1)) 
-                            endif
-                        else
-                            msect = string(x3%p%indx(1))
-                            if (associated(x4%p, skiplists%tail)) then
-                                nsect = string(1)
-                            else
-                                nsect = string(x4%p%indx(1)) 
-                            endif
-                        endif
-
+                        msect = string(x3%p%indx(1))
+                        nsect = string(x4%p%indx(1)) 
                         dim5 = sectors(msect)%ndim
                         dim6 = sectors(nsect)%ndim
+                        icopy = x3%p%lcopy(msect,i-1)
 
                         if (counter == 1) then
-                            if (x3%p%lcopy(msect, i-1)) then
-                                x1%p%subprod(isect,i)%submat(:,:,1) = x3%p%subprod(msect,i-1)%submat(:,:,1)
-                            else
-                                x1%p%subprod(isect,i)%submat(:,:,1) = x3%p%subprod(msect,i-1)%submat(:,:,2)
-                            endif
+                            x1%p%subprod(isect,i)%submat(:,:,1) = x3%p%subprod(msect,i-1)%submat(:,:,icopy)
                         else
-                            if (x3%p%lcopy(msect, i-1)) then
-                                call dgemm( 'N','N', dim6, dim4, dim5, one,                        &
-                                            x3%p%subprod(msect,i-1)%submat(:,:,1),   max_dim_sect, &
-                                            x1%p%subprod(isect,i)%submat(:,:,1),     max_dim_sect, &
-                                            zero, tmp_mat,                           max_dim_sect   )
-                            else
-                                call dgemm( 'N','N', dim6, dim4, dim5, one,                        &
-                                            x3%p%subprod(msect,i-1)%submat(:,:,2),   max_dim_sect, &
-                                            x1%p%subprod(isect,i)%submat(:,:,1),     max_dim_sect, &
-                                            zero, tmp_mat,                           max_dim_sect   )
+                            call dgemm( 'N','N', dim6, dim4, dim5, one,                          &
+                                        x3%p%subprod(msect,i-1)%submat(:,:,icopy), max_dim_sect, &
+                                        x1%p%subprod(isect,i)%submat(:,:,1),       max_dim_sect, &
+                                        zero, tmp_mat,                             max_dim_sect   )
 
-                            endif
                             x1%p%subprod(isect,i)%submat(:,:,1) = tmp_mat
                             num_prod = num_prod + 1
                         endif
@@ -812,7 +586,7 @@
                     enddo
 
                     x1%p%lsave(isect,i,1) = .true.
-                    x1%p%lcopy(isect,i) = .true.
+                    x1%p%lcopy(isect,i) = 1
 
                 endif
 
