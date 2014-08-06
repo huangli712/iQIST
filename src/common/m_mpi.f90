12b3ac15099efe3683ad87e7c18d979cf2f8be4c
lihuang <li.huang@unifr.ch>
2014-08-06 16:57:54 +0200

    modify m_mpi.f90, improve the comments

--- src/common/m_mpi.f90
+++ src/common/m_mpi.f90
@@ -11,29 +11,7 @@
 !!!           code. we note that the original mpi interfaces/subroutines 
 !!!           are rather complicated for newbies, thus we try to wrap the
 !!!           most important and useful (not all) mpi subroutines (in our
-!!!           opinion) in this module to facilite the usage of mpi. these
-!!!           subroutines are
-!!!               MPI_INIT(),
-!!!               MPI_FINALIZE(),
-!!!               MPI_WTIME(),
-!!!               MPI_WTICK(),
-!!!               MPI_BARRIER(),
-!!!               MPI_DIMS_CREATE(),
-!!!               MPI_CART_CREATE(),
-!!!               MPI_CART_COORDS(),
-!!!               MPI_COMM_SPLIT(),
-!!!               MPI_COMM_RANK(),
-!!!               MPI_COMM_SIZE(),
-!!!               MPI_GET_PROCESSOR_NAME(),
-!!!               MPI_BCAST(),
-!!!               MPI_GATHER(),
-!!!               MPI_GATHERV(),
-!!!               MPI_ALLGATHER(),
-!!!               MPI_ALLGATHERV(),
-!!!               MPI_REDUCE(),
-!!!               MPI_ALLREDUCE(),
-!!!           etc. in the module, we also try to implement a light-weight
-!!!           error handler. enjoy it!
+!!!           opinion) in this module to facilite the usage of mpi. 
 !!! status  : unstable
 !!! comment : this module has been tested under the following environment:
 !!!               mpich1    1.2.7p1
@@ -49,8 +27,28 @@
 !!
 !! This module wraps the most useful mpi calls by using generic programming
 !! techniques. It supports most of the collective operations (such as BCAST,
-!! GATHER, REDUCE, etc.). However, none of the point-to-point operations is
-!! supported.
+!! GATHER, REDUCE, etc.). These subroutines are
+!!     MPI_INIT(),
+!!     MPI_FINALIZE(),
+!!     MPI_WTIME(),
+!!     MPI_WTICK(),
+!!     MPI_BARRIER(),
+!!     MPI_DIMS_CREATE(),
+!!     MPI_CART_CREATE(),
+!!     MPI_CART_COORDS(),
+!!     MPI_COMM_SPLIT(),
+!!     MPI_COMM_RANK(),
+!!     MPI_COMM_SIZE(),
+!!     MPI_GET_PROCESSOR_NAME(),
+!!     MPI_BCAST(),
+!!     MPI_GATHER(),
+!!     MPI_GATHERV(),
+!!     MPI_ALLGATHER(),
+!!     MPI_ALLGATHERV(),
+!!     MPI_REDUCE(),
+!!     MPI_ALLREDUCE(),
+!! etc. However, none of the point-to-point operations is supported. in the
+!! module, we also try to implement a light-weight error handler. enjoy it!
 !!
 !! Usage
 !! =====
@@ -107,7 +105,7 @@
 !!========================================================================
 
 ! dp: number precision, double precision for reals
-     integer, private, parameter :: dp = kind(1.0d0)
+     integer, private, parameter :: dp    = kind(1.0d0)
 
 ! mystd: device descriptor, console output
      integer, private, parameter :: mystd = 6
