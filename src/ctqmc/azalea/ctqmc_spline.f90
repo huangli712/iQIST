15ba6426c6e98b1dc41bec32c4dc8062e3438db2
lihuang <li.huang@unifr.ch>
2014-08-07 19:06:18 +0200

    replace ctqmc_make_splint() and ctqmc_make_spline() with CSSL subroutines

--- src/ctqmc/azalea/ctqmc_spline.f90
+++ src/ctqmc/azalea/ctqmc_spline.f90
@@ -39,13 +39,13 @@
 
 ! external functions
 ! internal interpolation engine
-     real(dp), external :: ctqmc_make_splint
+     procedure(real(dp)) :: s_spl_splint
 
 ! local variables
 ! return value
      real(dp) :: val
 
-     val = ctqmc_make_splint(ntime, tmesh, htau(:, flvr, flvr), hsed(:, flvr, flvr), dtau)
+     val = s_spl_splint(ntime, tmesh, htau(:, flvr, flvr), hsed(:, flvr, flvr), dtau)
 
      return
   end function ctqmc_make_htau
@@ -113,7 +113,7 @@
              d2y = zero
 
 ! call the service layer
-             call ctqmc_make_spline(ntime, tmesh, htau(:,i,j), startu, startd, d2y)
+             call s_spl_splder(ntime, tmesh, htau(:,i,j), startu, startd, d2y)
 
 ! copy the results to hsed
              hsed(:,i,j) = d2y
@@ -123,145 +123,3 @@
 
      return
   end subroutine ctqmc_make_hsed
-
-!>>> ctqmc_make_spline: evaluate the 2-order derivates of yval
-  subroutine ctqmc_make_spline(ydim, xval, yval, startu, startd, d2y)
-     use constants
-
-     implicit none
-
-! external arguments
-! dimension of xval and yval
-     integer, intent(in)   :: ydim
-
-! first-derivate at point 1
-     real(dp), intent(in)  :: startu
-
-! first-derivate at point ydim
-     real(dp), intent(in)  :: startd
-
-! old knots
-     real(dp), intent(in)  :: xval(ydim)
-
-! old function values to be interpolated
-     real(dp), intent(in)  :: yval(ydim)
-
-! 2-order derivates
-     real(dp), intent(out) :: d2y(ydim)
-
-! local variables
-! loop index
-     integer  :: i
-     integer  :: k
-
-! dummy variables
-     real(dp) :: p
-     real(dp) :: qn
-     real(dp) :: un
-     real(dp) :: sig
-
-! dummy arrays
-     real(dp) :: u(ydim)
-
-! deal with left boundary
-     if ( startu > .99E30 ) then
-         d2y(1) = zero
-         u(1) = zero
-     else
-         p = xval(2) - xval(1)
-         d2y(1) = -half
-         u(1) = ( 3.0_dp / p ) * ( ( yval(2) - yval(1) ) / p - startu )
-     endif
-
-     do i=2,ydim-1
-         sig    = ( xval(i) - xval(i-1) ) / ( xval(i+1) - xval(i-1) )
-         p      = sig * d2y(i- 1) + two
-         d2y(i) = ( sig - one ) / p
-         u(i)   = ( 6.0_dp * ( ( yval(i+1) - yval(i) ) / &
-                    ( xval(i+1) - xval(i) ) - ( yval(i) - yval(i-1) ) / &
-                    ( xval(i) - xval(i-1) ) ) / &
-                    ( xval(i+1) - xval(i-1) ) - sig * u(i-1) ) / p
-     enddo ! over i={2,ydim-1} loop
-
-! deal with right boundary
-     if ( startd > .99E30 ) then
-         qn = zero
-         un = zero
-     else
-         p = xval(ydim) - xval(ydim-1)
-         qn = half
-         un = ( 3.0_dp / p ) * ( startd - ( yval(ydim) - yval(ydim-1) ) / p )
-     endif
-
-     d2y(ydim) = ( un - qn * u(ydim-1) ) / ( qn * d2y(ydim-1) + one )
-
-     do k=ydim-1,1,-1
-         d2y(k) = d2y(k) * d2y(k+1) + u(k)
-     enddo ! over k={ydim-1,1} loop
-
-     return
-  end subroutine ctqmc_make_spline
-
-!>>> ctqmc_make_splint: evaluate the spline value at x point
-  function ctqmc_make_splint(xdim, xval, yval, d2y, x) result(val)
-     use constants
-
-     implicit none
-
-! external arguments
-! dimension of xval and yval
-     integer, intent(in)  :: xdim
-
-! new mesh point
-     real(dp), intent(in) :: x
-
-! old mesh
-     real(dp), intent(in) :: xval(xdim)
-
-! old function value
-     real(dp), intent(in) :: yval(xdim)
-
-! 2-order derviates of old function
-     real(dp), intent(in) :: d2y(xdim)
-
-! local variables
-! lower boundary
-     integer  :: khi
-
-! higher boundary
-     integer  :: klo
-
-! distance between two successive mesh points
-     real(dp) :: h
-
-! dummy variables
-     real(dp) :: a, b
-
-! return value
-     real(dp) :: val
-
-! calculate the interval of spline zone
-     h = xval(2) - xval(1)
-
-! special trick is adopted to determine klo and kho
-     klo = floor(x/h) + 1
-     khi = klo + 1
-
-! note: we do not need to check khi here, since x can not reach right boundary
-! and left boundary either all
-!<     if ( khi > xdim ) then
-!<         klo = xdim - 1
-!<         khi = xdim
-!<     endif
-
-! calculate splined parameters a and b
-     a = ( xval(khi) - x ) / h
-     b = ( x - xval(klo) ) / h
-
-! spline it, obtain the fitted function value at x point
-     val = a * yval(klo) + b * yval(khi) + &
-               ( ( a*a*a - a ) * d2y(klo) + ( b*b*b - b ) * d2y(khi) ) * &
-               ( h*h ) / 6.0_dp
-
-     return
-  end function ctqmc_make_splint
