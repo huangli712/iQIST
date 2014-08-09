e59ce9f673f36a7f00db9eb076d26b73a9b9a73c
lihuang <li.huang@unifr.ch>
2014-08-10 00:54:01 +0200

    refine ctqmc_flavor.f90

--- src/ctqmc/azalea/ctqmc_flavor.f90
+++ src/ctqmc/azalea/ctqmc_flavor.f90
@@ -1407,13 +1407,14 @@
      return
   end subroutine ctqmc_make_flavor2
 
-!>>> determine \tau_s1 and \tau_s2 for lshift old segment or anti-segment
+!!>>> ctqmc_make_flavor3: determine \tau_s1 and \tau_s2 for lshift old
+!!>>> segment or anti-segment
   subroutine ctqmc_make_flavor3(flvr, iso, isn, ring, tau_start1, tau_start2)
-     use constants
-     use control
-     use context
+     use constants, only : dp
+     use control, only : beta
+     use context, only : ckink
 
-     use spring
+     use spring, only : spring_sfmt_stream
 
      implicit none
 
