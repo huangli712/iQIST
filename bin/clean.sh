01e42200f90746f11d68fae4d68745d39de77062
lihuang <li.huang@unifr.ch>
2014-08-19 01:42:09 +0300

    implement bin/clean.sh, from Heraklion

--- bin/clean.sh
+++ bin/clean.sh
@@ -5,13 +5,13 @@
 ## Introduction
 ## ============
 ##
-## It is a bash shell script. The purpose of this script is to make symbolic
-## links for the executable codes in the bin directory (current directory). 
+## It is a bash shell script. The purpose of this script is to clean the
+## current directory
 ##
 ## Usage
 ## =====
 ##
-## ./setup.sh
+## ./clean.sh
 ##
 ## Author
 ## ======
@@ -24,62 +24,8 @@
 ## History
 ## =======
 ##
-## 08/09/2014 by li huang
+## 08/19/2014 by li huang
 ##
 ##
 
-# azalea code
-if [ -e "../src/ctqmc/azalea/ctqmc" ]
-then
-    echo "[AZALEA]: found"
-    ln -fs ../src/ctqmc/azalea/ctqmc azalea.x
-    echo "[AZALEA]: setup OK"
-fi
-
-# gardenia code
-if [ -e "../src/ctqmc/gardenia/ctqmc" ]
-then
-    echo "[GARDENIA]: found"
-    ln -fs ../src/ctqmc/gardenia/ctqmc gardenia.x
-    echo "[GARDENIA]: setup OK"
-fi
-
-# narcissus code
-if [ -e "../src/ctqmc/narcissus/ctqmc" ]
-then
-    echo "[NARCISSUS]: found"
-    ln -fs ../src/ctqmc/narcissus/ctqmc narcissus.x
-    echo "[NARCISSUS]: setup OK"
-fi
-
-# begonia code
-if [ -e "../src/ctqmc/begonia/ctqmc" ]
-then
-    echo "[BEGONIA]: found"
-    ln -fs ../src/ctqmc/begonia/ctqmc begonia.x
-    echo "[BEGONIA]: setup OK"
-fi
-
-# lavender code
-if [ -e "../src/ctqmc/lavender/ctqmc" ]
-then
-    echo "[LAVENDER]: found"
-    ln -fs ../src/ctqmc/lavender/ctqmc lavender.x
-    echo "[LAVENDER]: setup OK"
-fi
-
-# pansy code
-if [ -e "../src/ctqmc/pansy/ctqmc" ]
-then
-    echo "[PANSY]: found"
-    ln -fs ../src/ctqmc/pansy/ctqmc pansy.x
-    echo "[PANSY]: setup OK"
-fi
-
-# manjushaka code
-if [ -e "../src/ctqmc/manjushaka/ctqmc" ]
-then
-    echo "[MANJUSHAKA]: found"
-    ln -fs ../src/ctqmc/manjushaka/ctqmc manjushaka.x
-    echo "[MANJUSHAKA]: setup OK"
-fi
+rm -fr *.x
