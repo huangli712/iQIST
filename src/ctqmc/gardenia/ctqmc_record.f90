0a9eca2dbfd2ff8cff802e8ff1380c78eefe93e8
li huang <li.huang@unifr.ch>
2014-09-11 00:46:56 +0200

    modify ctqmc_record.f90, refine the code style and limit the module usage, not finished

--- src/ctqmc/gardenia/ctqmc_record.f90
+++ src/ctqmc/gardenia/ctqmc_record.f90
@@ -112,7 +112,8 @@
 
   contains
 
-!>>> record impurity green's function using normal representation
+!!>>> cat_record_gtau1: record impurity green's function using normal
+!!>>> representation
   subroutine cat_record_gtau1()
      implicit none
 
@@ -158,7 +159,8 @@
      return
   end subroutine cat_record_gtau1
 
-!>>> record impurity green's function using legendre polynomial representation
+!!>>> cat_record_gtau2: record impurity green's function using legendre
+!!>>> polynomial representation
   subroutine cat_record_gtau2()
      implicit none
 
@@ -205,7 +207,8 @@
      return
   end subroutine cat_record_gtau2
 
-!>>> record impurity green's function using chebyshev polynomial representation
+!!>>> cat_record_gtau3: record impurity green's function using chebyshev
+!!>>> polynomial representation
   subroutine cat_record_gtau3()
      implicit none
 
@@ -253,7 +256,8 @@
   end subroutine cat_record_gtau3
   end subroutine ctqmc_record_gtau
 
-!>>> record the auxiliary correlation function in imaginary time axis
+!!>>> ctqmc_record_ftau: record the auxiliary correlation function in
+!!>>> imaginary time axis
   subroutine ctqmc_record_ftau()
      use constants
      use control
@@ -482,11 +486,13 @@
   end subroutine cat_record_ftau3
   end subroutine ctqmc_record_ftau
 
-!>>> record the impurity green's function in matsubara frequency space
+!!>>> ctqmc_record_grnf: record the impurity green's function in matsubara
+!!>>> frequency space
   subroutine ctqmc_record_grnf()
-     use constants
-     use control
-     use context
+     use control, only : norbs
+     use control, only : nfreq
+     use context, only : gmat
+     use context, only : grnf
 
      implicit none
 
@@ -507,9 +513,11 @@
      return
   end subroutine ctqmc_record_grnf
 
-!>>> record the histogram of perturbation expansion series
+!!>>> ctqmc_record_hist: record the histogram of perturbation expansion series
   subroutine ctqmc_record_hist()
-     use context
+     use control, only : mkink
+     use context, only : ckink
+     use context, only : hist
 
      implicit none
 
@@ -523,12 +531,59 @@
      return
   end subroutine ctqmc_record_hist
 
-!>>> record the occupation matrix, double occupation matrix, and auxiliary
-! physical observables simulataneously
+!!>>> ctqmc_record_prob: record the probability of atomic states
+  subroutine ctqmc_record_prob()
+     use constants, only : one
+
+     use control, only : norbs
+     use context, only : prob
+     use context, only : stts
+
+     implicit none
+
+! local variables
+! current flavor channel
+     integer :: flvr
+
+! atomic state index
+     integer :: pstat
+
+! current atomic state for segment representation
+     integer :: state(norbs)
+
+! generate current atomic state
+     do flvr=1,norbs
+         select case ( stts(flvr) )
+
+             case (0:1)
+                 state(flvr) = 0
+
+             case (2:3)
+                 state(flvr) = 1
+
+         end select
+     enddo ! over flvr={1,norbs} loop
+
+! convert atomic state array to index
+     call ctqmc_make_state(norbs, pstat, state)
+
+! accumulate the data
+     prob(pstat) = prob(pstat) + one
+
+     return
+  end subroutine ctqmc_record_prob
+
+!!>>> ctqmc_record_nmat: record the occupation matrix, double occupation
+!!>>> matrix, and auxiliary physical observables simulataneously
   subroutine ctqmc_record_nmat()
-     use constants
-     use control
-     use context
+     use constants, only : dp, zero
+
+     use control, only : nband, norbs
+     use control, only : beta
+     use context, only : ckink
+     use context, only : index_s, index_e, time_s, time_e
+     use context, only : paux, nmat, nnmat
+     use context, only : rank, stts, uumat
 
      implicit none
 
@@ -1052,46 +1107,6 @@
 
      return
   end subroutine ctqmc_record_vrtx
-
-!>>> record the probability of atomic states
-  subroutine ctqmc_record_prob()
-     use constants
-     use control
-     use context
-
-     implicit none
-
-! local variables
-! current flavor channel
-     integer :: flvr
-
-! atomic state index
-     integer :: pstat
-
-! current atomic state for segment representation
-     integer :: state(norbs)
-
-! generate current atomic state
-     do flvr=1,norbs
-         select case ( stts(flvr) )
-
-             case (0:1)
-                 state(flvr) = 0
-
-             case (2:3)
-                 state(flvr) = 1
-
-         end select
-     enddo ! over flvr={1,norbs} loop
-
-! convert atomic state array to index
-     call ctqmc_make_state(norbs, pstat, state)
-
-! accumulate the data
-     prob(pstat) = prob(pstat) + one
-
-     return
-  end subroutine ctqmc_record_prob
 
 !>>> reduce the gtau from all children processes
   subroutine ctqmc_reduce_gtau(gtau_mpi)
