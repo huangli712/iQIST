5173c8d8f6fb103cb80f2f0b568b789309ff23d2
li huang <li.huang@unifr.ch>
2014-09-10 22:42:00 +0200

    modify ctqmc_stream.f90, refine the code style and limit the module usage

--- src/ctqmc/gardenia/ctqmc_stream.f90
+++ src/ctqmc/gardenia/ctqmc_stream.f90
@@ -1,43 +1,31 @@
-!-------------------------------------------------------------------------
-! project : gardenia
-! program : ctqmc_config
-!           ctqmc_setup_array
-!           ctqmc_selfer_init
-!           ctqmc_solver_init
-!           ctqmc_final_array
-! source  : ctqmc_stream.f90
-! type    : subroutine
-! author  : li huang (email:huangli712@gmail.com)
-! history : 09/16/2009 by li huang
-!           09/20/2009 by li huang
-!           09/24/2009 by li huang
-!           09/27/2009 by li huang
-!           10/24/2009 by li huang
-!           10/29/2009 by li huang
-!           11/01/2009 by li huang
-!           11/10/2009 by li huang
-!           11/18/2009 by li huang
-!           12/01/2009 by li huang
-!           12/05/2009 by li huang
-!           02/27/2010 by li huang
-!           06/08/2010 by li huang
-! purpose : initialize and finalize the hybridization expansion version
-!           continuous time quantum Monte Carlo (CTQMC) quantum impurity
-!           solver and dynamical mean field theory (DMFT) self-consistent
-!           engine
-! input   :
-! output  :
-! status  : unstable
-! comment :
-!-------------------------------------------------------------------------
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : ctqmc_config
+!!!           ctqmc_setup_array
+!!!           ctqmc_selfer_init
+!!!           ctqmc_solver_init
+!!!           ctqmc_final_array
+!!! source  : ctqmc_stream.f90
+!!! type    : subroutines
+!!! author  : li huang (email:huangli712@gmail.com)
+!!! history : 09/16/2009 by li huang
+!!!           06/08/2010 by li huang
+!!!           09/10/2014 by li huang
+!!! purpose : initialize and finalize the hybridization expansion version
+!!!           continuous time quantum Monte Carlo (CTQMC) quantum impurity
+!!!           solver and dynamical mean field theory (DMFT) self-consistent
+!!!           engine
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
 
-!>>> setup key parameters for continuous time quantum Monte Carlo quantum
-! impurity solver and dynamical mean field theory kernel
+!!>>> ctqmc_config: setup key parameters for continuous time quantum Monte
+!!>>> Carlo quantum impurity solver and dynamical mean field theory kernel
   subroutine ctqmc_config()
-     use constants
-     use control
+     use parser, only : p_create, p_parse, p_get, p_destroy
+     use mmpi, only : mp_bcast, mp_barrier
 
-     use mmpi
+     use control ! ALL
 
      implicit none
 
@@ -45,16 +33,20 @@
 ! used to check whether the input file (solver.ctqmc.in) exists
      logical :: exists
 
-!=========================================================================
-! setup dynamical mean field theory self-consistent engine related common variables
-!=========================================================================
+!!========================================================================
+!!>>> setup general control flags                                      <<<
+!!========================================================================
      isscf  = 2            ! non-self-consistent (1) or self-consistent mode (2)
      issun  = 2            ! without symmetry    (1) or with symmetry   mode (2)
      isspn  = 1            ! spin projection, PM (1) or AFM             mode (2)
      isbin  = 2            ! without binning     (1) or with binning    mode (2)
      isort  = 1            ! normal measurement  (1) or legendre polynomial  (2) or chebyshev polynomial (3)
      isvrt  = 1            ! without vertex      (1) or with vertex function (2)
-!-------------------------------------------------------------------------
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+!!========================================================================
+!!>>> setup common variables for quantum impurity model                <<<
+!!========================================================================
      nband  = 1            ! number of correlated bands
      nspin  = 2            ! number of spin projection
      norbs  = nspin*nband  ! number of correlated orbitals (= nband * nspin)
@@ -74,9 +66,9 @@
      alpha  = 0.70_dp      ! mixing parameter for self-consistent engine
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-!=========================================================================
-! setup continuous time quantum Monte Carlo quantum impurity solver related common variables
-!=========================================================================
+!!========================================================================
+!!>>> setup common variables for quantum impurity solver               <<<
+!!========================================================================
      lemax  = 32           ! maximum order for legendre polynomial
      legrd  = 20001        ! number of mesh points for legendre polynomial
      chmax  = 32           ! maximum order for chebyshev polynomial
