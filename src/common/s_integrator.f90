d6f829c6c7f41d247f57fa09d93b35130207e98a
li huang <li.huang@unifr.ch>
2014-09-20 00:46:52 +0200

    try to implement s_integrator.f90, not finished

--- src/common/s_integrator.f90
+++ src/common/s_integrator.f90
@@ -3,7 +3,7 @@
 !!! program : s_int_trapezoid
 !!!           s_int_simpson
 !!! source  : s_integrator.f90
-!!! type    : subroutines
+!!! type    : functions
 !!! author  : li huang (email:huangli712@gmail.com)
 !!! history : 09/20/2014 by li huang
 !!! purpose : these subroutines are used to do numerical integration with
@@ -12,36 +12,58 @@
 !!! comment :
 !!!-----------------------------------------------------------------------
 
+!!>>> s_int_trapezoid: numerical integration with trapezoid algorithm
   function s_int_trapezoid(f, a, b, n) result(val)
-     use constants
+     use constants, only : dp, zero, two
 
      implicit none
 
-     double precision, intent(in) :: a, b
-     integer, intent(in) :: n
-     double precision :: f
-     double precision :: h, trapSum
-     integer :: i
+! external arguments
+! number of data points
+     integer, intent(in)  :: n
 
+! boundries for numerical integration
+     real(dp), intent(in) :: a
+     real(dp), intent(in) :: b
+
+! external function
+     procedure( real(dp) ) :: f
+
+! local variables
+! loop index
+     integer  :: i
+
+! return value
      real(dp) :: val
 
+! step for integration
+     real(dp) :: h
+
+! sum for trapezoid rule
+     real(dp) :: trapSum
+
+! evaluate the step
      h = (b-a) / dble(n)
 
-     trapSum = 0.0d0
-
-     do i=1, n-1
+! calculate trapezoid sum
+     trapSum = zero
+     do i=1,n-1
          trapSum = trapSum + f(a+dble(i)*h)
-     enddo
+     enddo ! over i={1,n-1} loop
 
-     val = (h/2.0d0) * ( f(a) + f(b) + 2.0d0*trapSum )
+! calculate the final value
+     val = ( h / two ) * ( f(a) + f(b) + two*trapSum )
 
      return 
   end function s_int_trapezoid
 
+!!>>> s_int_simpson: numerical integration with simpson algorithm
   function s_int_simpson(f, a, b, n) result(val)
      use constants
+
      implicit none
 
+! external argumenst
      double precision, intent(in) :: a,b
      integer, intent(in) :: n
      double precision :: h, oddSum, evenSum
