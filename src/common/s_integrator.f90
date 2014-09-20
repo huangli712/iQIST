5d2586a4bfb16e1a19c1dc85c4c9a68677be7445
li huang <li.huang@unifr.ch>
2014-09-20 19:50:31 +0200

    modify s_integrator.f90, finish the test procedure, and add some introductory comments

--- src/common/s_integrator.f90
+++ src/common/s_integrator.f90
@@ -12,6 +12,48 @@
 !!! comment :
 !!!-----------------------------------------------------------------------
 
+!!
+!!
+!! Introduction
+!! ============
+!!
+!! To use s_int_trapezoid() or s_int_simpson(), you have to define the 
+!! integrand at first. For example:
+!!
+!!
+!!  function f(x)
+!!     use constants, only : dp
+!!
+!!     implicit none
+!!
+!!     real(dp) :: x
+!!     real(dp) :: f
+!!
+!!     f = x * x
+!!  end function f
+!!
+!! Next, you have to determine the lower bound a and upper bound b, and
+!! the number of points n. Noted that now both the s_int_trapezoid() and
+!! s_int_simpson() functions only support the 1-D numerical integration.
+!!
+!! 1. use s_int_trapezoid()
+!! ------------------------
+!!
+!! procedure( real(dp) ) :: s_int_trapezoid
+!! procedure( real(dp) ) :: f
+!!
+!! val = s_int_trapezoid(f, a, b, n)
+!!
+!! 2. use s_int_simpson()
+!! ----------------------
+!!
+!! procedure( real(dp) ) :: s_int_simpson
+!! procedure( real(dp) ) :: f
+!!
+!! val = s_int_simpson(f, a, b, n)
+!!
+!!
+
 !!>>> s_int_trapezoid: numerical integration with trapezoid algorithm
   function s_int_trapezoid(f, a, b, n) result(val)
      use constants, only : dp, zero, two
@@ -26,7 +68,7 @@
      real(dp), intent(in) :: a
      real(dp), intent(in) :: b
 
-! external function, it means the integrator
+! external function, it means the integrand
      real(dp) :: f
 
 ! local variables
@@ -71,7 +113,7 @@
      real(dp), intent(in) :: a
      real(dp), intent(in) :: b
 
-! external function, it means the integrator
+! external function, it means the integrand
      real(dp) :: f
 
 ! local variables
