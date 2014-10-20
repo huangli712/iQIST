0c8d8cf696e2feb875414bafaecb390e6fd08fa9
li huang <li.huang@unifr.ch>
2014-10-20 22:34:30 +0200

    modify atomic_full.f90, get rid of the trailing whitespaces

--- src/tools/jasmine/atomic_full.f90
+++ src/tools/jasmine/atomic_full.f90
@@ -2,13 +2,13 @@
 !!! project : jasmine
 !!! program : atomic_make_foccu
 !!!           atomic_make_ffmat
-!!!           atomic_make_fhmat 
+!!!           atomic_make_fhmat
 !!! source  : atomic_full.f90
 !!! type    : subroutines
 !!! author  : yilin wang (email: qhwyl2006@126.com)
 !!! history : 07/09/2014 by yilin wang
 !!!           08/22/2014 by yilin wang
-!!! purpose : make occupancy of eigensates of atomic Hamiltonian 
+!!! purpose : make occupancy of eigensates of atomic Hamiltonian
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
@@ -20,39 +20,39 @@
 
      use m_full, only: bin_basis
      use m_full, only: occu, eigvec
-  
+
      implicit none
-  
+
 ! local variables
 ! loop index over orbits
      integer :: iorb
-  
+
 ! loop index over configurations
      integer :: ibas
-  
+
      occu = zero
      do ibas=1,ncfgs
          do iorb=1,norbs
              if (bin_basis(iorb, ibas) .eq. 1) then
                  occu(ibas, ibas) = occu(ibas, ibas) + one
              endif
-         enddo 
-     enddo 
-  
+         enddo
+     enddo
+
      call atomic_tran_repr_real(ncfgs, occu, eigvec)
-  
+
      return
   end subroutine atomic_make_foccu
 
-!!>>> atomic_make_ffmat: make fmat for annihilation operators 
+!!>>> atomic_make_ffmat: make fmat for annihilation operators
 !!>>> for full space case
   subroutine atomic_make_ffmat()
      use control, only : norbs, ncfgs
      use m_full, only : fmat, eigvec
      use m_full, only : dec_basis, index_basis
-  
+
      implicit none
-  
+
 ! local variables
 ! loop index
      integer :: i,j,k
@@ -63,9 +63,9 @@
 ! right Fock state
      integer :: right
 
-! the sign change 
+! the sign change
      integer :: isgn
-  
+
      do i=1,norbs
          do j=1,ncfgs
              right = dec_basis(j)
@@ -74,14 +74,14 @@
                 k = index_basis(left)
                 fmat(k, j, i) = dble(isgn)
              endif
-         enddo 
-     enddo 
-  
+         enddo
+     enddo
+
 ! rotate it to the atomic eigenvector basis
      do i=1, norbs
          call atomic_tran_repr_real(ncfgs, fmat(:,:,i), eigvec)
      enddo
-  
+
      return
   end subroutine atomic_make_ffmat
 
@@ -93,9 +93,9 @@
      use m_full, only : dec_basis, index_basis, bin_basis
      use m_spmat, only : emat, umat
      use m_full, only : hmat
-  
+
      implicit none
-  
+
 ! local variables
 ! loop index
      integer :: i
@@ -111,36 +111,36 @@
 
 ! binary code form of a state
      integer :: code(norbs)
-  
+
 ! start to make Hamiltonian
 ! initialize hmat
      hmat = czero
-  
-! first, two fermion operator terms 
+
+! first, two fermion operator terms
      do jbas=1, ncfgs
          alploop: do alpha=1,norbs
          betloop: do betta=1,norbs
-  
+
              sgn = 0
              knew = dec_basis(jbas)
              code(1:norbs) = bin_basis(1:norbs, jbas)
-           
+
              if ( abs(emat(alpha, betta)) .lt. epst ) cycle
-  
+
 ! simulate one annihilation operator
              if (code(betta) == 1) then
                  do i=1,betta-1
                      if (code(i) == 1) sgn = sgn + 1
-                 enddo 
+                 enddo
                  code(betta) = 0
-  
+
 ! simulate one creation operator
                  if (code(alpha) == 0) then
                      do i=1,alpha-1
                          if (code(i) == 1) sgn = sgn + 1
                      enddo
                      code(alpha) = 1
-  
+
 ! determine the row number and hamiltonian matrix elememt
                      knew = knew - 2**(betta-1)
                      knew = knew + 2**(alpha-1)
@@ -150,54 +150,54 @@
                          call s_print_error('atomic_make_fhmat', &
                                             'error while determining row1')
                      endif
-  
-                     hmat(ibas,jbas) = hmat(ibas,jbas) + emat(alpha,betta) * (-1.0d0)**sgn 
-  
+
+                     hmat(ibas,jbas) = hmat(ibas,jbas) + emat(alpha,betta) * (-1.0d0)**sgn
+
                  endif ! back if (code(alpha) == 0) block
              endif ! back if (code(betta) == 1) block
-  
+
          enddo betloop ! over betta={1,norbs} loop
          enddo alploop ! over alpha={1,norbs} loop
      enddo ! over jbas={1,ncfgs} loop
-  
+
 ! four fermion operator terms (coulomb interaction)
      do jbas=1, ncfgs
          alphaloop : do alpha=1,norbs
          bettaloop : do betta=1,norbs
          deltaloop : do delta=1,norbs
          gammaloop : do gamma=1,norbs
-  
+
              sgn  = 0
              knew = dec_basis(jbas)
              code(1:norbs) = bin_basis(1:norbs, jbas)
-  
+
              if ((alpha .eq. betta) .or. (delta .eq. gamma)) cycle
              if ( abs(umat(alpha,betta,delta,gamma)) .lt. epst ) cycle
-  
+
 ! simulate two annihilation operators
              if ((code(delta) == 1) .and. (code(gamma) == 1)) then
                  do i=1,gamma-1
                      if(code(i) == 1) sgn = sgn + 1
-                 enddo 
+                 enddo
                  code(gamma) = 0
-  
+
                  do i=1,delta-1
                      if(code(i) == 1) sgn = sgn + 1
-                 enddo 
+                 enddo
                  code(delta) = 0
-  
+
 ! simulate two creation operator
                  if ((code(alpha) == 0) .and. (code(betta) == 0)) then
                      do i=1,betta-1
                          if(code(i) == 1) sgn = sgn + 1
-                     enddo 
+                     enddo
                      code(betta) = 1
-  
+
                      do i=1,alpha-1
                          if(code(i) == 1) sgn = sgn + 1
-                     enddo 
+                     enddo
                      code(alpha) = 1
-  
+
 ! determine the row number and hamiltonian matrix elememt
                      knew = knew - 2**(gamma-1) - 2**(delta-1)
                      knew = knew + 2**(betta-1) + 2**(alpha-1)
@@ -206,17 +206,17 @@
                      if (ibas == 0) then
                          call s_print_error('atomic_make_fhmat', 'error while determining row3')
                      endif
-  
+
                      hmat(ibas,jbas) = hmat(ibas,jbas) + umat(alpha,betta,delta,gamma) * (-1.0d0)**sgn
-  
+
                  endif ! back if ((code(delta) == 1) .and. (code(gamma) == 1)) block
              endif ! back if ((code(alpha) == 0) .and. (code(betta) == 0)) block
-  
+
          enddo gammaloop ! over gamma={1,norbs-1} loop
          enddo deltaloop ! over delta={gamma+1,norbs} loop
          enddo bettaloop ! over betta={alpha+1,norbs} loop
          enddo alphaloop ! over alpha={1,norbs-1} loop
-     enddo  
-  
+     enddo
+
      return
   end subroutine atomic_make_fhmat
