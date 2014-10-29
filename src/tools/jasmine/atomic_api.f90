4c38a7b9b3e0ede35fee8eb6c4feadfb27249df8
li huang <li.huang@unifr.ch>
2014-10-29 22:01:43 +0100

    add atomic_api.f90

---
+++ src/tools/jasmine/atomic_api.f90
@@ -0,0 +1,22 @@
+!!!-----------------------------------------------------------------------
+!!! project : lilac
+!!! program : api
+!!!           api@T_jasmine
+!!!           api@init_atomic
+!!!           api@exec_atomic
+!!!           api@stop_atomic
+!!! source  : atomic_api.f90
+!!! type    : module
+!!! author  : li huang (email:huangli712@gmail.com)
+!!! history : 10/29/2014 by li huang
+!!! purpose : the purpose of this module is to define a generic and robust
+!!!           application programming interface (API) for atomic eigenvalue
+!!!           problem solver
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
+  module api
+     implicit none
+
+  end module api
