a503fb9bc610d74f3157803791f4334b2c60e241
li huang <li.huang@unifr.ch>
2014-11-03 22:59:10 +0100

    modify ctqmc_dmft.f90, remove ctqmc_dmft_mixer(), and add ctqmc_dmft_anydos()

--- src/ctqmc/begonia/ctqmc_dmft.f90
+++ src/ctqmc/begonia/ctqmc_dmft.f90
@@ -22,14 +22,17 @@
 !!>>> quantum Monte Carlo quantum impurity solver plus dynamical mean field
 !!>>> theory simulation
   subroutine ctqmc_dmft_selfer()
-     use constants, only : dp, mystd
+     use constants, only : dp, one, half, czi, mystd
 
-     use control, only : nband
-     use control, only : mune
+     use control, only : nband, norbs
+     use control, only : mfreq
+     use control, only : Uc, Jz
+     use control, only : mune, alpha
      use control, only : myid, master
      use context, only : tmesh, rmesh
      use context, only : eimp
-     use context, only : wssf, hybf
+     use context, only : grnf
+     use context, only : wtau, wssf, hybf
 
      implicit none
 
@@ -52,60 +55,58 @@
 ! allocate memory
      allocate(htmp(mfreq,norbs,norbs), stat=istat)
      if ( istat /= 0 ) then
-         call ctqmc_print_error('ctqmc_dmft_selfer','can not allocate enough memory')
-     endif
+         call s_print_error('ctqmc_dmft_selfer','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
 
 ! initialize htmp
-     htmp = czero
+     htmp = hybf
 
 ! calculate new hybridization function using self-consistent condition
-     call ctqmc_dmft_bethe(htmp, grnf)
+     call ctqmc_dmft_bethe(hybf, grnf)
 
 ! mixing new and old hybridization function: htmp and hybf
-     call ctqmc_dmft_mixer(hybf, htmp)
-
-! update original hybridization function
-     hybf = htmp
+     call s_mix_z(size(hybf), htmp, hybf, alpha)
 
 ! \mu_{eff} = (N - 0.5)*U - (N - 1)*2.5*J
      qmune = ( real(nband) - half ) * Uc - ( real(nband) - one ) * 2.5_dp * Jz
      qmune = mune - qmune
 
 ! calculate new bath weiss's function
+! G^{-1}_0 = i\omega + mu - E_{imp} - \Delta(i\omega)
      do i=1,norbs
          do k=1,mfreq
-             wssf(k,i,i) = cmesh(k) + qmune - eimp(i) - hybf(k,i,i)
+             wssf(k,i,i) = czi * rmesh(k) + qmune - eimp(i) - hybf(k,i,i)
          enddo ! over k={1,mfreq} loop
      enddo ! over i={1,norbs} loop
 
      do k=1,mfreq
-         call ctqmc_zmat_inv(norbs, wssf(k,:,:))
+         call s_inv_z(norbs, wssf(k,:,:))
      enddo ! over k={1,mfreq} loop
 
 ! fourier transformation bath weiss's function from matsubara frequency
 ! space to imaginary time space
-     call ctqmc_fourier_hybf(wssf, wtau)
+     call ctqmc_four_hybf(wssf, wtau)
 
 ! write out the new bath weiss's function in matsubara frequency axis
      if ( myid == master ) then ! only master node can do it
          call ctqmc_dump_wssf(rmesh, wssf)
-     endif
+     endif ! back if ( myid == master ) block
 
 ! write out the new bath weiss's function in imaginary time axis
      if ( myid == master ) then ! only master node can do it
          call ctqmc_dump_wtau(tmesh, wtau)
-     endif
+     endif ! back if ( myid == master ) block
 
 ! write out the new hybridization function
      if ( myid == master ) then ! only master node can do it
          call ctqmc_dump_hybf(rmesh, hybf)
-     endif
+     endif ! back if ( myid == master ) block
 
 ! print necessary self-consistent simulation information
      if ( myid == master ) then ! only master node can do it
          write(mystd,'(2X,a)') 'BEGONIA >>> DMFT hybridization function is updated'
          write(mystd,*)
-     endif
+     endif ! back if ( myid == master ) block
 
 ! deallocate memory
      deallocate(htmp)
@@ -113,11 +114,15 @@
      return
   end subroutine ctqmc_dmft_selfer
 
-!>>> check the convergence of self-energy function
+!!>>> ctqmc_dmft_conver: check the convergence of self-energy function
   subroutine ctqmc_dmft_conver(iter, convergence)
-     use constants
-     use control
-     use context
+     use constants, only : dp, zero, one, two, eps8, mystd
+
+     use control, only : norbs, niter
+     use control, only : mfreq
+     use control, only : alpha
+     use control, only : myid, master
+     use context, only : sig1, sig2
 
      implicit none
 
@@ -161,44 +166,28 @@
      convergence = ( ( seps <= eps8 ) .and. ( iter >= minit ) )
 
 ! update sig1
-     sig1 = sig1 * (one - alpha) + sig2 * alpha
+     call s_mix_z(size(sig1), sig2, sig1, one - alpha)
 
 ! write convergence information to screen
      if ( myid == master ) then ! only master node can do it
          write(mystd,'(3(2X,a,i3))') 'BEGONIA >>> cur_iter:', iter, 'min_iter:', minit, 'max_iter:', niter
-         write(mystd,'(2(2X,a,E10.4))') 'BEGONIA >>> sig_curr:', seps, 'eps_curr:', eps8
+         write(mystd,'(2(2X,a,E12.4))') 'BEGONIA >>> sig_curr:', seps, 'eps_curr:', eps8
          write(mystd,'( (2X,a,L1))') 'BEGONIA >>> self-consistent iteration convergence is ', convergence
          write(mystd,*)
-     endif
+     endif ! back if ( myid == master ) block
 
      return
   end subroutine ctqmc_dmft_conver
 
-!>>> complex(dp) version, mixing two vectors using linear mixing algorithm
-  subroutine ctqmc_dmft_mixer(vec1, vec2)
-     use constants
-     use control
-
-     implicit none
-
-! external arguments
-! older green/weiss/sigma function in input
-     complex(dp), intent(inout) :: vec1(mfreq,norbs,norbs)
-
-! newer green/weiss/sigma function in input, newest in output
-     complex(dp), intent(inout) :: vec2(mfreq,norbs,norbs)
-
-! linear mixing scheme
-     vec2 = vec1 * (one - alpha) + vec2 * alpha
-
-     return
-  end subroutine ctqmc_dmft_mixer
-
-!>>> self-consistent conditions, bethe lattice, semicircular density of
-! states, force a paramagnetic order, equal bandwidth
+!!>>> ctqmc_dmft_bethe: dmft self-consistent conditions, bethe lattice,
+!!>>> semicircular density of states, force a paramagnetic order, equal
+!!>>> band width
   subroutine ctqmc_dmft_bethe(hybf, grnf)
-     use constants
-     use control
+     use constants, only : dp
+
+     use control, only : norbs
+     use control, only : mfreq
+     use control, only : part
 
      implicit none
 
@@ -222,3 +211,146 @@
 
      return
   end subroutine ctqmc_dmft_bethe
+
+!!>>> ctqmc_dmft_anydos: dmft self-consistent conditions, general density
+!!>>> of states, calculate the new hybridization function by using hilbert
+!!>>> transformation and numerical integration
+  subroutine ctqmc_dmft_anydos(hybf, grnf, sigf)
+     use constants, only : dp, zero, czi, czero, mytmp
+     use mmpi, only : mp_bcast, mp_barrier
+
+     use control, only : norbs
+     use control, only : mfreq
+     use control, only : mune
+     use control, only : myid, master
+     use context, only : rmesh
+     use context, only : eimp
+
+     implicit none
+
+! external arguments
+! hybridization function
+     complex(dp), intent(out) :: hybf(mfreq,norbs,norbs)
+
+! impurity green's function
+     complex(dp), intent(out) :: grnf(mfreq,norbs,norbs)
+
+! self-energy function
+     complex(dp), intent(in)  :: sigf(mfreq,norbs,norbs)
+
+! local parameters
+! size of real frequency grid
+     integer, parameter :: ngrid = 801
+
+! local variables
+! loop index
+     integer  :: i
+     integer  :: j
+     integer  :: k
+
+! used to check whether the input file (solver.anydos.in) exists
+     logical  :: exists
+
+! energy interval for the frequency grid
+     real(dp) :: step
+
+! normalization factor for the orbital-resolved density of states
+     real(dp) :: dsum
+
+! complex(dp) dummy variables, to evalute the new impurity green's function
+     complex(dp) :: caux
+
+! real frequency grid, we assume it is equidistant
+     real(dp) :: epsi(ngrid)
+
+! orbital-resolved density of states, they can be non-degenerate
+     real(dp) :: pdos(ngrid,norbs)
+
+! initialize essential array
+     epsi = zero
+     pdos = zero
+
+     hybf = czero
+     grnf = czero
+
+! read in frequency grid and density of states if available
+!-------------------------------------------------------------------------
+     if ( myid == master ) then ! only master node can do it
+         exists = .false.
+
+! inquire about file's existence
+         inquire (file = 'solver.anydos.in', exist = exists)
+
+! find input file: solver.anydos.in, read it
+         if ( exists .eqv. .true. ) then
+
+! read in frequency grid and density of states from solver.anydos.in
+! note: we assume the density of states for different orbitals are
+! degenerate. please make sure it again before using this subroutine.
+             open(mytmp, file='solver.anydos.in', form='formatted', status='unknown')
+             do i=1,ngrid
+                 read(mytmp,*) epsi(i), pdos(i,1)
+                 do j=2,norbs
+                     pdos(:,j) = pdos(:,1)
+                 enddo ! over j={2,norbs} loop
+             enddo ! over i={1,ngrid} loop
+             close(mytmp)
+
+         else
+             call s_print_error('ctqmc_dmft_anydos','file solver.anydos.in does not exist')
+         endif ! back if ( exists .eqv. .true. ) block
+     endif ! back if ( myid == master ) block
+
+! broadcast epsi and pdos from master node to all children nodes
+# if defined (MPI)
+
+! broadcast data
+     call mp_bcast(epsi, master)
+
+! broadcast data
+     call mp_bcast(pdos, master)
+
+! block until all processes have reached here
+     call mp_barrier()
+
+# endif  /* MPI */
+
+! evaluate energy interval
+     step = epsi(2) - epsi(1)
+
+! calculate new impurity green's function by using hilbert transformation
+     do j=1,norbs
+
+! evaluate dsum, which is used to normalize the density of states
+         dsum = sum( pdos(:,j) ) * step
+
+         do k=1,mfreq
+
+! caux = i\omega + \mune - E_{imp} - Sigma(i\omega)
+             caux = czi * rmesh(k) + mune - eimp(j) - sigf(k,j,j)
+
+! perform numerical integration
+             do i=1,ngrid
+                 grnf(k,j,j) = grnf(k,j,j) + pdos(i,j) / ( caux - epsi(i) )
+             enddo ! over i={1,ngrid} loop
+
+! renormalize grnf
+             grnf(k,j,j) = grnf(k,j,j) * ( step / dsum )
+
+         enddo ! over k={1,mfreq} loop
+     enddo ! over j={1,norbs} loop
+
+! calculate G^{-1}, now grnf contains G^{-1}
+     do k=1,mfreq
+         call s_inv_z(norbs, grnf(k,:,:))
+     enddo ! over k={1,mfreq} loop
+
+! calculate final hybridization function using dyson's equation
+     do k=1,mfreq
+         do i=1,norbs
+             hybf(k,i,i) = czi * rmesh(k) + mune - eimp(i) - sigf(k,i,i) - grnf(k,i,i)
+         enddo ! over i={1,norbs} loop
+     enddo ! over k={1,mfreq} loop
+
+     return
+  end subroutine ctqmc_dmft_anydos
