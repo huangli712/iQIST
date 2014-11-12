c73eea547fb33c6db0c6c990d263c29b73266947
li huang <li.huang@unifr.ch>
2014-11-13 00:28:11 +0100

    modify ctqmc.py, try to implement it

--- src/tools/hibiscus/script/ctqmc.py
+++ src/tools/hibiscus/script/ctqmc.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
 
-class ctqmc:
+##
+##
+## Introduction
+## ============
+##
+## Usage
+## =====
+##
+## Author
+## ======
+##
+## This python script is designed, created, implemented, and maintained by
+##
+## Li Huang // email: huangli712@gmail.com
+##
+## History
+## =======
+##
+## 11/13/2014 by li huang
+##
+##
+
+class ctqmc(object):
     pass
