ffd1e63e46660c1d495f776b9a5489d63ca3cb2b
li huang <li.huang@unifr.ch>
2014-11-18 12:15:23 +0100

    modify sac_print.f90, improve the code style

--- src/tools/hibiscus/stoch/sac_print.f90
+++ src/tools/hibiscus/stoch/sac_print.f90
@@ -16,32 +16,40 @@
 !!! comment :
 !!!-----------------------------------------------------------------------
 
-!>>> print the startup information for stochastic analytic continuation code
+!!>>> sac_print_header: print the startup information for stochastic
+!!>>> analytic continuation code
   subroutine sac_print_header()
-     use constants
+     use constants, only : mystd
+
      use control, only : nprocs
 
      implicit none
 
-     write(mystd,'(2X,a)') 'HIBISCUS'
+! string for current date and time
+     character (len = 20) :: date_time_string
+
+! obtain current date and time
+     call s_time_builder(date_time_string)
+
+     write(mystd,'(2X,a)') 'HIBISCUS/stoch'
      write(mystd,'(2X,a)') '>>> A Stochastic Analytic Continuation Code for Imaginary Time Data'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'version: 2011.08.18T            '
-     write(mystd,'(2X,a)') 'develop: by li huang, CAEP & IOP'
-     write(mystd,'(2X,a)') 'support: huangli712@yahoo.com.cn'
-     write(mystd,'(2X,a)') 'license: GPL2 and later versions'
+     write(mystd,'(2X,a)') 'Version: 2014.10.11T '//'(built at '//__TIME__//" "//__DATE__//')'
+     write(mystd,'(2X,a)') 'Develop: by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
+     write(mystd,'(2X,a)') 'Support: huangli712@gmail.com'
+     write(mystd,'(2X,a)') 'License: GNU General Public License version 3'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'HIBISCUS >>> running'
+     write(mystd,'(2X,a)') 'HIBISCUS/stoch >>> start running at '//date_time_string
 
 # if defined (MPI)
 
-     write(mystd,'(2X,a,i4)') 'HIBISCUS >>> parallelism: Yes >>> processors:', nprocs
+     write(mystd,'(2X,a,i4)') 'HIBISCUS/stoch >>> parallelism: Yes >>> processors:', nprocs
 
 # else   /* MPI */
 
-     write(mystd,'(2X,a,i4)') 'HIBISCUS >>> parallelism: No  >>> processors:', 1
+     write(mystd,'(2X,a,i4)') 'HIBISCUS/stoch >>> parallelism: No  >>> processors:', 1
 
 # endif  /* MPI */
 
@@ -50,35 +58,43 @@
      return
   end subroutine sac_print_header
 
-!>>> print the ending information for stochastic analytic continuation code
+!!>>> sac_print_footer: print the ending information for stochastic
+!!>>> analytic continuation code
   subroutine sac_print_footer()
-     use constants
+     use constants, only : dp, mystd
 
      implicit none
 
-! used to record the time information
+! string for current date and time
+     character (len = 20) :: date_time_string
+
+! used to record the time usage information
      real(dp) :: tot_time
 
-! obtain time information
+! obtain time usage information
      call cpu_time(tot_time)
 
-     write(mystd,'(2X,a,f10.2,a)') 'HIBISCUS >>> total time spent:', tot_time, 's'
+! obtain current date and time
+     call s_time_builder(date_time_string)
+
+     write(mystd,'(2X,a,f10.2,a)') 'HIBISCUS/stoch >>> total time spent:', tot_time, 's'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'HIBISCUS >>> I am tired and want to go to bed. Bye!'
-     write(mystd,'(2X,a)') 'HIBISCUS >>> ending'
+     write(mystd,'(2X,a)') 'HIBISCUS/stoch >>> I am tired and want to go to bed. Bye!'
+     write(mystd,'(2X,a)') 'HIBISCUS/stoch >>> happy ending at '//date_time_string
 
      return
   end subroutine sac_print_footer
 
-!>>> print the running parameters, only for reference
+!!>>> sac_print_summary: print the running parameters, only for reference
   subroutine sac_print_summary()
-     use constants
-     use control
+     use constants, only : mystd, ev2k
+
+     use control ! ALL
 
      implicit none
 
-     write(mystd,'(2X,a)') 'HIBISCUS >>> parameters list:'
+     write(mystd,'(2X,a)') 'HIBISCUS/stoch >>> parameters list:'
 
      write(mystd,'(2(4X,a,i10)  )') 'ntime:', ntime, 'nalph:', nalph
      write(mystd,'(2(4X,a,i10)  )') 'nwmax:', nwmax, 'nwarm:', nwarm
@@ -86,20 +102,24 @@
      write(mystd,'(2(4X,a,i10)  )') 'ngamm:', ngamm, 'ndump:', ndump
      write(mystd,'(2(4X,a,i10)  )') 'ltype:', ltype, 'lemax:', lemax
 
-     write(mystd,'(2(4X,a,f10.5))') 'ainit:', ainit, 'eta1 :', eta1
+     write(mystd,'(2(4X,a,f10.5))') 'ainit:', ainit, 'wstep:', wstep
      write(mystd,'(2(4X,a,f10.5))') 'ratio:', ratio, 'sigma:', sigma
-     write(mystd,'(2(4X,a,f10.5))') 'beta :', beta , 'wstep:', wstep
+     write(mystd,'(2(4X,a,f10.5))') 'eta1 :', eta1 , 'eta2 :', eta2
+     write(mystd,'(2(4X,a,f10.5))') 'beta :', beta , 'temp :', ev2k/beta
 
      write(mystd,*)
 
      return
   end subroutine sac_print_summary
 
-!>>> print the runtime information, including statistic data, only for reference
+!!>>> sac_print_runtime: print the runtime information, including the
+!!>>> statistic data, only for reference
   subroutine sac_print_runtime(step, time_start, time_end)
-     use constants
-     use control
-     use context
+     use constants, only : dp, zero, mystd
+
+     use control, only : nalph, nstep
+     use context, only : move_accept, move_tcount
+     use context, only : swap_accept, swap_tcount
 
      implicit none
 
@@ -125,7 +145,7 @@
          else
              write(mystd,'(2(8X,a,f10.4))') 'move:', move_accept(i) / move_tcount(i), &
                                             'swap:', swap_accept(i) / swap_tcount(i)
-         endif
+         endif ! back if ( nalph == 1 ) block
      enddo ! over i={1,nalph} loop
      write(mystd,'(4X,a,f10.4,a)') '>>> time used in this interval:', time_end - time_start, 's'
      write(mystd,*)
