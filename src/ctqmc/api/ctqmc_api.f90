6e4600941ec38071466949a39597cf2d78ee43c2
li huang <li.huang@unifr.ch>
2014-11-21 01:27:58 +0100

    modify ctqmc_api.f90, fix the comment

--- src/ctqmc/api/ctqmc_api.f90
+++ src/ctqmc/api/ctqmc_api.f90
@@ -292,7 +292,7 @@
      implicit none
 
 !!========================================================================
-!!>>> declare global parameters                                        <<<
+!!>>> declare private parameters                                       <<<
 !!========================================================================
 
 ! dp: number precision, double precision for reals
