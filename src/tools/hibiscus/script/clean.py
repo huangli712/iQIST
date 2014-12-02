60dd6a61ca514268b75ceea9ade515f0c0f3bebd
li huang <li.huang@unifr.ch>
2014-12-02 18:30:09 +0100

    modify script/clean.py, implement clean.py, now it works

--- src/tools/hibiscus/script/clean.py
+++ src/tools/hibiscus/script/clean.py
@@ -1,7 +1,47 @@
-import os
+#!/usr/bin/env python
 
-for path, dirs, files in os.walk('.'):
-  for f in files:
-    if f.endswith('DS_Store'):
-        print path
-        print f
+##
+##
+## Introduction
+## ============
+##
+## It is a python script. The purpose of this script is to clean the dull
+## .DS_Store files for the Mac OS X filesystem.
+##
+## This script should be used by the developer only.
+##
+## Usage
+## =====
+##
+## ./clean.py directory_name
+##
+## Author
+## ======
+##
+## This python script is designed, created, implemented, and maintained by
+##
+## Li Huang // email: huangli712@gmail.com
+##
+## History
+## =======
+##
+## 12/02/2014 by li huang
+##
+##
+
+import os
+import sys
+
+# setup the directory we want to scan
+argu = sys.argv[1:]
+if ( len(argu) > 0 ):
+    scan_dir = argu[0]
+else:
+    scan_dir = '.'
+
+# scan the directory and delete all of the found .DS_Store files directly
+for path, dirs, files in os.walk(scan_dir):
+    for f in files:
+        if f.endswith('DS_Store'):
+            print path + '/' + f
+            os.remove(path + '/' + f)
