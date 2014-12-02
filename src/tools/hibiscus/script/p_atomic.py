8ccc5615bddb8d28a5f18ce43a033317cae978cc
li huang <li.huang@unifr.ch>
2014-12-02 23:25:22 +0100

    modify p_atomic.py, try to implement it

--- src/tools/hibiscus/script/p_atomic.py
+++ src/tools/hibiscus/script/p_atomic.py
@@ -5,8 +5,14 @@
 ## Introduction
 ## ============
 ##
+## It is a python script. The purpose of this script is generate essential
+## input file (atom.config.in) for the jasmine code. Note that you can not
+## use it to control the jasmine code.
+##
 ## Usage
 ## =====
+##
+## see the document string
 ##
 ## Author
 ## ======
@@ -18,9 +24,11 @@
 ## History
 ## =======
 ##
-## 11/13/2014 by li huang
+## 12/02/2014 by li huang
 ##
 ##
+
+import sys
 
 class p_atomic_solver(object):
     """
@@ -29,13 +37,61 @@ class p_atomic_solver(object):
     def __init__(self):
         """
         """
-        pass
+        self._p_cmp = {
+            'ibasis' : 1   ,
+            'ictqmc' : 1   ,
+            'icu'    : 1   ,
+            'icf'    : 0   ,
+            'isoc'   : 0   ,
+            'nband'  : 1   ,
+            'nspin'  : 2   ,
+            'norbs'  : 2   ,
+            'ncfgs'  : 4   ,
+            'nmini'  : 0   ,
+            'nmaxi'  : 2   ,
+            'Uc'     : 2.00,
+            'Uv'     : 2.00,
+            'Jz'     : 0.00,
+            'Js'     : 0.00,
+            'Jp'     : 0.00,
+            'Ud'     : 2.00,
+            'Jh'     : 0.00,
+            'mune'   : 0.00,
+            'lambda' : 0.00,
+        }
 
-    def setp(self):
-        pass
+        self._p_inp = {}
+
+    def setp(self, **kwargs):
+        """
+        """
+        if kwargs is not None:
+            for key, value in kwargs.iteritems():
+                self._p_inp[key] = value
 
     def check(self):
-        pass
+        """
+        """
+        for key in self._p_inp.iterkeys():
+            if key not in self._p_cmp:
+                sys.exit('FATAL ERROR: wrong key ' + key)
+            if type( self._p_inp[key] ) is not type( self._p_cmp[key] ):
+                sys.exit('FATAL ERROR: wrong value ' + key + ' = ' + str(self._p_inp[key]))
 
     def write(self):
-        pass
+        """
+        """
+        f = open('atom.config.in','w')
+        for key in self._p_inp.iterkeys():
+            empty = ( 8 - len(key) ) * ' ' + ': '
+            f.write(key + empty + str(self._p_inp[key]) + '\n')
+        f.close()
+
+#if __name__ == '__main__':
+#    p = p_atomic_solver()
+#    p.setp(ibasis = 2, Uv = 2.0)
+#    p.setp(icu = 30)
+#    p.setp(icu = 1)
+#    p.setp()
+#    p.check()
+#    p.write()
