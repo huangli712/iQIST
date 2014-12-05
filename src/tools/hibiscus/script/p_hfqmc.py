e63e4411a214a2431924f4c34a3cafb719d9a174
li huang <li.huang@unifr.ch>
2014-12-05 17:15:33 +0100

    modify p_hfqmc.py, try to implement it

---
+++ src/tools/hibiscus/script/p_hfqmc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+##
+##
+## Introduction
+## ============
+##
+## It is a python script. The purpose of this script is generate essential
+## input file (solver.hfqmc.in) for the daisy code. Note that you can not
+## use it to control the daisy code.
+##
+## Usage
+## =====
+##
+## see the document string
+##
+## Author
+## ======
+##
+## This python script is designed, created, implemented, and maintained by
+##
+## Li Huang // email: huangli712@gmail.com
+##
+## History
+## =======
+##
+## 12/03/2014 by li huang
+##
+##
+
+import sys
