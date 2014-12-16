6cdf5b975c4d37644b8cfe1a15f0dae7027e8b3b
li huang <li.huang@unifr.ch>
2014-12-16 01:05:35 +0100

    modify manjushaka/ctqmc_flavor.f90 and ctqmc_update.f90, try to reconstruct it

--- src/ctqmc/manjushaka/ctqmc_update.f90
+++ src/ctqmc/manjushaka/ctqmc_update.f90
@@ -248,21 +248,6 @@
 ! not equal to zero
      call try_remove_flavor(fis, fie, tau_start, tau_end, lrmv)
 
-!<! if lrmv is false, we set the pass as false immediately
-!<     if ( lrmv .eqv. .false. ) then
-!<         pass = .false.
-!<! we will determine the pass by lazy trace evalution
-!<     else
-!<! first, calculate the transition ratio between old and new configurations,
-!<! for the determinand part
-!<         call cat_remove_detrat(flvr, cis, cie, deter_ratio)
-!<
-!<! calculate the transition ratio between old and new configurations,
-!<! for the local trace part, by lazy trace evaluation
-!<         call cat_remove_ztrace(fis, fie, tau_start, tau_end, trace_ratio)
-!<         call ctqmc_lazy_ztrace( 2, 1, 2*sum(rank) - 2, deter_ratio, spring_sfmt_stream(), p, pass, tau_start, tau_end )
-!<     endif ! back if ( lrmv .eqv. .false. ) block
-
 ! calculate the transition ratio between old and new configurations,
 ! for the local trace part
      if ( lrmv .eqv. .true. ) then
@@ -400,20 +385,44 @@
 ! not equal to zero
      call try_lshift_flavor(flvr, fiso, fisn, tau_start1, tau_start2, lshf)
 
-! if lshf is false, we set the pass as false immediately
-     if ( lshf .eqv. .false. ) then
-         pass = .false.
-! we will determine the pass by lazy trace evalution
-     else
-! first, calculate the transition ratio between old and new configurations,
-! for the determinand part
-         call cat_lshift_detrat(flvr, ciso, tau_start1, tau_start2, deter_ratio)
+!<! if lshf is false, we set the pass as false immediately
+!<     if ( lshf .eqv. .false. ) then
+!<         pass = .false.
+!<! we will determine the pass by lazy trace evalution
+!<     else
+!<! first, calculate the transition ratio between old and new configurations,
+!<! for the determinand part
+!<         call cat_lshift_detrat(flvr, ciso, tau_start1, tau_start2, deter_ratio)
+!<
+!<! calculate the transition ratio between old and new configurations,
+!<! for the local trace part, by lazy trace evaluation
+!<         call cat_lshift_ztrace(flvr, fiso, fisn, tau_start1, tau_start2, trace_ratio)
+!<         call ctqmc_lazy_ztrace( 3, 1, 2*sum(rank), deter_ratio, spring_sfmt_stream(), p, pass, tau_start1, tau_start2 )
+!<     endif ! back if ( lshf .eqv. .false. ) block
 
 ! calculate the transition ratio between old and new configurations,
-! for the local trace part, by lazy trace evaluation
+! for the local trace part
+     if ( lshf .eqv. .true. ) then
          call cat_lshift_ztrace(flvr, fiso, fisn, tau_start1, tau_start2, trace_ratio)
+     else
+         trace_ratio = zero
+     endif ! back if ( lshf .eqv. .true. ) block
+
+! calculate the transition ratio between old and new configurations,
+! for the determinant part
+     if ( lshf .eqv. .true. ) then
+         call cat_lshift_detrat(flvr, ciso, tau_start1, tau_start2, deter_ratio)
+     else
+         deter_ratio = zero
+     endif ! back if ( lshf .eqv. .true. ) block
+
+! we will determine the pass by lazy trace evalution
+! if lshf is false, we set the pass as false immediately
+     if ( lshf .eqv. .true. ) then
          call ctqmc_lazy_ztrace( 3, 1, 2*sum(rank), deter_ratio, spring_sfmt_stream(), p, pass, tau_start1, tau_start2 )
-     endif ! back if ( lshf .eqv. .false. ) block
+     else
+         pass = .false.
+     endif ! back if ( lshf .eqv. .true. ) block
 
 ! if update action is accepted
      if ( pass .eqv. .true. ) then
