8589d3be495536fa42d4e21e8948fedca973803e
li huang <li.huang@unifr.ch>
2014-12-20 23:48:32 +0100

    add new robot.py in script directory, not finished

---
+++ src/tools/hibiscus/script/robot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+##
+##
+## Introduction
+## ============
+##
+## It is a python script. The purpose of this script is to clean the dull
+## .DS_Store files for the Mac OS X filesystem.
+##
+## This script should be used by the developer only.
+##
+## Usage
+## =====
+##
+## ./clean.py directory_name
+##
+## Author
+## ======
+##
+## This python script is designed, created, implemented, and maintained by
+##
+## Li Huang // email: huangli712@gmail.com
+##
+## History
+## =======
+##
+## 12/20/2014 by li huang
+##
+##
+
+import os
+import sys
+
+# setup the directory we want to scan
+argu = sys.argv[1:]
+if ( len(argu) > 0 ):
+    scan_dir = argu[0]
+else:
+    scan_dir = '.'
