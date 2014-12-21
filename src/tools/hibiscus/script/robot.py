b975a5f13a466baea2374c2ea3bcceb2053d667a
li huang <li.huang@unifr.ch>
2014-12-21 01:48:55 +0100

    add robot.json, develop robot.py, not finished

--- src/tools/hibiscus/script/robot.py
+++ src/tools/hibiscus/script/robot.py
@@ -13,7 +13,7 @@
 ## Usage
 ## =====
 ##
-## ./robot.py job_file
+## ./robot.py robot_file
 ##
 ## Author
 ## ======
@@ -29,20 +29,48 @@
 ##
 ##
 
-#import os
-#import sys
-
-## setup the directory we want to scan
-#argu = sys.argv[1:]
-#if ( len(argu) > 0 ):
-#    scan_dir = argu[0]
-#else:
-#    scan_dir = '.'
-
+import os
+import sys
 import json
 
-with open("job.json") as json_file:
-    json_data = json.load(json_file)
-    print json_data
-    for i in json_data.keys():
-        print i, json_data[i]
+def parse_robot_json(robot_json):
+    json_data = {}
+    cfg_robot = {}
+    with open(robot_json) as json_file:
+        json_data = json.load(json_file)
+    cfg_robot = json_data.copy()
+    return cfg_robot
+
+def parse_jobs_json(jobs_json):
+    json_data = {}
+    cfg_jobs  = []
+    with open(jobs_json) as json_file:
+        json_data = json.load(json_file)
+    cfg_robot = json_data["jobs"]
+    return cfg_robot
+
+if __name__ == '__main__':
+
+# setup the robot_file we want to parse
+    argu = sys.argv[1:]
+    if ( len(argu) > 0 ):
+        robot_file = argu[0]
+    else:
+        robot_file = './robot.json'
+
+# parse the robot_file
+    my_robot = parse_robot_json(robot_file)
+
+# prepare the directory
+
+# prepare the files
+
+# main loop
+    while True:
+
+# step 1: parse the jobs_file
+#        my_jobs = parse_jobs_json(my_robot["jobs"])
+
+# step 2: generate the sub-jobs
+
+
