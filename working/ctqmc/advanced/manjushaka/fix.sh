f4200a020faed13bee4beb1c3cedb403a1e0bbf9
li huang <li.huang@unifr.ch>
2015-01-03 00:11:08 +0100

    update advance/manjushaka directory

--- working/ctqmc/advanced/manjushaka/fix.sh
+++ working/ctqmc/advanced/manjushaka/fix.sh
@@ -5,7 +5,7 @@ do
     cd $i
     echo "job directory:"
     pwd
-    sed -i '' 's/begonia, lavender, pansy and manjushaka/BEGONIA, LAVENDER, PANSY, and MANJUSHAKA/g' atom.config.in
+    sed -i '' 's/manjushaka/MANJUSHAKA/g' solver.ctqmc.in
     echo ''
     cd ..
 done
