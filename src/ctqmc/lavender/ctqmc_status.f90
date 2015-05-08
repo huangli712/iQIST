c6ccda100664aaf58b3b98bc699b78cc1e460db1
Li Huang <huangli712@gmail.com>
2015-05-08 17:05:42 +0800

    modify lavender/ctqmc_status.f90 and ctqmc_update.f90, fix the call to ctqmc_make_ztrace()

--- src/ctqmc/lavender/ctqmc_status.f90
+++ src/ctqmc/lavender/ctqmc_status.f90
@@ -249,7 +249,7 @@
 
 ! update the matrix trace for product of F matrix and time evolution operators
      i = 2 * sum(rank) ! get total number of operators
-     call ctqmc_make_ztrace(4, i, matrix_ntrace)
+     call ctqmc_make_ztrace(4, i, matrix_ntrace, zero, zero)
 
 ! update the operators trace
      call ctqmc_make_evolve()
