7415507c37b77f0c8d3eb1f3a33a7c184be2fbed
Li Huang <huangli712@gmail.com>
2015-05-25 15:46:40 +0800

    modify narcissus/ctqmc_solver.f90, add support to fidelity susceptibility

--- src/ctqmc/narcissus/ctqmc_solver.f90
+++ src/ctqmc/narcissus/ctqmc_solver.f90
@@ -31,7 +31,9 @@
      use control, only : myid, master
      use context, only : tmesh, rmesh
      use context, only : hist, prob
-     use context, only : nmat, nnmat, schi, sschi, ochi, oochi
+     use context, only : nmat, nnmat
+     use context, only : lmat, rmat, lrmat
+     use context, only : schi, sschi, ssfom, ochi, oochi, oofom
      use context, only : g2_re, g2_im, h2_re, h2_im, ps_re, ps_im
      use context, only : symm
      use context, only : gtau, ftau, grnf
@@ -85,17 +87,32 @@
 ! impurity double occupation number matrix, for mpi case
      real(dp), allocatable :: nnmat_mpi(:,:)
 
+! number of operators at left half axis, for mpi case
+     real(dp), allocatable :: lmat_mpi(:)
+
+! number of operators at right half axis, for mpi case
+     real(dp), allocatable :: rmat_mpi(:)
+
+! used to evaluate fidelity susceptibility, for mpi case
+     real(dp), allocatable :: lrmat_mpi(:,:)
+
 ! spin-spin correlation function, totally-averaged, for mpi case
      real(dp), allocatable :: schi_mpi(:)
 
 ! spin-spin correlation function, orbital-resolved, for mpi case
      real(dp), allocatable :: sschi_mpi(:,:)
 
+! spin-spin correlation function, orbital-resolved, for mpi case
+     real(dp), allocatable :: ssfom_mpi(:,:)
+
 ! orbital-orbital correlation function, totally-averaged, for mpi case
      real(dp), allocatable :: ochi_mpi(:)
 
 ! orbital-orbital correlation function, orbital-resolved, for mpi case
      real(dp), allocatable :: oochi_mpi(:,:,:)
+
+! orbital-orbital correlation function, orbital-resolved, for mpi case
+     real(dp), allocatable :: oofom_mpi(:,:,:)
 
 ! used to measure two-particle green's function, real part, for mpi case
      real(dp), allocatable :: g2_re_mpi(:,:,:,:,:)
@@ -145,6 +162,21 @@
          call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
 
+     allocate(lmat_mpi(norbs),             stat=istat)
+     if ( istat /= 0 ) then
+         call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
+
+     allocate(rmat_mpi(norbs),             stat=istat)
+     if ( istat /= 0 ) then
+         call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
+
+     allocate(lrmat_mpi(norbs,norbs),      stat=istat)
+     if ( istat /= 0 ) then
+         call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
+
      allocate(schi_mpi(ntime),             stat=istat)
      if ( istat /= 0 ) then
          call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
@@ -155,12 +187,22 @@
          call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
 
+     allocate(ssfom_mpi(nbfrq,nband),      stat=istat)
+     if ( istat /= 0 ) then
+         call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
+
      allocate(ochi_mpi(ntime),             stat=istat)
      if ( istat /= 0 ) then
          call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
 
      allocate(oochi_mpi(ntime,norbs,norbs),stat=istat)
+     if ( istat /= 0 ) then
+         call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
+     endif ! back if ( istat /= 0 ) block
+
+     allocate(oofom_mpi(nbfrq,norbs,norbs),stat=istat)
      if ( istat /= 0 ) then
          call s_print_error('ctqmc_impurity_solver','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
@@ -374,6 +416,21 @@
                  call ctqmc_record_ochi()
              endif ! back if ( mod(cstep, nmonte) == 0 .and. btest(issus, 2) ) block
 
+! record the spin-spin correlation function
+             if ( mod(cstep, nmonte) == 0 .and. btest(issus, 3) ) then
+                 call ctqmc_record_sfom()
+             endif ! back if ( mod(cstep, nmonte) == 0 .and. btest(issus, 3) ) block
+
+! record the orbital-orbital correlation function
+             if ( mod(cstep, nmonte) == 0 .and. btest(issus, 4) ) then
+                 call ctqmc_record_ofom()
+             endif ! back if ( mod(cstep, nmonte) == 0 .and. btest(issus, 4) ) block
+
+! record the fidelity susceptibility
+             if ( mod(cstep, nmonte) == 0 .and. btest(issus, 5) ) then
+                 call ctqmc_record_lmat()
+             endif ! back if ( mod(cstep, nmonte) == 0 .and. btest(issus, 5) ) block
+
 ! record nothing
              if ( mod(cstep, nmonte) == 0 .and. btest(isvrt, 0) ) then
                  CONTINUE
@@ -499,13 +556,22 @@
 ! collect the double occupation matrix data from nnmat to nnmat_mpi
      call ctqmc_reduce_nmat(nmat_mpi, nnmat_mpi)
 
+! collect the fidelity susceptibility data from lmat to lmat_mpi
+! collect the fidelity susceptibility data from rmat to rmat_mpi
+! collect the fidelity susceptibility data from lrmat to lrmat_mpi
+     call ctqmc_reduce_lmat(lmat_mpi, rmat_mpi, lrmat_mpi)
+
 ! collect the spin-spin correlation function data from schi to schi_mpi
 ! collect the spin-spin correlation function data from sschi to sschi_mpi
+! collect the spin-spin correlation function data from ssfom to ssfom_mpi
      call ctqmc_reduce_schi(schi_mpi, sschi_mpi)
+     call ctqmc_reduce_sfom(          ssfom_mpi)
 
 ! collect the orbital-orbital correlation function data from ochi to ochi_mpi
 ! collect the orbital-orbital correlation function data from oochi to oochi_mpi
+! collect the orbital-orbital correlation function data from oofom to oofom_mpi
      call ctqmc_reduce_ochi(ochi_mpi, oochi_mpi)
+     call ctqmc_reduce_ofom(          oofom_mpi)
 
 ! collect the two-particle green's function from g2_re to g2_re_mpi
 ! collect the two-particle green's function from g2_im to g2_im_mpi
@@ -534,10 +600,15 @@
 
      nmat  = nmat_mpi  * real(nmonte) / real(nsweep)
      nnmat = nnmat_mpi * real(nmonte) / real(nsweep)
+     lmat  = lmat_mpi  * real(nmonte) / real(nsweep)
+     rmat  = rmat_mpi  * real(nmonte) / real(nsweep)
+     lrmat = lrmat_mpi * real(nmonte) / real(nsweep)
      schi  = schi_mpi  * real(nmonte) / real(nsweep)
      sschi = sschi_mpi * real(nmonte) / real(nsweep)
+     ssfom = ssfom_mpi * real(nmonte) / real(nsweep)
      ochi  = ochi_mpi  * real(nmonte) / real(nsweep)
      oochi = oochi_mpi * real(nmonte) / real(nsweep)
+     oofom = oofom_mpi * real(nmonte) / real(nsweep)
 
      g2_re = g2_re_mpi * real(nmonte) / real(nsweep)
      g2_im = g2_im_mpi * real(nmonte) / real(nsweep)
@@ -608,14 +679,21 @@
          call ctqmc_dump_nmat(nmat, nnmat)
      endif ! back if ( myid == master ) block
 
-! write out the final spin-spin correlation function data, schi and sschi
+! write out the final fidelity susceptibility data, lmat, rmat, and lrmat
      if ( myid == master ) then ! only master node can do it
-         call ctqmc_dump_schi(schi, sschi)
+         call ctqmc_dump_lmat(lmat, rmat, lrmat)
      endif ! back if ( myid == master ) block
 
-! write out the final orbital-orbital correlation function data, ochi and oochi
+! write out the final spin-spin correlation function data, schi, sschi, and ssfom
+     if ( myid == master ) then ! only master node can do it
+         call ctqmc_dump_schi(schi, sschi)
+         call ctqmc_dump_sfom(      ssfom)
+     endif ! back if ( myid == master ) block
+
+! write out the final orbital-orbital correlation function data, ochi, oochi, and oofom
      if ( myid == master ) then ! only master node can do it
          call ctqmc_dump_ochi(ochi, oochi)
+         call ctqmc_dump_ofom(      oofom)
      endif ! back if ( myid == master ) block
 
 ! write out the final two-particle green's function data, g2_re and g2_im
@@ -672,10 +750,15 @@
      deallocate(prob_mpi )
      deallocate(nmat_mpi )
      deallocate(nnmat_mpi)
+     deallocate(lmat_mpi )
+     deallocate(rmat_mpi )
+     deallocate(lrmat_mpi)
      deallocate(schi_mpi )
      deallocate(sschi_mpi)
+     deallocate(ssfom_mpi)
      deallocate(ochi_mpi )
      deallocate(oochi_mpi)
+     deallocate(oofom_mpi)
      deallocate(g2_re_mpi)
      deallocate(g2_im_mpi)
      deallocate(h2_re_mpi)
