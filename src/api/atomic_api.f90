5c8f9c74d5bfaf60d25ee0289017cfb9dfcac4e3
Li Huang <huangli712@gmail.com>
2015-08-21 00:12:10 +0800

    modify api/atomic_api.f90, clean the unused codes

--- src/api/atomic_api.f90
+++ src/api/atomic_api.f90
@@ -1,197 +1,17 @@
 !!!-----------------------------------------------------------------------
-!!! project : lilac
+!!! project : CAPI (Common Application Programming Interface)
 !!! program : japi
 !!! source  : atomic_api.f90
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
-!!! history : 10/29/2014 by li huang
+!!! history : 10/29/2014 by li huang (created)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : the purpose of this module is to define a generic and robust
 !!!           application programming interface (API) for atomic eigenvalue
 !!!           problem solver
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
-
-!!
-!!
-!! Introduction
-!! ============
-!!
-!! This module can provide a light weight interface (i.e., application
-!! programming interface, API) for Fortran/Python language to the atomic
-!! eigenvalue problem solver. The user can use it to access the jasmine
-!! code.
-!!
-!! How to build the Fortran API
-!! ============================
-!!
-!! 1. edit src/build/make.sys
-!! --------------------------
-!!
-!! Activate the API macro (keep MPY macro disable).
-!!
-!! 2. compile api
-!! --------------
-!!
-!! Please compile api (this directory) again. You can use the 'make api'
-!! command in the src/build directory, or use the 'make' command in the
-!! src/api directory.
-!!
-!! 3. compile the jasmine component
-!! --------------------------------
-!!
-!! Please compile the desired jasmine component again. You have to clean it
-!! at first, and then compile it. Noted that you have to compile it in the
-!! library mode, i.e., you must use 'make lib' (in the src/tools/jasmine
-!! directory) or 'make jasmine-lib' (in the src/build directory), etc.
-!!
-!! 4. get what you need
-!! --------------------
-!!
-!! If everything is OK, you will find the libatomic.a file in the jasmine
-!! component folder (for example, src/tools/jasmine directory). Please copy
-!! it (together with src/api/japi.mod) to your own directory. That's all.
-!!
-!! How to build the Python API
-!! ===========================
-!!
-!! 1. edit src/build/make.sys
-!! --------------------------
-!!
-!! Activate the API macro and MPY macro at the same time.
-!!
-!! 2. compile api
-!! --------------
-!!
-!! Please compile api (this directory) again. You can use the 'make api'
-!! command in the src/build directory, or use the 'make' command in the
-!! src/api directory.
-!!
-!! 3. compile the jasmine component
-!! --------------------------------
-!!
-!! Please compile the desired jasmine component again. You have to clean it
-!! at first, and then compile it. Noted that you have to compile it in the
-!! library mode, i.e., you must use 'make lib' (in the src/tools/jasmine
-!! directory) or 'make jasmine-lib' (in the src/build directory), etc.
-!!
-!! 4. generate pyjasmine.so
-!! ------------------------
-!!
-!! In the src/api directory, just input 'make pyjasmine' command and wait.
-!! At last you will get the pyjasmine.so which is what you need.
-!!
-!! Usage (Fortran version)
-!! =======================
-!!
-!! In the following, we will use an example to show you how to use api to
-!! control the jasmine code. When you want to compile your code, you have
-!! to ensure that japi.mod and libatomic.a are in correct PATH. Or else
-!! the compiler will complain that it can not find them.
-!!
-!! 1. import api support
-!! ---------------------
-!!
-!! use japi
-!!
-!! 2. create T_jasmine
-!! -------------------
-!!
-!! type (T_jasmine) :: I_solver ! define I_solver
-!! ...
-!! I_solver%ibasis = 1  ! setup I_solver
-!! I_solver%ictqmc = 1
-!! ...
-!! I_solver%Uc     = 4.0
-!! I_solver%Uv     = 4.0
-!! I_solver%Jz     = 0.0
-!! I_solver%Js     = 0.0
-!! I_solver%Jp     = 0.0
-!! ...
-!!
-!! Note: Every parameter for the atomic eigenvalue problem solver must be
-!! initialized here.
-!!
-!! 3. init the atomic eigenvalue problem solver
-!! --------------------------------------------
-!!
-!! call init_atomic(I_solver)
-!!
-!! 4. start the atomic eigenvalue problem solver
-!! ---------------------------------------------
-!!
-!! call exec_atomic()
-!!
-!! 5. close the atomic eigenvalue problem solver
-!! ---------------------------------------------
-!!
-!! call stop_atomic()
-!!
-!! 6. access the computational results
-!! -----------------------------------
-!!
-!! You have to write your fortran codes manually to access the results.
-!!
-!! Usage (Python version)
-!! ======================
-!!
-!! In the following, we will use an example to show you how to use api to
-!! control the jasmine code. When you want to run your Python code, you
-!! have to ensure that pyjasmine.so is in correct PATH. Or else the Python
-!! will complain that it can not find iqist.
-!!
-!! 1. import pyjasmine
-!! -------------------
-!!
-!! from pyjasmine import japi as atomic
-!!
-!! You have to ensure that the pyjasmine package is in the sys.path. For
-!! example, you can use the following code to modify sys.path
-!!
-!! sys.path.append('../../src/api/')
-!!
-!! 2. configure the atomic eigenvalue problem solver
-!! -------------------------------------------------
-!!
-!! You have to setup the key parameters for the atomic eigenvalue problem
-!! solver, and write them down to the 'atom.config.in' file. Now you can
-!! do that manually. On the other hand, we provide a useful Python module
-!! to facilitate this work (see src/tools/hibiscus/script/u_atomic.py).
-!!
-!! 3. init the atomic eigenvalue problem solver
-!! --------------------------------------------
-!!
-!! atomic.init_atomic() # there is no parameter for init_atomic()
-!!
-!! 4. start the atomic eigenvalue problem solver
-!! ---------------------------------------------
-!!
-!! atomic.exec_atomic()
-!!
-!! 5. close the atomic eigenvalue problem solver
-!! ---------------------------------------------
-!!
-!! atomic.stop_atomic()
-!!
-!! 6. access the computational results
-!! -----------------------------------
-!!
-!! You have to write your own Python codes to access the results.
-!!
-!! Examples
-!! ========
-!!
-!! Fortran version
-!! ---------------
-!!
-!! N/A.
-!!
-!! Python version
-!! --------------
-!!
-!! see iqist/tutor/t964/template.py.
-!!
-!!
 
   module japi
      implicit none
@@ -206,11 +26,6 @@
 !!========================================================================
 !!>>> declare global data structure                                    <<<
 !!========================================================================
-
-! note: now f2py does not support derived types, so we have to comment
-! out them when f2py is used.
-
-# if !defined (MPY)
 
 ! define type T_jasmine, which is used to describe the control parameters
 ! for the jasmine code
@@ -240,65 +55,5 @@
          real(dp) :: mune
          real(dp) :: lambda
      end type T_jasmine
-
-# endif  /* MPY */
-
-!!========================================================================
-!!>>> declare accessibility for module routines                        <<<
-!!========================================================================
-
-     public :: init_atomic
-     public :: exec_atomic
-     public :: stop_atomic
-
-  contains ! encapsulated functionality
-
-# if !defined (MPY)
-
-!!>>> init_atomic: initialize the atomic eigenvalue problem solver
-!!>>> fortran version
-  subroutine init_atomic(I_solver)
-     implicit none
-
-! external arguments
-! type structure of generic atomic eigenvalue problem solver
-     class(*), intent(in) :: I_solver
-
-     call cat_init_atomic(I_solver)
-
-     return
-  end subroutine init_atomic
-
-# else   /* MPY */
-
-!!>>> init_atomic: initialize the atomic eigenvalue problem solver
-!!>>> python version
-  subroutine init_atomic()
-     implicit none
-
-     call cat_init_atomic()
-
-     return
-  end subroutine init_atomic
-
-# endif  /* MPY */
-
-!!>>> exec_atomic: execute the atomic eigenvalue problem solver
-  subroutine exec_atomic()
-     implicit none
-
-     call cat_exec_atomic()
-
-     return
-  end subroutine exec_atomic
-
-!!>>> stop_atomic: stop the atomic eigenvalue problem solver
-  subroutine stop_atomic()
-     implicit none
-
-     call cat_stop_atomic()
-
-     return
-  end subroutine stop_atomic
 
   end module japi
