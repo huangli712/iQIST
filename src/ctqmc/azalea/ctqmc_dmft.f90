598d3ba28ee556dd8a8b548960d90f529bffad09
Li Huang <huangli712@gmail.com>
2015-08-30 11:48:31 +0800

    azalea/ctqmc_dmft.f90, update code style

--- src/ctqmc/azalea/ctqmc_dmft.f90
+++ src/ctqmc/azalea/ctqmc_dmft.f90
@@ -8,7 +8,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 09/16/2009 by li huang (created)
-!!!           08/09/2014 by li huang (last modified)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : the self-consistent engine for dynamical mean field theory
 !!!           (DMFT) simulation. it is only suitable for hybridization
 !!!           expansion version continuous time quantum Monte Carlo (CTQMC)
