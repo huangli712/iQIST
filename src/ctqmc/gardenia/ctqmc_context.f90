b1886352ae2e3e238f983a126a20b21f2ee8111c
Li Huang <huangli712@gmail.com>
2015-08-30 11:45:43 +0800

    gardenia/ctqmc_context.f90, update code style

--- src/ctqmc/gardenia/ctqmc_context.f90
+++ src/ctqmc/gardenia/ctqmc_context.f90
@@ -13,9 +13,8 @@
 !!! source  : ctqmc_context.f90
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
-!!! history : 09/16/2009 by li huang
-!!!           06/08/2010 by li huang
-!!!           09/07/2014 by li huang
+!!! history : 09/16/2009 by li huang (created)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : To define the key data structure and global arrays/variables
 !!!           for hybridization expansion version continuous time quantum
 !!!           Monte Carlo (CTQMC) quantum impurity solver and dynamical
