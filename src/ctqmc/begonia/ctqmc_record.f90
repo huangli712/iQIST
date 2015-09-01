1dec111367d33f0f201aed81ac82dced4970f1d9
Li Huang <huangli712@gmail.com>
2015-09-01 18:36:33 +0800

    begonia/ctqmc_record.f90, fix ctqmc_reduce_hist()

--- src/ctqmc/begonia/ctqmc_record.f90
+++ src/ctqmc/begonia/ctqmc_record.f90
@@ -475,9 +475,10 @@
   end subroutine ctqmc_reduce_grnf
 
 !!>>> ctqmc_reduce_hist: reduce the hist from all children processes
-  subroutine ctqmc_reduce_hist(hist_mpi)
+  subroutine ctqmc_reduce_hist(hist_mpi, hist_err)
      use constants, only : dp, zero
      use mmpi, only : mp_allreduce, mp_barrier
+     use mmpi, only : mpi_max
 
      use control, only : mkink
      use control, only : nprocs
@@ -488,9 +489,11 @@
 ! external arguments
 ! histogram for perturbation expansion series
      real(dp), intent(out) :: hist_mpi(mkink)
+     real(dp), intent(out) :: hist_err(mkink)
 
-! initialize hist_mpi
+! initialize hist_mpi and hist_err
      hist_mpi = zero
+     hist_err = zero
 
 ! build hist_mpi, collect data from all children processes
 # if defined (MPI)
@@ -509,6 +512,17 @@
 
 ! calculate the average
      hist_mpi = hist_mpi / real(nprocs)
+
+! build hist_err, collect data from all children processes
+# if defined (MPI)
+
+! collect data
+     call mp_allreduce(abs(hist - hist_mpi), hist_err, mpi_max)
+
+! block until all processes have reached here
+     call mp_barrier()
+
+# endif /* MPI */
 
      return
   end subroutine ctqmc_reduce_hist
