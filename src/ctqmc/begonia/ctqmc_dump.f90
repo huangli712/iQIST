a0d65150ada7a0fe0622fdca2986c139a839bba1
Li Huang <huangli712@gmail.com>
2015-09-01 17:58:17 +0800

    begonia/ctqmc_dump.f90, fix ctqmc_dump_wssf()

--- src/ctqmc/begonia/ctqmc_dump.f90
+++ src/ctqmc/begonia/ctqmc_dump.f90
@@ -207,9 +207,9 @@
 !!>>> ctqmc_dump_wssf: write out bath weiss's function in matsubara
 !!>>> frequency space
   subroutine ctqmc_dump_wssf(rmesh, wssf)
-     use constants, only : dp, mytmp
+     use constants, only : dp, zero, mytmp
 
-     use control, only : nband, norbs
+     use control, only : norbs
      use control, only : mfreq
 
      implicit none
@@ -230,17 +230,15 @@
      open(mytmp, file='solver.wss.dat', form='formatted', status='unknown')
 
 ! write it
-     do i=1,nband
+     do i=1,norbs
          do j=1,mfreq
              write(mytmp,'(i6,5f16.8)') i, rmesh(j), &
-                                  real(wssf(j,i,i)), &
-                                 aimag(wssf(j,i,i)), &
-                      real(wssf(j,i+nband,i+nband)), &
-                     aimag(wssf(j,i+nband,i+nband))
+              real(wssf(j,i,i)), aimag(wssf(j,i,i)), &
+                                         zero, zero
          enddo ! over j={1,mfreq} loop
          write(mytmp,*) ! write empty lines
          write(mytmp,*)
-     enddo ! over i={1,nband} loop
+     enddo ! over i={1,norbs} loop
 
 ! close data file
      close(mytmp)
