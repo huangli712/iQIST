0fe1faf0cd83a30ef0bb6cf48a75574d742fa543
Li Huang <huangli712@gmail.com>
2015-09-02 22:55:16 +0800

    manjushaka/ctqmc_update.f90, improve the code style

--- src/ctqmc/manjushaka/ctqmc_update.f90
+++ src/ctqmc/manjushaka/ctqmc_update.f90
@@ -21,9 +21,8 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!!           yilin wang (email:qhwyl2006@126.com)
-!!! history : 09/16/2009 by li huang
-!!!           06/09/2010 by li huang
-!!!           11/11/2014 by yilin wang
+!!! history : 09/16/2009 by li huang (created)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : provide basic infrastructure (elementary updating subroutines)
 !!!           for hybridization expansion version continuous time quantum
 !!!           Monte Carlo (CTQMC) quantum impurity solver.
