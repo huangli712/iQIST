ca874ac66e6ebebaae34dded90067b733e22e040
Li Huang <huangli712@gmail.com>
2015-09-02 11:35:05 +0800

    jasmine/atomic_full.f90, improve the code style

--- src/tools/jasmine/atomic_full.f90
+++ src/tools/jasmine/atomic_full.f90
@@ -7,9 +7,8 @@
 !!! source  : atomic_full.f90
 !!! type    : subroutines
 !!! author  : yilin wang (email:qhwyl2006@126.com)
-!!! history : 07/09/2014 by yilin wang
-!!!           08/22/2014 by yilin wang
-!!!           10/27/2014 by li huang
+!!! history : 07/09/2014 by yilin wang (created)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : computational subroutines for the calculations of occupation
 !!!           number, spin moment, F-matrix, and atomic Hamiltonian in the
 !!!           full Fock space.
