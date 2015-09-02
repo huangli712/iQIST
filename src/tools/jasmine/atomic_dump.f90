70e2e0c1193d0580cc012c82dd737cc6b2246725
Li Huang <huangli712@gmail.com>
2015-09-02 11:27:22 +0800

    jasmine/atomic_dump.f90, improve the code style

--- src/tools/jasmine/atomic_dump.f90
+++ src/tools/jasmine/atomic_dump.f90
@@ -14,9 +14,8 @@
 !!! source  : atomic_dump.f90
 !!! type    : subroutines
 !!! author  : yilin wang (email:qhwyl2006@126.com)
-!!! history : 07/09/2014 by yilin wang
-!!!           08/22/2014 by yilin wang
-!!!           10/20/2014 by li huang
+!!! history : 07/09/2014 by yilin wang (created)
+!!!           08/17/2015 by li huang (last modified)
 !!! purpose : write output files
 !!! status  : unstable
 !!! comment :
