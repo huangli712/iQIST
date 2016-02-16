b75303d96e85983c8c1791ab4f92eecf80f04054
Li Huang <lihuang.dmft@gmail.com>
2016-02-16 12:36:35 +0800

    modify gardenia/ctqmc_main.f90, update the date time, use ctqmc_print_it_info()

--- src/ctqmc/gardenia/ctqmc_main.f90
+++ src/ctqmc/gardenia/ctqmc_main.f90
@@ -5,7 +5,7 @@
 !!! engine plus hybridization expansion version continuous time quantum  !
 !!! Monte Carlo (CTQMC) quantum impurity solver                          !
 !!! author  : Li Huang (at IOP/CAS & SPCLab/CAEP & UNIFR)                !
-!!! version : v2015.01.06T                                               !
+!!! version : v2016.02.13T                                               !
 !!! status  : WARNING: IN TESTING STAGE, USE IT IN YOUR RISK             !
 !!! comment : this impurity solver is based on segment picture formalism !
 !!!           any question, please contact with lihuang.dmft@gmail.com   !
@@ -16,7 +16,7 @@
 !! WARNING
 !! =======
 !!
-!! If you want to obtain an executable program, please go to src/build/,
+!! If you want to obtain an executable program, please go to iqist/build/,
 !! type 'make gardenia' command in the terminal. On the contrary, if you
 !! want to compile gardenia as a library, please use 'make gardenia-lib'.
 !! If you want to obtain a python module, please use 'make gardenia-pylib'.
@@ -110,12 +110,11 @@
 !! Documents
 !! =========
 !!
-!! For more details, please go to iqist/doc/manual directory.
+!! For more details, please see the on line reference manual.
 !!
 !!
 
   program ctqmc_main
-     use constants, only : mystd
      use mmpi, only : mp_init, mp_finalize
      use mmpi, only : mp_comm_rank, mp_comm_size
      use mmpi, only : mp_barrier
@@ -184,7 +183,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'GARDENIA >>> DMFT iter:', iter, ' <<< SELFING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! call the continuous time quantum Monte Carlo quantum impurity solver, to
@@ -197,7 +196,8 @@
 !-------------------------------------------------------------------------
 ! it is suitable for lattice model hamiltonian plus dynamical mean field
 ! theory calculation
-     DMFT_CTQMC_ITERATION: do iter=1,niter
+     DMFT_CTQMC_ITERATION: &
+     do iter=1,niter
 
 ! check the running mode
          if ( isscf == 1 ) then
@@ -206,7 +206,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'GARDENIA >>> DMFT iter:', iter, ' <<< SELFING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! call the continuous time quantum Monte Carlo quantum impurity solver, to
@@ -238,7 +238,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'GARDENIA >>> DMFT iter:', iter, ' <<< BINNING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! accumulate the quantum Monte Carlo data
