b3b682d0264d8d1a9b5a660c87547f8f9c7070b5
Li Huang <lihuang.dmft@gmail.com>
2016-02-18 00:06:18 +0800

    modify lavender/ctqmc_main.f90, use ctqmc_print_it_info()

--- src/ctqmc/lavender/ctqmc_main.f90
+++ src/ctqmc/lavender/ctqmc_main.f90
@@ -5,7 +5,7 @@
 !!! engine plus hybridization expansion version continuous time quantum  !
 !!! Monte Carlo (CTQMC) quantum impurity solver                          !
 !!! author  : Li Huang (at IOP/CAS & SPCLab/CAEP & UNIFR)                !
-!!! version : v2015.01.06T                                               !
+!!! version : v2016.02.13T                                               !
 !!! status  : WARNING: IN TESTING STAGE, USE IT IN YOUR RISK             !
 !!! comment : this impurity solver is based on general matrix formalism  !
 !!!           any question, please contact with lihuang.dmft@gmail.com   !
@@ -16,7 +16,7 @@
 !! WARNING
 !! =======
 !!
-!! If you want to obtain an executable program, please go to src/build/,
+!! If you want to obtain an executable program, please go to iqist/build/,
 !! type 'make lavender' command in the terminal. On the contrary, if you
 !! want to compile lavender as a library, please use 'make lavender-lib'.
 !! If you want to obtain a python module, please use 'make lavender-pylib'.
@@ -27,7 +27,7 @@
 !! The lavender code is a hybridization expansion version continuous time
 !! quantum Monte Carlo quantum impurity solver. It adopts the general
 !! matrix formalism, and implements many useful and advanced features. So
-!! it is somewhat less efficient then the begonia code. It can be used as
+!! it is somewhat less efficient than the begonia code. It can be used as
 !! a standard to benchmark the other ctqmc impurity solvers. The current
 !! lavender code also includes a mini dynamical mean field theory engine
 !! which implements the self-consistent equation for Bethe lattice in
@@ -104,12 +104,11 @@
 !! Documents
 !! =========
 !!
-!! For more details, please go to iqist/doc/manual directory.
+!! For more details, please see the on line reference manual.
 !!
 !!
 
   program ctqmc_main
-     use constants, only : mystd
      use mmpi, only : mp_init, mp_finalize
      use mmpi, only : mp_comm_rank, mp_comm_size
      use mmpi, only : mp_barrier
@@ -178,7 +177,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'LAVENDER >>> DMFT iter:', iter, ' <<< SELFING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! call the continuous time quantum Monte Carlo quantum impurity solver, to
@@ -191,7 +190,8 @@
 !-------------------------------------------------------------------------
 ! it is suitable for lattice model hamiltonian plus dynamical mean field
 ! theory calculation
-     DMFT_CTQMC_ITERATION: do iter=1,niter
+     DMFT_CTQMC_ITERATION: &
+     do iter=1,niter
 
 ! check the running mode
          if ( isscf == 1 ) then
@@ -200,7 +200,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'LAVENDER >>> DMFT iter:', iter, ' <<< SELFING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! call the continuous time quantum Monte Carlo quantum impurity solver, to
@@ -232,7 +232,7 @@
 
 ! write the iter to screen
          if ( myid == master ) then ! only master node can do it
-             write(mystd,'(2X,a,i3,a)') 'LAVENDER >>> DMFT iter:', iter, ' <<< BINNING'
+             call ctqmc_print_it_info(iter)
          endif ! back if ( myid == master ) block
 
 ! accumulate the quantum Monte Carlo data
