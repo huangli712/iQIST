9b1ec2d7bf8b0a714e010a630ab65649e1b03c64
Li Huang <lihuang.dmft@gmail.com>
2016-02-17 23:05:36 +0800

    modify begonia/ctqmc_print.f90, use cname to replace BEGONIA

--- src/ctqmc/begonia/ctqmc_print.f90
+++ src/ctqmc/begonia/ctqmc_print.f90
@@ -4,6 +4,7 @@
 !!!           ctqmc_print_footer
 !!!           ctqmc_print_summary
 !!!           ctqmc_print_runtime
+!!!           ctqmc_print_it_info
 !!! source  : ctqmc_print.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
@@ -23,6 +24,7 @@
   subroutine ctqmc_print_header()
      use constants, only : mystd
 
+     use control, only : cname
      use control, only : nprocs
 
      implicit none
@@ -33,25 +35,25 @@
 ! obtain current date and time
      call s_time_builder(date_time_string)
 
-     write(mystd,'(2X,a)') 'BEGONIA'
+     write(mystd,'(2X,a)') cname
      write(mystd,'(2X,a)') '>>> A DMFT Engine With Continuous Time Quantum Monte Carlo Impurity Solver'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'Version: 2015.01.06T '//'(built at '//__TIME__//" "//__DATE__//')'
+     write(mystd,'(2X,a)') 'Version: 2016.02.13T '//'(built at '//__TIME__//" "//__DATE__//')'
      write(mystd,'(2X,a)') 'Develop: by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
      write(mystd,'(2X,a)') 'Support: lihuang.dmft@gmail.com'
      write(mystd,'(2X,a)') 'License: GNU General Public License version 3'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'BEGONIA >>> start running at '//date_time_string
+     write(mystd,'(2X,a)') cname//' >>> start running at '//date_time_string
 
 # if defined (MPI)
 
-     write(mystd,'(2X,a,i4)') 'BEGONIA >>> parallelism: Yes >>> processors:', nprocs
+     write(mystd,'(2X,a,i4)') cname//' >>> parallelism: Yes >>> processors:', nprocs
 
 # else   /* MPI */
 
-     write(mystd,'(2X,a,i4)') 'BEGONIA >>> parallelism: No  >>> processors:', 1
+     write(mystd,'(2X,a,i4)') cname//' >>> parallelism: No  >>> processors:', 1
 
 # endif  /* MPI */
 
@@ -65,6 +67,8 @@
 !!>>> theory self-consistent engine
   subroutine ctqmc_print_footer()
      use constants, only : dp, mystd
+
+     use control, only : cname
 
      implicit none
 
@@ -80,11 +84,11 @@
 ! obtain current date and time
      call s_time_builder(date_time_string)
 
-     write(mystd,'(2X,a,f10.2,a)') 'BEGONIA >>> total time spent:', tot_time, 's'
+     write(mystd,'(2X,a,f10.2,a)') cname//' >>> total time spent:', tot_time, 's'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'BEGONIA >>> I am tired and want to go to bed. Bye!'
-     write(mystd,'(2X,a)') 'BEGONIA >>> happy ending at '//date_time_string
+     write(mystd,'(2X,a)') cname//' >>> I am tired and want to go to bed. Bye!'
+     write(mystd,'(2X,a)') cname//' >>> happy ending at '//date_time_string
 
      return
   end subroutine ctqmc_print_footer
@@ -97,7 +101,7 @@
 
      implicit none
 
-     write(mystd,'(2X,a)') 'BEGONIA >>> parameters list:'
+     write(mystd,'(2X,a)') cname//' >>> parameters list:'
 
      write(mystd,'(2(4X,a,i10))')   'isscf :', isscf  , 'isbin :', isbin
      write(mystd,'(2(4X,a,i10))')   'issun :', issun  , 'isspn :', isspn
@@ -129,6 +133,7 @@
   subroutine ctqmc_print_runtime(iter, cstep)
      use constants, only : dp, one, half, mystd
 
+     use control, only : cname
      use control, only : nsweep, nmonte
      use context, only : cnegs, caves
      use context, only : insert_tcount, insert_accept, insert_reject
@@ -152,7 +157,7 @@
      real(dp) :: raux
 
 ! about iteration number
-     write(mystd,'(2X,a,i3,2(a,i10))') 'BEGONIA >>> iter:', iter, ' sweep:', cstep, ' of ', nsweep
+     write(mystd,'(2X,a,i3,2(a,i10))') cname//' >>> iter:', iter, ' sweep:', cstep, ' of ', nsweep
 
 ! about auxiliary physical observables
      raux = real(caves) / nmonte
@@ -160,8 +165,8 @@
      write(mystd,'(2(4X,a,f10.5))') 'etot :', paux(1) / raux, 'epot :', paux(2) / raux
      write(mystd,'(2(4X,a,f10.5))') 'ekin :', paux(3) / raux, '<Sz> :', paux(4) / raux
      write(mystd,'(2(4X,a,f10.5))') '<N1> :', paux(5) / raux, '<N2> :', paux(6) / raux
-     write(mystd,'(2(4X,a,f10.5))') '<K2> :', paux(7) / raux, '<K3> :', paux(8) / raux
-     write(mystd,'(1(4X,a,f10.5))') '<K4> :', paux(9) / raux
+     write(mystd,'(2(4X,a,e10.5))') '<K2> :', paux(7) / raux, '<K3> :', paux(8) / raux
+     write(mystd,'(1(4X,a,e10.5))') '<K4> :', paux(9) / raux
 
 ! about insert action
      if ( insert_tcount <= half ) insert_tcount = -one ! if insert is disable
