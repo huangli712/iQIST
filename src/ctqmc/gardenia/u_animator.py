b6df13cc18bdd57030d5ce495bcc307641283482
Li Huang <lihuang.dmft@gmail.com>
2016-03-08 16:39:40 +0800

    implement u_animator.py

--- src/ctqmc/gardenia/u_animator.py
+++ src/ctqmc/gardenia/u_animator.py
@@ -31,37 +31,51 @@
 
 import sys
 import numpy
+import matplotlib
+matplotlib.use("pdf") # setup backend
+import matplotlib.pyplot as plt
 
 num_iter = 20
 num_diag = 200
 num_band = 1
 num_orbs = 2
 max_pair = 100
+zero = 0.0
+beta = 8.00
 
 time_s = numpy.zeros((max_pair,num_orbs,num_diag,num_iter), dtype = numpy.float)
 time_e = numpy.zeros((max_pair,num_orbs,num_diag,num_iter), dtype = numpy.float)
+time_z = numpy.zeros((max_pair,num_orbs,num_diag,num_iter), dtype = numpy.float)
 rank_t = numpy.zeros((num_orbs,num_diag,num_iter), dtype = numpy.int)
 
 f = open('solver.diag.dat', 'r')
 for iter in range(num_iter):
     for diag in range(num_diag):
         line = f.readline()
-        print line
         line = f.readline()
-        print line
         for orbs in range(num_orbs):
             line = f.readline().split()
             rank_t[orbs,diag,iter] = int(line[4])
-            print line
             for pair in range( rank_t[orbs,diag,iter] ):
                 line = f.readline().split()
                 time_s[pair,orbs,diag,iter] = float(line[1]) 
                 time_e[pair,orbs,diag,iter] = float(line[2])
-                print line
         line = f.readline()
-        print line
         line = f.readline()
-        print line
         print iter, num_iter
-        sys.exit(-1)
 f.close()
+
+plt.text(zero-0.5, 0.0, r'0', fontsize = 18)
+plt.text(beta+0.5, 0.0, r'$\beta$', fontsize = 18)
+plt.gca().set_aspect(2.0)
+plt.gca().set_xticks([])
+plt.gca().set_yticks([])
+plt.plot([zero,zero],[-0.2,0.2], color = 'black', lw = 5)
+plt.plot([beta,beta],[-0.2,0.2], color = 'black', lw = 5)
+plt.plot([zero,beta],[-0.0,0.0], color = 'black', lw = 5)
+
+for orbs in range(num_orbs):
+    pair = rank_t[orbs,0,0]
+    plt.plot( time_s[0:pair,orbs,0,0], time_z[0:pair,orbs,0,0], marker = 'o' )
+    plt.plot( time_e[0:pair,orbs,0,0], time_z[0:pair,orbs,0,0], marker = 'o' )
+plt.savefig('test.pdf')
