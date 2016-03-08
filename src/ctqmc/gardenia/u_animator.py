b41bc96ac432a157a90d548355c2451751c54559
Li Huang <lihuang.dmft@gmail.com>
2016-03-08 15:38:39 +0800

    add u_animator.py

---
+++ src/ctqmc/gardenia/u_animator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+##
+##
+## Introduction
+## ============
+##
+## It is a python script. The purpose of this script is generate essential
+## input file (solver.ctqmc.in) for the quantum impurity solver components.
+## Note that you can not use it to control these codes.
+##
+## Usage
+## =====
+##
+## see the document string
+##
+## Author
+## ======
+##
+## This python script is designed, created, implemented, and maintained by
+##
+## Li Huang // email: lihuang.dmft@gmail.com
+##
+## History
+## =======
+##
+## 03/28/2015 by li huang (created)
+## 08/17/2015 by li huang (last modified)
+##
+##
+
+import sys
+import numpy
+
+f = open('solver.diag.dat', 'r')
+print 'ff'
+f.close()
