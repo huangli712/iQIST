67b317560244b218f58984f72f0a38733705fc19
Li Huang <lihuang.dmft@gmail.com>
2017-01-13 15:29:47 +0800

    update tutor directory

--- tutor/t63/template.py
+++ tutor/t63/template.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import numpy
 
 # import mpi support
 from mpi4py import MPI
@@ -9,44 +8,17 @@ from mpi4py import MPI
 # modify sys.path
 sys.path.append('../../src/tools/hibiscus/script/')
 
-# import the writer for hfqmc configuration file
-from u_hfqmc import *
+# import the writer for atomic configuration file
+from u_atomic import *
 
 # modify sys.path
-sys.path.append('../../src/hfqmc/daisy/')
+sys.path.append('../../src/tools/jasmine/')
 
-# import daisy software package
-from pydaisy import *
-
-def do_dmft_loop(mfreq, norbs, grnf):
-    """ implement the DMFT self-consistent condition for bethe lattice
-        t = 0.5, beta = 10.0, half-filling case
-    """
-    size_t = mfreq * norbs
-    rmesh = numpy.zeros(mfreq, dtype = numpy.float)
-    for i in range(mfreq):
-        rmesh[i] = (2 * i + 1.0) * numpy.pi / 10.0
-    grnf_t = numpy.reshape(grnf, (mfreq, norbs), order = 'F')
-    wssf_t = grnf_t * 0.0
-    for i in range(norbs):
-        for j in range(mfreq):
-            wssf_t[j,i] = 1.0 / (1j * rmesh[j] - 0.25 * grnf_t[j,i])
-    return numpy.reshape(wssf_t, size_t, order = 'F')
+# import jasmine software package
+from pyjasmine import *
 
 # get mpi communicator
 comm = MPI.COMM_WORLD
-
-# check the status of hfqmc impurity solver
-if cat_solver_id() == 901:
-    if comm.rank == 0 :
-        print "Hello world! This is the DAISY code."
-else:
-    if comm.rank == 0 :
-        print "Where is the DAISY code?"
-    sys.exit(-1)
-if cat_solver_status() != 1 :
-    print "I am sorry. This hfqmc impurity solver is not ready."
-    sys.exit(-1)
 
 # mpi barrier
 comm.Barrier()
@@ -54,15 +26,15 @@ comm.Barrier()
 # prepare the input file
 if comm.rank == 0:
     # create an instance
-    p = p_hfqmc_solver('daisy')
+    p = p_atomic_solver()
 
     # setup the parameters
-    p.setp(isscf = 1, isbin = 1, Uc = 4.0, mune = 2.0, beta = 10.0)
+    p.setp(nband = 2, ictqmc = 3, icu = 1, Uc = 4.0, Uv = 2.0, Jz = 1.0, Js = 1.0, Jp = 1.0)
 
     # verify the parameters
     p.check()
 
-    # generate the solver.hfqmc.in file
+    # generate the atom.config.in file
     p.write()
 
     # destroy the instance
@@ -71,34 +43,16 @@ if comm.rank == 0:
 # mpi barrier
 comm.Barrier()
 
-# setup parameters
-mfreq = 8193 # number of matsubara frequency points
-norbs = 2    # number of orbitals
-niter = 20   # number of iterations
-size_t = mfreq * norbs
+# only the master node can do this job
+if comm.rank == 0:
+    # init the atomic eigenvalue problem solver
+    cat_init_atomic()
 
-# allocate memory
-wssf = numpy.zeros(size_t, dtype = numpy.complex, order = 'F')
-grnf = numpy.zeros(size_t, dtype = numpy.complex, order = 'F')
-grnf_s = numpy.zeros(size_t, dtype = numpy.complex, order = 'F')
+    # execute the atomic eigenvalue problem solver
+    cat_exec_atomic()
 
-# init hfqmc impurity solver
-cat_init_hfqmc(comm.rank, comm.size)
-
-# try to implement the DMFT self-consistent loop
-for i in range(niter):
-    cat_exec_hfqmc(i+1)
-    grnf = cat_get_grnf(size_t)
-    wssf = do_dmft_loop(mfreq, norbs, grnf)
-    cat_set_wssf(size_t, wssf)
-    print 'MAX_ERROR:', (numpy.absolute(grnf - grnf_s)).max()
-    grnf_s = (grnf + grnf_s)/2.0
-
-# stop hfqmc impurity solver
-cat_stop_hfqmc()
+    # stop the atomic eigenvalue problem solver
+    cat_stop_atomic()
 
 # mpi barrier
 comm.Barrier()
-
-# deallocate memory
-del wssf, grnf, grnf_s
