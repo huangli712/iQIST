4d15389afb24d2dc97020c50585f98a4c0b08bb0
Li Huang <lihuang.dmft@gmail.com>
2017-01-17 23:49:58 +0800

    modify df_dump.f90, add df_dump_full_vert() etc subroutines

--- src/app/begonia/df_dump.f90
+++ src/app/begonia/df_dump.f90
@@ -1,21 +1,42 @@
 
   subroutine df_dump_dual_grnf()
+     implicit none
+
+     return
   end subroutine df_dump_dual_grnf
 
   subroutine df_dump_dual_sigf()
+     implicit none
+
+     return
   end subroutine df_dump_dual_sigf
 
   subroutine df_dump_latt_grnf()
+     implicit none
+
+     return
   end subroutine df_dump_latt_grnf
 
   subroutine df_dump_latt_sigf()
+     implicit none
+
+     return
   end subroutine df_dump_latt_sigf
 
   subroutine df_dump_spin_susc()
+     implicit none
+
+     return
   end subroutine df_dump_spin_susc
 
   subroutine df_dump_char_susc()
+     implicit none
+
+     return
   end subroutine df_dump_char_susc
 
   subroutine df_dump_full_vert()
+     implicit none
+
+     return
   end subroutine df_dump_full_vert
