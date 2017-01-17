98e99abd96cd83909b07c42ae7368bfe89192a41
Li Huang <lihuang.dmft@gmail.com>
2017-01-17 23:24:28 +0800

    modify df_stream.f90, add df_config() etc subroutines

--- src/app/begonia/df_stream.f90
+++ src/app/begonia/df_stream.f90
@@ -1,4 +1,19 @@
 
 
-  subroutine df_stream()
-  end subroutine df_stream
+  subroutine df_config()
+     implicit none
+
+     return
+  end subroutine df_config
+
+  subroutine df_setup_array()
+     implicit none
+
+     return
+  end subroutine df_setup_array
+
+  subroutine df_final_array()
+     implicit none
+
+     return
+  end subroutine df_final_array
