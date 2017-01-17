dd9ea8661f273085086cf3fbcf962e8084839c71
Li Huang <lihuang.dmft@gmail.com>
2017-01-18 00:01:59 +0800

    update df_core.f90 and df_diag.f90, add subroutines

--- src/app/begonia/df_core.f90
+++ src/app/begonia/df_core.f90
@@ -2,4 +2,20 @@
   subroutine df_run()
   end subroutine df_run
 
-  subroutine df_make_
+  subroutine cat_dual_wssf()
+  end subroutine cat_dual_wssf
+
+  subroutine cat_dual_grnf()
+  end subroutine cat_dual_grnf()
+
+  subroutine cat_dual_sigf()
+  end subroutine cat_dual_sigf
+
+  subroutine cat_latt_grnf()
+  end subroutine cat_latt_grnf
+
+  subroutine cat_latt_sigf()
+  end subroutine cat_latt_sigf
+
+  subroutine cat_full_vert()
+  end subroutine cat_full_vert
--- src/app/begonia/df_diag.f90
+++ src/app/begonia/df_diag.f90
@@ -1,4 +1,6 @@
 
-
   subroutine df_diagram()
   end subroutine df_diagram
+
+  subroutine cat_bubble()
+  subroutine cat_bubble
