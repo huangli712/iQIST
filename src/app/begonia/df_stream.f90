a6433ce19ff098098413e2cadf0f55f69e218ef5
Li Huang <lihuang.dmft@gmail.com>
2017-01-17 23:27:23 +0800

    modify df_stream.f90, add df_dmfvar_init() etc subroutines

--- src/app/begonia/df_stream.f90
+++ src/app/begonia/df_stream.f90
@@ -12,6 +12,12 @@
      return
   end subroutine df_setup_array
 
+  subroutine df_dmfvar_init()
+     implicit none
+
+     return
+  end subroutine df_dmfvar_init
+
   subroutine df_vertex_init()
      implicit none
 
