a557e8d2b235bc39694523545b8eeefebce168e4
Li Huang <lihuang.dmft@gmail.com>
2017-01-17 23:35:35 +0800

    modify df_print.f90, add df_print_summary() etc subroutines

--- src/app/begonia/df_print.f90
+++ src/app/begonia/df_print.f90
@@ -1,4 +1,16 @@
 
 
-  subroutine df_print()
-  end subroutine df_print
+  subroutine df_print_header()
+  end subroutine df_print_header
+
+  subroutine df_print_footer()
+  end subroutine df_print_footer
+
+  subroutine df_print_summary()
+  end subroutine df_print_summary
+
+  subroutine df_print_runtime()
+  end subroutine df_print_runtime
+
+  subroutine df_print_it_info()
+  end subroutine df_print_it_info
