c249455fe625cd63988edbc880b3b63c57316bdf
Li Huang <lihuang.dmft@gmail.com>
2017-01-18 16:18:38 +0800

    modify df_main.f90, add mpi support

--- src/app/begonia/df_main.f90
+++ src/app/begonia/df_main.f90
@@ -4,7 +4,34 @@
      use mmpi, only : mp_comm_rank, mp_comm_size
      use mmpi, only : mp_barrier
 
+     use df_control, only : nprocs, myid, master
+
      implicit none
+
+! initialize mpi envirnoment
+# if defined (MPI)
+
+! initialize the mpi execution environment
+     call mp_init()
+
+! determines the rank of the calling process in the communicator
+     call mp_comm_rank(myid)
+
+! determines the size of the group associated with a communicator
+     call mp_comm_size(nprocs)
+
+# endif  /* MPI */
+
+! finalize mpi envirnoment
+# if defined (MPI)
+
+! blocks until all processes have reached this routine
+     call mp_barrier()
+
+! terminates mpi execution environment
+     call mp_finalize()
+
+# endif  /* MPI */
 
      print *, 'Hello World!'
   end program df_main
