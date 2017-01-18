fbd3ef0777dc69346b6cd95a2df77524461cff36
Li Huang <lihuang.dmft@gmail.com>
2017-01-18 16:12:48 +0800

    modify df_context.f90, try to implement it, not finished

--- src/app/begonia/df_context.f90
+++ src/app/begonia/df_context.f90
@@ -1,3 +1,55 @@
 
   module df_context
+     use constants, only : dp
+
+     use df_control
+
+     integer, private :: istat
+
+!! dmft variables
+! dmft hybridization function
+     complex(dp), public, save, allocatable :: dmft_h(:,:)
+
+! dmft green's function
+     complex(dp), public, save, allocatable :: dmft_g(:,:)
+
+! dmft self-energy function
+     complex(dp), public, save, allocatable :: dmft_s(:,:)
+
+
+!! dual variables
+! dual green's function
+     complex(dp), public, save, allocatable :: dual_g(:,:)
+
+! dual self-energy function
+     complex(dp), public, save, allocatable :: dual_s(:,:)
+
+! dual bare green's function
+     complex(dp), public, save, allocatable :: dual_b(:,:)
+
+
+!! lattice variables
+! lattice green's function
+     complex(dp), public, save, allocatable :: latt_g(:,:)
+
+! lattice self-energy function
+     complex(dp), public, save, allocatable :: latt_s(:,:)
+
+     public :: df_allocate_memory
+     public :: df_deallocate_memory
+
+  contains
+
+  subroutine df_allocate_memory()
+     implicit none
+
+     return
+  end subroutine df_allocate_memory
+
+  subroutine df_deallocate_memory()
+     implicit none
+
+     return
+  end subroutine df_deallocate_memory
+
   end module df_context
--- src/app/begonia/df_control.f90
+++ src/app/begonia/df_control.f90
@@ -1,3 +1,73 @@
 
   module df_control
+     use constants, only : dp
+
+     implicit none
+
+!!========================================================================
+!!>>> character variables                                              <<<
+!!========================================================================
+
+! the code name of the current ctqmc impurity solver
+     character(len = 06), public, save :: cname = 'BEGONIA'
+
+!!========================================================================
+!!>>> integer variables                                                <<<
+!!========================================================================
+
+! number of correlated bands
+     integer, public, save :: nband  = 1
+
+! number of spin projection
+     integer, public, save :: nspin  = 2
+
+! number of correlated orbitals (= nband * nspin)
+     integer, public, save :: norbs  = 2
+
+! number of matsubara frequency for the two-particle green's function
+     integer, public, save :: nffrq  = 32
+
+! number of bosonic frequncy for the two-particle green's function
+     integer, public, save :: nbfrq  = 8
+
+!!========================================================================
+!!>>> real variables                                                   <<<
+!!========================================================================
+
+! chemical potential or fermi level
+!
+! note: it should/can be replaced with eimp
+     real(dp), public, save :: mune  = 2.00_dp
+
+! inversion of temperature
+     real(dp), public, save :: beta  = 8.00_dp
+
+! coupling parameter t for Hubbard model
+     real(dp), public, save :: part  = 0.50_dp
+
+! mixing parameter for dynamical mean field theory self-consistent engine
+     real(dp), public, save :: alpha = 0.70_dp
+
+!!========================================================================
+!!>>> MPI related common variables                                     <<<
+!!========================================================================
+
+! number of processors: default value 1
+     integer, public, save :: nprocs = 1
+
+! the id of current process: default value 0
+     integer, public, save :: myid   = 0
+
+! denote as the controller process: default value 0
+     integer, public, save :: master = 0
+
+! the id of current process in cartesian topology (cid == myid)
+     integer, public, save :: cid    = 0
+
+! the x coordinates of current process in cartesian topology
+     integer, public, save :: cx     = 0
+
+! the y coordinates of current process in cartesian topology
+     integer, public, save :: cy     = 0
+
   end module df_control
