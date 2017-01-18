2a6cfd8ec54f0c3f56a42ff7cc5aaf06b14417b6
Li Huang <lihuang.dmft@gmail.com>
2017-01-18 18:11:19 +0800

    modify df_dump.f90, add df_dump_magn_vert() etc subroutines

--- src/app/begonia/df_dump.f90
+++ src/app/begonia/df_dump.f90
@@ -11,6 +11,12 @@
      return
   end subroutine df_dump_dual_sigf
 
+  subroutine df_dump_dual_wssf()
+     implicit none
+
+     return
+  end subroutine df_dump_dual_wssf
+
   subroutine df_dump_latt_grnf()
      implicit none
 
@@ -34,6 +40,18 @@
 
      return
   end subroutine df_dump_char_susc
+
+  subroutine df_dump_dens_vert()
+     implicit none
+
+     return
+  end subroutine df_dump_dens_vert
+
+  subroutine df_dump_magn_vert()
+     implicit none
+
+     return
+  end subroutine df_dump_magn_vert
 
   subroutine df_dump_full_vert()
      implicit none
