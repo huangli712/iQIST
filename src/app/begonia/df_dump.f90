d6989309f9a9d16b4fd3c689412957a1c98fe284
Li Huang <lihuang.dmft@gmail.com>
2017-01-22 17:48:24 +0800

    modify df_dump.f90, try to implement df_dump_dmft_grnf()

--- src/app/begonia/df_dump.f90
+++ src/app/begonia/df_dump.f90
@@ -2,6 +2,43 @@
   subroutine df_dump_dmft_grnf()
      implicit none
 
+     use constants, only : dp, mytmp
+
+     use control, only : norbs
+     use control, only : mfreq
+
+     implicit none
+
+! external arguments
+! matsubara frequency mesh
+     real(dp), intent(in)    :: rmesh(mfreq)
+
+! impurity green's function
+     complex(dp), intent(in) :: grnf(mfreq,norbs,norbs)
+     complex(dp), intent(in) :: gerr(mfreq,norbs,norbs)
+
+! local variables
+! loop index
+     integer :: i
+     integer :: j
+
+! open data file: solver.grn.dat
+     open(mytmp, file='solver.grn.dat', form='formatted', status='unknown')
+
+! write it
+     do i=1,norbs
+         do j=1,mfreq
+             write(mytmp,'(i6,5f16.8)') i, rmesh(j), &
+              real(grnf(j,i,i)), aimag(grnf(j,i,i)), &
+              real(gerr(j,i,i)), aimag(gerr(j,i,i))
+         enddo ! over j={1,mfreq} loop
+         write(mytmp,*) ! write empty lines
+         write(mytmp,*)
+     enddo ! over i={1,norbs} loop
+
+! close data file
+     close(mytmp)
+
      return
   end subroutine df_dump_dmft_grnf
 
