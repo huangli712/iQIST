85fd76f5e03983af09cd2089b5a88e3acf0ef3e7
Li Huang <lihuang.dmft@gmail.com>
2017-01-22 17:40:15 +0800

    modify df_stream.f90, rename subroutine names

--- src/app/begonia/df_stream.f90
+++ src/app/begonia/df_stream.f90
@@ -1,5 +1,4 @@
 ! parse input files, readin data
-
   subroutine df_config()
      implicit none
 
@@ -7,34 +6,42 @@
   end subroutine df_config
 
   subroutine df_setup_array()
+     use df_context ! ALL
+
      implicit none
 
+! allocate memory for df_context module
+     call df_allocate_memory_dmft()
+     call df_allocate_memory_dual()
+     call df_allocate_memory_latt()
+     call df_allocate_memory_vert()
+     
      return
   end subroutine df_setup_array
 
-  subroutine df_mesher_init()
+  subroutine df_mesh_init()
      implicit none
 
      return
-  end subroutine df_mesher_init
+  end subroutine df_mesh_init
 
-  subroutine df_latter_init()
+  subroutine df_latt_init()
      implicit none
 
      return
-  end subroutine df_latter_init
+  end subroutine df_latt_init
 
-  subroutine df_dmfter_init()
+  subroutine df_dmft_init()
      implicit none
 
      return
-  end subroutine df_dmfter_init
+  end subroutine df_dmft_init
 
-  subroutine df_vertex_init()
+  subroutine df_vert_init()
      implicit none
 
      return
-  end subroutine df_vertex_init
+  end subroutine df_vert_init
 
   subroutine df_final_array()
      implicit none
