d0b338577bf1e60a9652ff64040fa33e6988cf1a
Li Huang <lihuang.dmft@gmail.com>
2017-01-26 22:57:10 +0800

    modify m_version.f90, try to implement it

--- src/base/m_version.f90
+++ src/base/m_version.f90
@@ -1,13 +1,12 @@
 !!!-----------------------------------------------------------------------
 !!! project : CSML (Common Service Modules Library)
-!!! program : stack
-!!! source  : m_stack.f90
+!!! program : version
+!!! source  : m_version.f90
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
-!!! history : 09/14/2009 by li huang (created)
-!!!           08/17/2015 by li huang (last modified)
-!!! purpose : the purpose of this module is to define a stack-type (LIFO)
-!!!           data structure in fortran version.
+!!! history : 01/26/2017 by li huang (created)
+!!!           01/26/2017 by li huang (last modified)
+!!! purpose : the purpose of this module is to define a version string.
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
@@ -17,34 +16,22 @@
 !! Introduction
 !! ============
 !!
-!! It is a common module which defines some common used numerical/physical
-!! constants. We always need it.
+!! It is a common module which defines the current version of iQIST.
 !!
 !! Usage
 !! =====
 !!
-!! 1. import completely
-!! --------------------
-!!
-!! use constants
+!! use version, only : __version__
 !! implicit none
 !!
-!! real(dp) :: A
-!! A = one
-!!
-!! 2. import partially
-!! -------------------
-!!
-!! use constants, only : dp, one
-!! implicit none
-!!
-!! real(dp) :: A
-!! A = one
+!! print *, __version__
 !!
 !!
 
   module version
      implicit none
 
+! version string
      character (len=20), public, parameter :: __version__ = 'v0.6.8 @ 2017.01.26D'
+
   end module version
