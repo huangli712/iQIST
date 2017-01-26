f6fddb38c442a4acc8cbca9ed0a936d240baafd8
Li Huang <lihuang.dmft@gmail.com>
2017-01-26 22:51:06 +0800

    modify m_version.f90, try to implement it

--- src/base/m_version.f90
+++ src/base/m_version.f90
@@ -1,4 +1,50 @@
+!!!-----------------------------------------------------------------------
+!!! project : CSML (Common Service Modules Library)
+!!! program : stack
+!!! source  : m_stack.f90
+!!! type    : module
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 09/14/2009 by li huang (created)
+!!!           08/17/2015 by li huang (last modified)
+!!! purpose : the purpose of this module is to define a stack-type (LIFO)
+!!!           data structure in fortran version.
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
 
+!!
+!!
+!! Introduction
+!! ============
+!!
+!! It is a common module which defines some common used numerical/physical
+!! constants. We always need it.
+!!
+!! Usage
+!! =====
+!!
+!! 1. import completely
+!! --------------------
+!!
+!! use constants
+!! implicit none
+!!
+!! real(dp) :: A
+!! A = one
+!!
+!! 2. import partially
+!! -------------------
+!!
+!! use constants, only : dp, one
+!! implicit none
+!!
+!! real(dp) :: A
+!! A = one
+!!
+!!
 
-  subroutine test()
-  end subroutine test
+  module version
+     implicit none
+
+     character, public, parameter :: CURR_VERSION
+  end module version
