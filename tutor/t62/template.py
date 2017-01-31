113795d21d8a912d47384abd40db3295991c71f1
Li Huang <lihuang.dmft@gmail.com>
2017-01-31 23:05:45 +0800

    update tutor files, azalea -> gardenia

--- tutor/t62/template.py
+++ tutor/t62/template.py
@@ -13,7 +13,7 @@ sys.path.append('../../src/tools/hibiscus/script/')
 from u_ctqmc import *
 
 # modify sys.path
-sys.path.append('../../src/ctqmc/azalea/')
+sys.path.append('../../src/ctqmc/gardenia/')
 
 # import iqist software package
 from pyiqist import *
@@ -24,10 +24,10 @@ comm = MPI.COMM_WORLD
 # check the status of ctqmc impurity solver
 if cat_solver_id() == 101:
     if comm.rank == 0 :
-        print "Hello world! This is the AZALEA code."
+        print "Hello world! This is the GARDENIA code."
 else:
     if comm.rank == 0 :
-        print "Where is the AZALEA code?"
+        print "Where is the GARDENIA code?"
     sys.exit(-1)
 if cat_solver_status() != 1 :
     print "I am sorry. This ctqmc impurity solver is not ready."
@@ -39,7 +39,7 @@ comm.Barrier()
 # prepare the input file
 if comm.rank == 0:
     # create an instance
-    p = p_ctqmc_solver('azalea')
+    p = p_ctqmc_solver('gardenia')
 
     # setup the parameters
     p.setp(isscf = 1, isbin = 1, U = 4.0, Uc = 4.0, Uv = 4.0, mune = 2.0, beta = 10.0)
