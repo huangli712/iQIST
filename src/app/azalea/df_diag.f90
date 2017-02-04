403532420622ba70a9941892c44b964c30346381
Li Huang <lihuang.dmft@gmail.com>
2017-02-04 11:50:32 +0800

    modify azalea/df_diag.f90, try to implement df_bubble()

--- src/app/azalea/df_diag.f90
+++ src/app/azalea/df_diag.f90
@@ -48,12 +48,60 @@
 ! local variables
      integer :: i
      integer :: j
+     integer :: k
+
+     real(dp) :: fw
 
      complex(dp) :: gk(nkpts)
-     complex(dp) :: gr(nkpts)
+     complex(dp) :: gr(nkpts), gr1(nkpts), gr2(nkpts)
+     complex(dp) :: gs(nkpts,nffrq,norbs)
 
-     print *, fmesh
-     print *, bmesh
+     do i=1,norbs
+         do j=1,nffrq
+             fw = fmesh(j) + w
+             if      ( fw > fmesh(nffrq) ) then
+                 gs(:,j,i) = czero
+             else if ( fw < fmesh(  1  ) ) then
+                 gs(:,j,i) = czero
+             else
+                 call cat_search_fmesh(fw, k)
+                 gs(:,j,i) = dual_g(:,k,i)
+             endif
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+
+     do i=1,norbs
+         do j=1,nffrq
+             gk = dual_g(:,j,i)
+             gr1 = czero
+             call df_fft2d(+1, nkp_x, nkp_y, gk, gr1) ! gk -> gr
+
+             gk = gs(:,j,i)
+             gr2 = czero
+             call df_fft2d(+1, nkp_x, nkp_y, gk, gr2) ! gk -> gr
+
+             gr = gr1 * gr2
+             call df_fft2d(-1, nkp_x, nkp_y, gr, gk) ! gr -> gk
+             bubble(:,j,i) = -gk
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+     bubble = bubble / real(nkpts * nkpts * beta)
+
      STOP
      return
   end subroutine df_bubble
+
+  subroutine cat_search_fmesh(w, ind)
+     use constants
+
+     use df_control
+
+     implicit none
+
+     integer, intent(out) :: ind
+     real(dp), intent(in) :: w
+
+     ind = ceiling( (w * beta / pi + nffrq + one) / two )
+
+     return
+  end subroutine cat_search_fmesh
