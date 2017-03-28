9145f6e8aff96c80fed63606b4a1da1fbf201f2b
Li Huang <lihuang.dmft@gmail.com>
2017-03-29 01:14:02 +0800

    clean working/tools/hibiscus

--- working/tools/hibiscus/entropy/entropy.md
+++
@@ -1,7 +0,0 @@
-# iQIST (Interacting Quantum Impurity Solver Toolkit)
-
-## HIBISCUS/entropy Test Cases
-
-### 2 band model
-
-* 1a // ntime = 129, nband = 2, norbs = 4, beta = 10.0, nwmax = 400, wstep = 0.040
--- working/tools/hibiscus/swing/2a/insulator.sh
+++
@@ -1,3 +0,0 @@
-exec=../../../../../src/tools/hibiscus/swing/swing_main.py
-para='-sig sig.in -nom 50 -beta 38.68333333 -wexp 1.05 -Ng 120 -FL False -L0 20 -b 0.3 -N0 500 -maxsteps 750000000'
-python $exec $para
