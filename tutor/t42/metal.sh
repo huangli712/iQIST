68e7e628ecb68b137302783cded3c9c0aabc35b7
Li Huang <lihuang.dmft@gmail.com>
2017-03-29 01:17:05 +0800

    remove tutor/t41 and t42

--- tutor/t42/metal.sh
+++
@@ -1,3 +0,0 @@
-exec=../../src/tools/hibiscus/swing/swing_main.py
-para='-sig std.sgm.dat -nom 128 -beta 40.0 -wexp 1.15 -Ng 60 -FL True -L0 20 -lcut 0.18 -b 0.9 -alpha4 1. -p0 0.1 -vunity 0.01 -maxsteps 750000000'
-python $exec $para
