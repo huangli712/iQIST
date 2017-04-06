6f9812bc0f23bc2cdd58ff28bb328e1f4879a9dd
Li Huang <lihuang.dmft@gmail.com>
2017-04-07 01:44:09 +0800

    update version strings

--- src/base/m_version.f90
+++ src/base/m_version.f90
@@ -5,7 +5,7 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 01/26/2017 by li huang (created)
-!!!           03/27/2017 by li huang (last modified)
+!!!           04/07/2017 by li huang (last modified)
 !!! purpose : the purpose of this module is to define a version string.
 !!! status  : unstable
 !!! comment :
@@ -32,10 +32,10 @@
      implicit none
 
 ! version string: version number + date info. + status info.
-     character(len=20), public, parameter :: FULL_VER = 'v0.6.8 @ 2017.01.31D'
+     character(len=20), public, parameter :: FULL_VER = 'v0.7.0 @ 2017.01.31D'
 
 ! version string: only version number
-     character(len=06), public, parameter :: CURR_VER = 'v0.6.8'
+     character(len=06), public, parameter :: CURR_VER = 'v0.7.0'
 
 ! version string: only date info.
      character(len=11), public, parameter :: DATE_VER = '2017.01.31'
