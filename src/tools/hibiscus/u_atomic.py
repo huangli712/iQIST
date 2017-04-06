d8ecc6aa27cd3f69e9e362b55043162175235f8d
Li Huang <lihuang.dmft@gmail.com>
2017-04-06 09:40:01 +0800

    add one word

--- src/tools/hibiscus/u_atomic.py
+++ src/tools/hibiscus/u_atomic.py
@@ -7,7 +7,7 @@
 ##
 ## It is a python script. The purpose of it is to generate essential input
 ## file (atom.config.in) for the jasmine code. Note that you can not use
-## it to control the jasmine code.
+## it to control the jasmine code directly.
 ##
 ## Usage
 ## =====
@@ -25,7 +25,7 @@
 ## =======
 ##
 ## 12/02/2014 by li huang (created)
-## 03/24/2017 by li huang (last modified)
+## 04/06/2017 by li huang (last modified)
 ##
 ##
 
