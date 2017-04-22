45d37d33c70e36b99ddba1bd410615a6393bc4b4
Li Huang <lihuang.dmft@gmail.com>
2017-04-22 18:42:57 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_control.f90
+++ src/ctqmc/narcissus/ctqmc_control.f90
@@ -226,21 +226,28 @@
 !! to judge:
 !!
 !! rule 1:
-!! isvrt is converted to a binary representation at first. for example,
-!! 10_10 is converted to 1010_2, 15_10 is converted to 1111_2, etc.
+!!     isvrt is firstly converted to a binary representation. for example,
+!!     10_10 is converted to 1010_2, 15_10 is converted to 1111_2, etc
 !!
 !! rule 2:
-!! then we examine the bits. if it is 1, then we do the calculation.
-! if it is 0, then we ignore the calculation. for example, we just use the
-! second bit (from right side to left side) to represent the calculation
-! of two-particle green's function. so, if isvrt is 10_10 (1010_2), we
-! will calculate the two-particle green's function. if isvrt is 13_10
-! (1101_2), we will not calculate it since the second bit is 0.
-!
-! the following are the definitions of bit representation:
-! if p == 1, do nothing
-! if p == 2, calculate two-particle green's function and vertex function
-! if p == 3, calculate particle-particle pair susceptibility
+!!     then we examine the bits one by one. if it is 1, then we try to do
+!!     the calculation. if it is 0, then we ignore the calculation. for
+!!     example, we just use the second bit (from right side to left side)
+!!     to represent the calculation of two-particle green's function. so,
+!!     if isvrt is 10_10 (1010_2), we will try to compute the two-particle
+!!     green's function. if isvrt is 13_10 (1101_2), we will not calculate
+!!     it since the second bit is 0
+!!
+!! the following are the definitions of bit representation:
+!!
+!! if p == 1:
+!!     do nothing
+!!
+!! if p == 2:
+!!     calculate two-particle green's function and vertex function
+!!
+!! if p == 3:
+!!     calculate particle-particle pair susceptibility
 !!
 !! example:
 !!   ( 1 1 1 0 1 0 1 0 1)_2
