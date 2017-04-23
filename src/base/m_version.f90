abcade326a1e9e350416b34daaef6dce41273c00
Li Huang <lihuang.dmft@gmail.com>
2017-04-24 00:52:33 +0800

    update the code style

--- src/base/m_version.f90
+++ src/base/m_version.f90
@@ -5,7 +5,7 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 01/26/2017 by li huang (created)
-!!!           04/17/2017 by li huang (last modified)
+!!!           04/24/2017 by li huang (last modified)
 !!! purpose : the purpose of this module is to define a version string.
 !!! status  : unstable
 !!! comment :
@@ -31,25 +31,53 @@
   module version
      implicit none
 
-! version string: version number + date info. + status info.
+!!
+!! @var FULL_VER
+!!
+!! version string, version number + date info. + status info.
+!!
      character(len=20), public, parameter :: FULL_VER = 'v0.7.0 @ 2017.01.31D'
 
-! version string: only version number
+!!
+!! @var CURR_VER
+!!
+!! version string, only version number
+!!
      character(len=06), public, parameter :: CURR_VER = 'v0.7.0'
 
-! version string: only date info.
+!!
+!! @var DATE_VER
+!!
+!! version string, only date info.
+!!
      character(len=11), public, parameter :: DATE_VER = '2017.01.31'
 
-! version string: only status info., D means devel, T testing, R released.
+!!
+!! @var STAT_VER
+!!
+!! version string, only status info., D means devel, T testing, R released.
+!!
      character(len=01), public, parameter :: STAT_VER = 'D'
 
-! version string: author info.
+!!
+!! @var AUTH_VER
+!!
+!! version string, author info.
+!!
      character(len=46), public, parameter :: AUTH_VER = 'by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
 
-! version string: email info.
+!!
+!! @var MAIL_VER
+!!
+!! version string, email info.
+!!
      character(len=22), public, parameter :: MAIL_VER = 'lihuang.dmft@gmail.com'
 
-! version string: license info.
+!!
+!! @var GPL3_VER
+!!
+!! version string, license info.
+!!
      character(len=36), public, parameter :: GPL3_VER = 'GNU General Public License version 3'
 
   end module version
