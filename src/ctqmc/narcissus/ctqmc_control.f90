9b7ef70857e88476e2be5013b45ae34ea7bbbc5f
Li Huang <lihuang.dmft@gmail.com>
2017-04-23 17:26:33 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_control.f90
+++ src/ctqmc/narcissus/ctqmc_control.f90
@@ -5,7 +5,7 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 09/15/2009 by li huang (created)
-!!!           04/22/2017 by li huang (last modified)
+!!!           04/23/2017 by li huang (last modified)
 !!! purpose : define global control parameters for hybridization expansion
 !!!           version continuous time quantum Monte Carlo (CTQMC) quantum
 !!!           impurity solver and dynamical mean field theory (DMFT) self-
