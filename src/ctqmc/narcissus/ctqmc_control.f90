ec7af1567159bde4409b1510b0e59a59b6e06091
Li Huang <lihuang.dmft@gmail.com>
2017-04-24 00:58:28 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_control.f90
+++ src/ctqmc/narcissus/ctqmc_control.f90
@@ -528,22 +528,46 @@
 !!>>> MPI related common variables                                     <<<
 !!========================================================================
 
-! number of processors: default value 1
+!!
+!! @var nprocs
+!!
+!! number of processors: default value 1
+!!
      integer, public, save :: nprocs = 1
 
-! the id of current process: default value 0
+!!
+!! @var myid
+!!
+!! the id of current process: default value 0
+!!
      integer, public, save :: myid   = 0
 
-! denote as the controller process: default value 0
+!!
+!! @var master
+!!
+!! denote as the controller process: default value 0
+!!
      integer, public, save :: master = 0
 
-! the id of current process in cartesian topology (cid == myid)
+!!
+!! @var cid
+!!
+!! the id of current process in cartesian topology (cid == myid)
+!!
      integer, public, save :: cid    = 0
 
-! the x coordinates of current process in cartesian topology
+!!
+!! @var cx
+!!
+!! the x coordinates of current process in cartesian topology
+!!
      integer, public, save :: cx     = 0
 
-! the y coordinates of current process in cartesian topology
+!!
+!! @var cy
+!!
+!! the y coordinates of current process in cartesian topology
+!!
      integer, public, save :: cy     = 0
 
   end module control
