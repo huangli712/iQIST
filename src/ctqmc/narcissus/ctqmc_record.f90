811d1f119e0a8f9a4192b99b6abcf63fc719596c
Li Huang <lihuang.dmft@gmail.com>
2017-04-26 01:36:04 +0800

    try to implement ctqmc_record_paux(), not finished

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -78,7 +78,7 @@
 !! record some auxiliary physical observables
 !!
   subroutine ctqmc_record_paux()
-     use constants, only : two
+     use constants, only : dp, two
 
      use control, only : nband, norbs
      use control, only : beta
