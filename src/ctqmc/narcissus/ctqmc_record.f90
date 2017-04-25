0ad6cac5ec315eb0149b74bac6e838ec7846bbb8
Li Huang <lihuang.dmft@gmail.com>
2017-04-26 01:21:35 +0800

    remove fche

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -329,9 +329,6 @@
 ! loop index for legendre polynomial
      integer  :: fleg
 
-! loop index for chebyshev polynomial
-     integer  :: fche
-
 ! index for imaginary time \tau
      integer  :: curr
 
