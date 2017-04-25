3b66bc8acbad2e0c7ab699debc3a1e86fc271b38
Li Huang <lihuang.dmft@gmail.com>
2017-04-25 17:42:07 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_update.f90
+++ src/ctqmc/narcissus/ctqmc_update.f90
@@ -53,25 +53,11 @@
      enddo ! over i={1,ntherm} loop
 
 ! reinit statistics variables
-     ins_t = zero
-     ins_a = zero
-     ins_r = zero
-
-     rmv_t = zero
-     rmv_a = zero
-     rmv_r = zero
-
-     lsh_t = zero
-     lsh_a = zero
-     lsh_r = zero
-
-     rsh_t = zero
-     rsh_a = zero
-     rsh_r = zero
-
-     rfl_t = zero
-     rfl_a = zero
-     rfl_r = zero
+     ins_t = zero; ins_a = zero; ins_r = zero
+     rmv_t = zero; rmv_a = zero; rmv_r = zero
+     lsh_t = zero; lsh_a = zero; lsh_r = zero
+     rsh_t = zero; rsh_a = zero; rsh_r = zero
+     rfl_t = zero; rfl_a = zero; rfl_r = zero
 
      return
   end subroutine ctqmc_warming
