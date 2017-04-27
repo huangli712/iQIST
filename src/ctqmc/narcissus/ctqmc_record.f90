dc9ad46bd5d874bfcd00680c554125db1746c090
Li Huang <lihuang.dmft@gmail.com>
2017-04-27 09:57:34 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -1807,7 +1807,8 @@
 !!
   subroutine ctqmc_reduce_grnf(grnf_mpi, grnf_err)
      use constants, only : dp, zero, czero, czi
-     use mmpi, only : mp_allreduce, mp_barrier
+     use mmpi, only : mp_allreduce,
+     use mmpi, only : mp_barrier
 
      use control, only : norbs
      use control, only : mfreq
