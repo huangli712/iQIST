c08e2eefb49112612fe6206d6bd79df295996f0a
Li Huang <lihuang.dmft@gmail.com>
2017-05-05 15:19:59 +0800

    fix ctqmc_reduce_pair

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -2483,12 +2483,11 @@
      p2pw_mpi = czero
      p2pw_err = czero
 
-! build ps_re_mpi and ps_im_mpi, collect data from all children processes
+! build p2pw_mpi, collect data from all children processes
 # if defined (MPI)
 
 ! collect data
-     call mp_allreduce(ps_re, ps_re_mpi)
-     call mp_allreduce(ps_im, ps_im_mpi)
+     call mp_allreduce(p2pw, p2pw_mpi)
 
 ! block until all processes have reached here
      call mp_barrier()
