52dd81f7b1672f07bf72ecd7ca71ea284804f109
Li Huang <lihuang.dmft@gmail.com>
2017-05-11 01:07:13 +0800

    adjust the output format

--- src/ctqmc/manjushaka/ctqmc_dmft.f90
+++ src/ctqmc/manjushaka/ctqmc_dmft.f90
@@ -209,6 +209,15 @@
 ! update sig1
      call s_mix_z(size(sig1), sig2, sig1, one - alpha)
 
+! write convergence information to screen
+     if ( myid == master ) then ! only master node can do it
+         write(mystd,'(4X,a,i03.2)') 'current iteration / ', iter
+         write(mystd,'(4X,a,e10.3)') 'current epsilon   / ', seps
+         write(mystd,'(4X,a,X,a11)') 'selected watchdog / ', 'self-energy'
+         write(mystd,'(4X,a,l2)')    'is it convergence / ', conv
+         write(mystd,'(2X,a)') cname//' >>> self-consistent iteration checker shutdown'
+         write(mystd,*)
+     endif ! back if ( myid == master ) block
 
      return
   end subroutine ctqmc_dmft_conver
