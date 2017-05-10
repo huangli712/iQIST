044d035f0100e12c1b0d57d07c1883e9b50b6adc
Li Huang <lihuang.dmft@gmail.com>
2017-05-10 09:51:13 +0800

    improve the code style

--- src/ctqmc/manjushaka/ctqmc_hybmat.f90
+++ src/ctqmc/manjushaka/ctqmc_hybmat.f90
@@ -58,10 +58,10 @@
      integer, intent(in)  :: is
      integer, intent(in)  :: ie
 
-! imaginary time \tau_s for new create operator
+! imaginary time \tau_s for new creation operator
      real(dp), intent(in) :: tau_start
 
-! imaginary time \tau_e for new destroy operator
+! imaginary time \tau_e for new annihilation operator
      real(dp), intent(in) :: tau_end
 
 ! previous calculated determinant ratio
