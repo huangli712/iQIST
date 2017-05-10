d77baca2045102bd78efad76a68cd94926a28b98
Li Huang <lihuang.dmft@gmail.com>
2017-05-10 09:40:19 +0800

    add header comment

--- src/ctqmc/manjushaka/ctqmc_hybmat.f90
+++ src/ctqmc/manjushaka/ctqmc_hybmat.f90
@@ -1,3 +1,30 @@
+!!!-----------------------------------------------------------------------
+!!! project : narcissus
+!!! program : cat_insert_matrix
+!!!           cat_remove_matrix
+!!!           cat_lshift_matrix
+!!!           cat_rshift_matrix
+!!!           cat_reflip_matrix
+!!!           cat_reload_matrix <<<---
+!!!           cat_insert_detrat
+!!!           cat_remove_detrat
+!!!           cat_lshift_detrat
+!!!           cat_rshift_detrat
+!!!           cat_reflip_detrat
+!!!           cat_reload_detrat <<<---
+!!! source  : ctqmc_hybmat.f90
+!!! type    : subroutines
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 09/16/2009 by li huang (created)
+!!!           05/10/2017 by li huang (last modified)
+!!! purpose : offer basic infrastructure (elementary updating subroutines)
+!!!           for hybridization expansion version continuous time quantum
+!!!           Monte Carlo (CTQMC) quantum impurity solver. the following
+!!!           subroutines mainly deal with the \mathscr{M} matrix: mmat,
+!!!           and \mathscr{G} matrix: gmat
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
 
 !!========================================================================
 !!>>> service layer: update M and G matrices                           <<<
