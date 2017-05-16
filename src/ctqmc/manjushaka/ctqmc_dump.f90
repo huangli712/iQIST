e63d34ef434302e942e4bb9009e4b45c30280a40
Li Huang <lihuang.dmft@gmail.com>
2017-05-17 00:02:03 +0800

    improve the code style

--- src/ctqmc/manjushaka/ctqmc_dump.f90
+++ src/ctqmc/manjushaka/ctqmc_dump.f90
@@ -537,9 +537,7 @@
 ! write it
      do i=1,norbs
          do j=1,mfreq
-             write(mytmp,'(i6,5f16.8)') i, rmesh(j), &
-              real(grnf(j,i,i)), aimag(grnf(j,i,i)), &
-              real(gerr(j,i,i)), aimag(gerr(j,i,i))
+             write(mytmp,'(i6,5f16.8)') i, rmesh(j), grnf(j,i,i), gerr(j,i,i)
          enddo ! over j={1,mfreq} loop
          write(mytmp,*) ! write empty lines
          write(mytmp,*)
