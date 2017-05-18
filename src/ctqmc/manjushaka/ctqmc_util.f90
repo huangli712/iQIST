8e28805ff160df19c210a9b57060696a59f8f85b
Li Huang <lihuang.dmft@gmail.com>
2017-05-18 23:22:29 +0800

    fix the header comments

--- src/ctqmc/manjushaka/ctqmc_util.f90
+++ src/ctqmc/manjushaka/ctqmc_util.f90
@@ -4,13 +4,14 @@
 !!!           ctqmc_four_hybf <<<---
 !!!           ctqmc_eval_htau
 !!!           ctqmc_eval_hsed <<<---
-!!!           ctqmc_symm_nmat
+!!!           ctqmc_symm_nimp
 !!!           ctqmc_symm_gtau
-!!!           ctqmc_symm_grnf
-!!!           ctqmc_smth_sigf   <<<---
-!!!           ctqmc_make_gtau   <<<---
-!!!           ctqmc_make_prod   <<<---
-!!!           ctqmc_make_hub1   <<<---
+!!!           ctqmc_symm_grnf <<<---
+!!!           ctqmc_smth_sigf <<<---
+!!!           ctqmc_make_gtau
+!!!           ctqmc_make_ftau <<<---
+!!!           ctqmc_make_prod <<<---
+!!!           ctqmc_make_hub2 <<<---
 !!! source  : ctqmc_util.f90
 !!! type    : functions & subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
