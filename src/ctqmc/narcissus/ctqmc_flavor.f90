b03280b3fe881cb24dd7ed9e96207251c564db9f
Li Huang <lihuang.dmft@gmail.com>
2017-05-18 09:25:19 +0800

    adjust the code layout

--- src/ctqmc/narcissus/ctqmc_flavor.f90
+++ src/ctqmc/narcissus/ctqmc_flavor.f90
@@ -23,7 +23,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 09/23/2009 by li huang (created)
-!!!           05/11/2017 by li huang (last modified)
+!!!           05/18/2017 by li huang (last modified)
 !!! purpose : offer basic infrastructure (elementary updating subroutines)
 !!!           for hybridization expansion version continuous time quantum
 !!!           Monte Carlo (CTQMC) quantum impurity solver. the following
