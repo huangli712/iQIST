d4dd9344cf13fce8144b4661297f3dc01404eb28
Li Huang <lihuang.dmft@gmail.com>
2017-05-18 17:24:33 +0800

    improve the code style

--- src/ctqmc/manjushaka/ctqmc_update.f90
+++ src/ctqmc/manjushaka/ctqmc_update.f90
@@ -203,12 +203,12 @@
 ! write the results, only master node can do it
          if ( myid == master ) then
              if ( cflag == 99 ) then
-                 write(mystd,'(4X,a)') '>>> quantum impurity solver status: error?'
+                 write(mystd,'(4X,a)') '>>> quantum impurity solver status: fatal error'
                  write(mystd,'(4X,a)') '>>> please check the status file: solver.status.dat'
                  call ctqmc_save_status()
                  call s_print_error('ctqmc_try_warning','unknown fatal error occur')
              else
-                 write(mystd,'(4X,a)') '>>> quantum impurity solver status: normal'
+                 write(mystd,'(4X,a)') '>>> quantum impurity solver status: healthy'
              endif ! back if ( cflag == 99 ) block
          endif ! back if ( myid == master ) block
 
