9f3341b4ee7edd0e831f1c8d5e9d303215b89552
Li Huang <lihuang.dmft@gmail.com>
2017-05-31 17:33:16 +0800

    improve the code style

--- src/ctqmc/narcissus/ctqmc_util.f90
+++ src/ctqmc/narcissus/ctqmc_util.f90
@@ -23,7 +23,7 @@
 !!! type    : functions & subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 10/01/2008 by li huang (created)
-!!!           05/27/2017 by li huang (last modified)
+!!!           05/31/2017 by li huang (last modified)
 !!! purpose : provide utility functions and subroutines for hybridization
 !!!           expansion version continuous time quantum Monte Carlo (CTQMC)
 !!!           quantum impurity solver.
@@ -863,6 +863,7 @@
 ! using normal representation
 !-------------------------------------------------------------------------
      STD_BLOCK: if ( isort == 1 ) then
+         CONTINUE
      endif STD_BLOCK ! back if ( isort == 1 ) block
 
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -935,8 +936,8 @@
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 ! deallocate memory
-     deallocate(ufun)
      deallocate(bfun)
+     deallocate(ufun)
      deallocate(tleg)
      deallocate(tsvd)
 
