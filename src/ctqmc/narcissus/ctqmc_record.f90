d4a54f276dc6816a4245af8c851102590f53c3eb
Li Huang <lihuang.dmft@gmail.com>
2017-06-22 15:37:54 +0800

    try to implement new ctqmc_record_g2ph

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -1586,9 +1586,6 @@
              mx1 = mx1 * rep_l(curr1,l1)
              !mx1 = mx1 * sqrt(two * l1 - 1) * rep_l(curr1,l1)
              !!cmx1 = mx1 * exp( czi * two * (wbn - 1) * pi / beta * te1 )
-             print *, caux2(wbn,ie1,f1), exp( czi * two * (wbn - 1) * pi / beta * te1 )
-             pause
-
 
              do is2=1,rank(f2)
                  ts2 = time_s( index_s(is2, f2), f2 )
