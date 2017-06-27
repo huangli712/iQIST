a24c8930a2bfc40989a04cd046f973643df8203a
Li Huang <lihuang.dmft@gmail.com>
2017-06-27 09:45:10 +0800

    try to implement new ctqmc_record_g2pp

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -1730,6 +1730,25 @@
 
      implicit none
 
+! check whether there is conflict
+! this subroutine is only designed for the particle-particle channel
+     call s_assert2( btest(isvrt, 3) .or. btest(isvrt, 4), 'in ctqmc_record_g2pp' )
+
+! you can not calculate the AABB and ABBA components at the same time
+     call s_assert2( .not. ( btest(isvrt, 3) .and. btest(isvrt, 4) ), 'in ctqmc_record_g2pp' )
+
+     select case ( isort )
+
+         case (1)
+             call cat_record_g2pp_std()
+
+         case (2)
+             call cat_record_g2pp_leg()
+
+         case (3)
+             call cat_record_g2pp_svd()
+
+     end select
 
      return
   end subroutine ctqmc_record_g2pp
@@ -1818,13 +1837,6 @@
 ! where m and n are the first two frequency indices for g2aux and h2aux
      complex(dp), allocatable :: g2aux(:,:,:)
      complex(dp), allocatable :: h2aux(:,:,:)
-
-! check whether there is conflict
-! this subroutine is only designed for the particle-particle channel
-     call s_assert2( btest(isvrt, 3) .or. btest(isvrt, 4), 'in ctqmc_record_g2pp' )
-
-! you can not calculate the AABB and ABBA components at the same time
-     call s_assert2( .not. ( btest(isvrt, 3) .and. btest(isvrt, 4) ), 'in ctqmc_record_g2pp' )
 
 ! evaluate nfaux, determine the size of g2aux and h2aux
      nfaux = nffrq + nbfrq - 1
