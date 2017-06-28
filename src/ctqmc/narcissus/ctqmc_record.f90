eb27fc586f5c6aaf1153d1559376b1110747e6f9
Li Huang <lihuang.dmft@gmail.com>
2017-06-29 00:22:25 +0800

    try to implement cat_record_g2pp_leg

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -2165,6 +2165,38 @@
          enddo ! over is1={1,rank(f1)} loop
      enddo ! over f1={1,norbs} loop
 
+! prepare some important arrays: pl_e
+     step = real(legrd - 1) / two
+     do f1=1,norbs
+         do ie1=1,rank(f1)
+             do f2=1,norbs
+                 do ie2=1,rank(f2)
+! determine dt (distance) and ms (sign)
+                     dt = time_e( index_e(ie2, f2), f2 ) - time_e( index_e(ie1, f1), f1 )
+                     ms = sign(one, dt)
+
+! adjust dt, keep it stay in (zero, beta)
+                     if ( dt < zero ) then
+                         dt = dt + beta
+                     endif ! back if ( dt < zero ) block
+
+! determine index for imaginary time
+                     curr = nint( ( two * dt / beta ) * step ) + 1
+
+! special tricks for the first point and the last point
+                     if ( curr == 1 .or. curr == legrd ) then
+                         ms = two * ms
+                     endif ! back if ( curr == 1 .or. curr == legrd ) block
+
+! fill pl_e
+                     do l1=1,lemax
+                         pl_e(l1,ie2,ie1,f2,f1) = ms * rep_l(curr,l1)
+                     enddo ! over l1={1,lemax} loop
+                 enddo ! over ie2={1,rank(f2)} loop
+             enddo ! over f2={1,norbs} loop
+         enddo ! over ie1={1,rank(f1)} loop
+     enddo ! over f1={1,norbs} loop
+
 ! prepare some important arrays: caux1 and caux2
      do f1=1,norbs
          call ctqmc_make_bexp(f1, nbfrq, maxval(rank), caux1(:,:,f1), caux2(:,:,f1))
