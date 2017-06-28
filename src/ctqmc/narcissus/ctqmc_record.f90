7678842ae128a48bdab3432f369e28e4ff0d93b6
Li Huang <lihuang.dmft@gmail.com>
2017-06-29 00:37:27 +0800

    try to implement cat_record_g2pp_leg

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -2252,6 +2252,7 @@
                  do l1=1,lemax                     ! legendre polynomial index: l
                      do l2=1,lemax                 ! legendre polynomial index: l'
                          ee = caux2(wbn,ie1,f1) * caux1(wbn,is2,f2)
+                         pp = pl_e(l1,ie1,ie2,f1,f2) * pl_s(l2,is1,is2,f1,f2) * lfun(l1,l2)
                          mm = mmat(ie1, is1, f1) * mmat(ie2, is2, f2)
                          if ( f1 == f2 ) then
                              mm = mm - mmat(ie1, is2, f1) * mmat(ie2, is1, f1)
