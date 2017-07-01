9dd41e66d8fd0d91c1097e9ef9b2d5f0d4bf2a02
Li Huang <lihuang.dmft@gmail.com>
2017-07-02 00:48:17 +0800

    try to implement cat_record_g2pp_svd

--- src/ctqmc/narcissus/ctqmc_record.f90
+++ src/ctqmc/narcissus/ctqmc_record.f90
@@ -2669,6 +2669,53 @@
          enddo ! over ie1={1,rank(f1)} loop
      enddo ! over f1={1,norbs} loop
 
+! prepare some important arrays: caux1 and caux2
+     do f1=1,norbs
+         call ctqmc_make_bexp(f1, nbfrq, maxval(rank), caux1(:,:,f1), caux2(:,:,f1))
+     enddo ! over f1={1,norbs} loop
+
+! calculate g2pp and h2pp
+!
+! G2_PP_AABB component
+!-------------------------------------------------------------------------
+
+     CALC_G2_PP_AABB: BLOCK
+
+         if ( btest(isvrt,3) ) then
+
+             do f1=1,norbs                         ! block index: A
+                 do f2=1,f1                        ! block index: B
+                     do is1=1,rank(f1)             ! \beta : creation operator
+                         do ie1=1,rank(f1)         ! \alpha: annihilation operator
+                             do is2=1,rank(f2)     ! \delta: creation operator
+                                 do ie2=1,rank(f2) ! \gamma: annihilation operator
+             !-------------------!
+             do wbn=1,nbfrq                        ! bosonic matsubara frequency: w
+                 do l1=1,svmax                     ! svd polynomial index: l
+                     do l2=1,svmax                 ! svd polynomial index: l'
+                         ee = caux2(wbn,ie1,f1) * caux1(wbn,is2,f2)
+                         pp = pl_s(l1,is2,is1,f2,f1) * pl_e(l2,ie2,ie1,f2,f1) * lfun(l1,l2)
+                         mm = mmat(ie1, is1, f1) * mmat(ie2, is2, f2)
+                         if ( f1 == f2 ) then
+                             mm = mm - mmat(ie1, is2, f1) * mmat(ie2, is1, f1)
+                         endif ! back if ( f1 == f2 ) block
+
+                         g2pp(l2,l1,wbn,f2,f1) = g2pp(l2,l1,wbn,f2,f1) + mm * pp * ee / beta
+                         h2pp(l2,l1,wbn,f2,f1) = h2pp(l2,l1,wbn,f2,f1) + mm * pp * ee / beta * pref(ie1,f1)
+                     enddo ! over l2={1,lemax} loop
+                 enddo ! over l1={1,lemax} loop
+             enddo ! over wbn={1,nbfrq} loop
+             !-------------------!
+                                 enddo ! over ie2={1,rank(f2)} loop
+                             enddo ! over is2={1,rank(f2)} loop
+                         enddo ! over ie1={1,rank(f1)} loop
+                     enddo ! is1={1,rank(f1)} loop
+                 enddo ! over f2={1,f1} loop
+             enddo ! over f1={1,norbs} loop
+
+         endif ! back if ( btest(isvrt,3) ) block
+
+     END BLOCK CALC_G2_PP_AABB
      return
   end subroutine cat_record_g2pp_svd
 
