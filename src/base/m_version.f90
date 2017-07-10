2063a18f461e3993e8adb4584d2edf8d7e00e6ee
Li Huang <lihuang.dmft@gmail.com>
2017-07-10 17:41:39 +0800

    rename some variables

--- src/base/m_version.f90
+++ src/base/m_version.f90
@@ -5,7 +5,7 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 01/26/2017 by li huang (created)
-!!!           05/12/2017 by li huang (last modified)
+!!!           07/10/2017 by li huang (last modified)
 !!! purpose : the purpose of this module is to define version strings.
 !!! status  : unstable
 !!! comment :
@@ -21,10 +21,10 @@
 !! Usage
 !! =====
 !!
-!! use version, only : FULL_VER
+!! use version, only : V_FULL
 !! implicit none
 !!
-!! print *, FULL_VER
+!! print *, V_FULL
 !!
 !!
 
@@ -32,52 +32,52 @@
      implicit none
 
 !!
-!! @var FULL_VER
+!! @var V_FULL
 !!
 !! version string, version number + date info. + status info.
 !!
-     character(len=20), public, parameter :: FULL_VER = 'v0.7.0 @ 2017.01.31D'
+     character(len=20), public, parameter :: V_FULL = 'v0.7.0 @ 2017.01.31D'
 
 !!
-!! @var CURR_VER
+!! @var V_CURR
 !!
 !! version string, only version number
 !!
-     character(len=06), public, parameter :: CURR_VER = 'v0.7.0'
+     character(len=06), public, parameter :: V_CURR = 'v0.7.0'
 
 !!
-!! @var DATE_VER
+!! @var V_DATE
 !!
 !! version string, only date info.
 !!
-     character(len=11), public, parameter :: DATE_VER = '2017.01.31'
+     character(len=11), public, parameter :: V_DATE = '2017.01.31'
 
 !!
-!! @var STAT_VER
+!! @var V_STAT
 !!
 !! version string, only status info., D means devel, T testing, R released.
 !!
-     character(len=01), public, parameter :: STAT_VER = 'D'
+     character(len=01), public, parameter :: V_STAT = 'D'
 
 !!
-!! @var AUTH_VER
+!! @var V_AUTH
 !!
 !! version string, author info.
 !!
-     character(len=46), public, parameter :: AUTH_VER = 'by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
+     character(len=46), public, parameter :: V_AUTH = 'by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
 
 !!
-!! @var MAIL_VER
+!! @var V_MAIL
 !!
 !! version string, email info.
 !!
-     character(len=22), public, parameter :: MAIL_VER = 'lihuang.dmft@gmail.com'
+     character(len=22), public, parameter :: V_MAIL = 'lihuang.dmft@gmail.com'
 
 !!
-!! @var GPL3_VER
+!! @var V_GPL3
 !!
 !! version string, license info.
 !!
-     character(len=36), public, parameter :: GPL3_VER = 'GNU General Public License version 3'
+     character(len=36), public, parameter :: V_GPL3 = 'GNU General Public License version 3'
 
   end module version
