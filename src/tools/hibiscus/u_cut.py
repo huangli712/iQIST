c8d90e26f7be22fa9c6c2153e55fb96689c5a25a
Li Huang <lihuang.dmft@gmail.com>
2017-07-25 12:31:54 +0800

    try to implement u_cut.py

--- src/tools/hibiscus/u_cut.py
+++ src/tools/hibiscus/u_cut.py
@@ -3,29 +3,24 @@
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+# define command-line arguments
+parser = argparse.ArgumentParser(description='Split a data file into several files.')
+parser.add_argument('fn', help='the file name')
 parser.add_argument('-b', dest = 'nblock', type = int, help = 'number of blocks')
 parser.add_argument('-l', dest = 'nline', type = int, help = 'number of lines for each data block')
-parser.add_argument('filename', help='the file name')
 args = parser.parse_args()
 
+# extract command-line arguments
 nblock =  args.nblock
 nline = args.nline
-filename = str(args.filename)
+fn = str(args.fn)
 
-print nblock, nline, filename
-
-f = open(filename, 'r')
-
-for ib in range(nblock):
-    fb = open(filename + '.' + str(ib + 1), 'w')
-
-    for il in range(nline):
-        read_data = f.readline()
-        print >> fb, read_data
-
-    fb.close()
-    f.readline()
-    f.readline()
-
-f.close()
+# try to split the given file into nblock files. each file should have
+# nline lines
+with open(fn, 'r') as f:
+    for ib in range(nblock):
+        with open(fn + '.' + str(ib + 1), 'w') as fb:
+            for il in range(nline):
+                print >> fb, f.readline().rstrip()
+        f.readline() # there should be two empty lines after each block 
+        f.readline()
