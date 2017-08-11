f24ddfee4fa046a161531d0c71c0fa98c6f2b80c
Li Huang <lihuang.dmft@gmail.com>
2017-08-11 16:02:33 +0800

    try to implement u_ktau.py

--- src/tools/hibiscus/u_ktau.py
+++ src/tools/hibiscus/u_ktau.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy
+import math
 import argparse
 
 # define command-line arguments
@@ -22,6 +23,21 @@ lc = args.lc
 wc = args.wc
 
 # allocate memory
-kmesh = numpy.zeros(ntime, dtype = numpy.float)
 ktau = numpy.zeros(ntime, dtype = numpy.float)
 ptau = numpy.zeros(ntime, dtype = numpy.float)
+
+# build linear imaginary time mesh
+kmesh = numpy.linspace(0.0, beta, ntime)
+
+if model == 1: # plasmon pole model
+    for i in range(ntime):
+        ktau[i] = (lc / wc)**2 / sinh(beta * wc / 2.0)
+        ktau[i] = ktau[i] * ( cosh(beta * wc / 2.0) - cosh(beta * wc / 2.0 - kmesh[i] * wc) )
+        ptau[i] = (lc / wc)**2 / sinh(beta * wc / 2.0)
+        ptau[i] = ptau[i] * sinh(beta * wc / 2.0 - kmsh(i) * wc) * wc
+
+if model == 2: # ohmic model
+    #do i=1,ntime
+    #    ktau(i) = lc * log(one + beta * wc * sin(pi * kmsh(i) / beta) / pi)
+    #    ptau(i) = lc * wc * cos(pi * kmsh(i) / beta) / (one + beta * wc * sin(pi * kmsh(i) / beta) / pi)
+    #enddo ! over i={1,ntime} loop
