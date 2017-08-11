3e360a863acc177289efd471e889a90f608f617e
Li Huang <lihuang.dmft@gmail.com>
2017-08-11 15:51:16 +0800

    try to implement u_ktau.py

--- src/tools/hibiscus/u_ktau.py
+++ src/tools/hibiscus/u_ktau.py
@@ -12,3 +12,13 @@ parser.add_argument('-b', dest = 'beta', type = float, help = 'inverse temperatu
 parser.add_argument('-l', dest = 'lc', type = float, help = 'experienced parameter for screening function lc')
 parser.add_argument('-w', dest = 'wc', type = float, help = 'experienced parameter for screening function wc')
 args = parser.parse_args()
+
+# extract command-line arguments
+fn = str(args.fn)
+model = args.model
+ntime = args.ntime
+beta = args.beta
+lc = args.lc
+wc = args.wc
+
+print fn, model, ntime, beta, lc, wc
