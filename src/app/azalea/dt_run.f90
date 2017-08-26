53e6184039c5c08fb0e450e27454baab20782d51
Li Huang <lihuang.dmft@gmail.com>
2017-08-26 17:12:04 +0800

    try to implement dt_run()

--- src/app/azalea/dt_run.f90
+++ src/app/azalea/dt_run.f90
@@ -1,5 +1,21 @@
   subroutine dt_run()
+     use control, only : isdia
+
      implicit none
+
+     DT_CORE: &
+     select case ( isdia )
+
+         case (1)
+             call dt_df_std()
+
+         case (2)
+             call dt_df_ladder()
+
+         case default
+             call s_print_error('dt_run','this feature is not implemented')
+
+     end select DT_CORE
 
      return
   end subroutine dt_run
