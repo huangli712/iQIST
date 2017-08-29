9f574c82c25700e0a03ceb4f03ba5e25cbddf86f
Li Huang <lihuang.dmft@gmail.com>
2017-08-29 18:04:39 +0800

    rename df to dt

--- src/app/azalea/dt_context.f90
+++ src/app/azalea/dt_context.f90
@@ -1,10 +1,10 @@
 !!!-----------------------------------------------------------------------
 !!! project : azalea
-!!! program : df_mesh module
-!!!           df_dmft module
-!!!           df_dual module
-!!!           df_latt module
-!!!           df_vert module
+!!! program : dt_mesh module
+!!!           dt_dmft module
+!!!           dt_dual module
+!!!           dt_latt module
+!!!           dt_vert module
 !!!           context module
 !!! source  : dmft_context.f90
 !!! type    : modules
@@ -18,13 +18,13 @@
 !!!-----------------------------------------------------------------------
 
 !!========================================================================
-!!>>> module df_mesh                                                   <<<
+!!>>> module dt_mesh                                                   <<<
 !!========================================================================
 
 !!
-!! @mod df_mesh
+!! @mod dt_mesh
 !!
-  module df_mesh
+  module dt_mesh
      use constants, only : dp
 
      real(dp), public, save, allocatable :: kx(:)
@@ -52,16 +52,16 @@
 !!
      real(dp), public, save, allocatable :: bmesh(:)
 
-  end module df_mesh
+  end module dt_mesh
 
 !!========================================================================
-!!>>> module df_dmft                                                   <<<
+!!>>> module dt_dmft                                                   <<<
 !!========================================================================
 
 !!
-!! @mod df_dmft
+!! @mod dt_dmft
 !!
-  module df_dmft
+  module dt_dmft
      use constants, only : dp
 
      implicit none
@@ -87,16 +87,16 @@
 !!
      complex(dp), public, save, allocatable :: dmft_h(:,:)
 
-  end module df_dmft
+  end module dt_dmft
 
 !!========================================================================
-!!>>> module df_dual                                                   <<<
+!!>>> module dt_dual                                                   <<<
 !!========================================================================
 
 !!
-!! @mod df_dual
+!! @mod dt_dual
 !!
-  module df_dual
+  module dt_dual
      use constants, only : dp
 
      implicit none
@@ -122,16 +122,16 @@
 !!
      complex(dp), public, save, allocatable :: dual_b(:,:,:)
 
-  end module df_dual
+  end module dt_dual
 
 !!========================================================================
-!!>>> module df_latt                                                   <<<
+!!>>> module dt_latt                                                   <<<
 !!========================================================================
 
 !!
 !! @mod latt
 !!
-  module df_latt
+  module dt_latt
      use constants, only : dp
 
      implicit none
@@ -150,16 +150,16 @@
 !!
      complex(dp), public, save, allocatable :: latt_s(:,:,:)
 
-  end module df_latt
+  end module dt_latt
 
 !!========================================================================
-!!>>> module df_vert                                                   <<<
+!!>>> module dt_vert                                                   <<<
 !!========================================================================
 
 !!
-!! @mod df_vert
+!! @mod dt_vert
 !!
-  module df_vert
+  module dt_vert
      use constants, only : dp
 
      implicit none
@@ -178,7 +178,7 @@
 !!
      complex(dp), public, save, allocatable :: vert_m(:,:,:)
 
-  end module df_vert
+  end module dt_vert
 
 !!========================================================================
 !!>>> module context                                                   <<<
@@ -189,11 +189,11 @@
 
      use control
 
-     use df_mesh
-     use df_dmft
-     use df_dual
-     use df_latt
-     use df_vert
+     use dt_mesh
+     use dt_dmft
+     use dt_dual
+     use dt_latt
+     use dt_vert
 
 !!========================================================================
 !!>>> declare global variables                                         <<<
@@ -207,18 +207,18 @@
 !!========================================================================
 
 ! declaration of module procedures: allocate memory
-     public :: df_allocate_memory_mesh
-     public :: df_allocate_memory_dmft
-     public :: df_allocate_memory_dual
-     public :: df_allocate_memory_latt
-     public :: df_allocate_memory_vert
+     public :: dt_allocate_memory_mesh
+     public :: dt_allocate_memory_dmft
+     public :: dt_allocate_memory_dual
+     public :: dt_allocate_memory_latt
+     public :: dt_allocate_memory_vert
 
 ! declaration of module procedures: deallocate memory
-     public :: df_deallocate_memory_mesh
-     public :: df_deallocate_memory_dmft
-     public :: df_deallocate_memory_dual
-     public :: df_deallocate_memory_latt
-     public :: df_deallocate_memory_vert
+     public :: dt_deallocate_memory_mesh
+     public :: dt_deallocate_memory_dmft
+     public :: dt_deallocate_memory_dual
+     public :: dt_deallocate_memory_latt
+     public :: dt_deallocate_memory_vert
 
   contains ! encapsulated functionality
 
@@ -226,7 +226,7 @@
 !!>>> allocate memory subroutines                                      <<<
 !!========================================================================
 
-  subroutine df_allocate_memory_mesh()
+  subroutine dt_allocate_memory_mesh()
      implicit none
 
      allocate(kx(nkp_x), stat=istat)
@@ -238,7 +238,7 @@
      allocate(bmesh(nbfrq), stat=istat)
 
      if ( istat /= 0 ) then
-         call s_print_error('df_allocate_memory_mesh','can not allocate enough memory')
+         call s_print_error('dt_allocate_memory_mesh','can not allocate enough memory')
      endif
 
      kx = zero
@@ -250,9 +250,9 @@
      bmesh = zero
 
      return
-  end subroutine df_allocate_memory_mesh
+  end subroutine dt_allocate_memory_mesh
 
-  subroutine df_allocate_memory_dmft()
+  subroutine dt_allocate_memory_dmft()
      implicit none
 
      allocate(dmft_g(nffrq,norbs), stat=istat)
@@ -260,7 +260,7 @@
      allocate(dmft_h(nffrq,norbs), stat=istat)
 
      if ( istat /= 0 ) then
-         call s_print_error('df_allocate_memory_dmft','can not allocate enough memory')
+         call s_print_error('dt_allocate_memory_dmft','can not allocate enough memory')
      endif
 
      dmft_g = czero
@@ -268,9 +268,9 @@
      dmft_h = czero
 
      return
-  end subroutine df_allocate_memory_dmft
+  end subroutine dt_allocate_memory_dmft
 
-  subroutine df_allocate_memory_dual()
+  subroutine dt_allocate_memory_dual()
      implicit none
 
      allocate(dual_g(nkpts,nffrq,norbs), stat=istat)
@@ -278,7 +278,7 @@
      allocate(dual_b(nkpts,nffrq,norbs), stat=istat)
 
      if ( istat /= 0 ) then
-         call s_print_error('df_allocate_memory_dual','can not allocate enough memory')
+         call s_print_error('dt_allocate_memory_dual','can not allocate enough memory')
      endif
 
      dual_g = czero
@@ -286,45 +286,45 @@
      dual_b = czero
 
      return
-  end subroutine df_allocate_memory_dual
+  end subroutine dt_allocate_memory_dual
 
-  subroutine df_allocate_memory_latt()
+  subroutine dt_allocate_memory_latt()
      implicit none
 
      allocate(latt_g(nkpts,nffrq,norbs), stat=istat)
      allocate(latt_s(nkpts,nffrq,norbs), stat=istat)
 
      if ( istat /= 0 ) then
-         call s_print_error('df_allocate_memory_latt','can not allocate enough memory')
+         call s_print_error('dt_allocate_memory_latt','can not allocate enough memory')
      endif
 
      latt_g = czero
      latt_s = czero
 
      return
-  end subroutine df_allocate_memory_latt
+  end subroutine dt_allocate_memory_latt
 
-  subroutine df_allocate_memory_vert()
+  subroutine dt_allocate_memory_vert()
      implicit none
 
      allocate(vert_d(nffrq,nffrq,nbfrq), stat=istat)
      allocate(vert_m(nffrq,nffrq,nbfrq), stat=istat)
 
      if ( istat /= 0 ) then
-         call s_print_error('df_allocate_memory_vert','can not allocate enough memory')
+         call s_print_error('dt_allocate_memory_vert','can not allocate enough memory')
      endif
 
      vert_d = czero
      vert_m = czero
 
      return
-  end subroutine df_allocate_memory_vert
+  end subroutine dt_allocate_memory_vert
 
 !!========================================================================
 !!>>> deallocate memory subroutines                                    <<<
 !!========================================================================
 
-  subroutine df_deallocate_memory_mesh()
+  subroutine dt_deallocate_memory_mesh()
      implicit none
 
      if ( allocated(kx) ) deallocate(kx)
@@ -336,9 +336,9 @@
      if ( allocated(bmesh) ) deallocate(bmesh)
 
      return
-  end subroutine df_deallocate_memory_mesh
+  end subroutine dt_deallocate_memory_mesh
 
-  subroutine df_deallocate_memory_dmft()
+  subroutine dt_deallocate_memory_dmft()
      implicit none
 
      if ( allocated(dmft_g) ) deallocate(dmft_g)
@@ -346,9 +346,9 @@
      if ( allocated(dmft_h) ) deallocate(dmft_h)
 
      return
-  end subroutine df_deallocate_memory_dmft
+  end subroutine dt_deallocate_memory_dmft
 
-  subroutine df_deallocate_memory_dual()
+  subroutine dt_deallocate_memory_dual()
      implicit none
 
      if ( allocated(dual_g) ) deallocate(dual_g)
@@ -356,24 +356,24 @@
      if ( allocated(dual_b) ) deallocate(dual_b)
 
      return
-  end subroutine df_deallocate_memory_dual
+  end subroutine dt_deallocate_memory_dual
 
-  subroutine df_deallocate_memory_latt()
+  subroutine dt_deallocate_memory_latt()
      implicit none
 
      if ( allocated(latt_g) ) deallocate(latt_g)
      if ( allocated(latt_s) ) deallocate(latt_s)
 
      return
-  end subroutine df_deallocate_memory_latt
+  end subroutine dt_deallocate_memory_latt
 
-  subroutine df_deallocate_memory_vert()
+  subroutine dt_deallocate_memory_vert()
      implicit none
 
      if ( allocated(vert_d) ) deallocate(vert_d)
      if ( allocated(vert_m) ) deallocate(vert_m)
 
      return
-  end subroutine df_deallocate_memory_vert
+  end subroutine dt_deallocate_memory_vert
 
   end module context
