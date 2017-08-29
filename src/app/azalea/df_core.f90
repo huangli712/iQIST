81e2ff6f799737402fa2fb217ba05bec754819f2
Li Huang <lihuang.dmft@gmail.com>
2017-08-29 08:39:15 +0800

    remove df_susc.f90, its context is copied into df_core.f90

--- src/app/azalea/df_core.f90
+++ src/app/azalea/df_core.f90
@@ -136,3 +136,173 @@
 
      return
   end subroutine cat_dual_shift
+
+  subroutine df_static_bubble(bubble, w)
+     use constants
+
+     use df_control
+     use df_context
+
+     implicit none
+
+! external arguments
+     real(dp), intent(in) :: w
+     complex(dp), intent(out) :: bubble(nkpts,nffrq,norbs)
+
+! local variables
+     integer :: i
+     integer :: j
+
+     complex(dp) :: gk(nkpts)
+     complex(dp) :: gr(nkpts)
+
+     do i=1,norbs
+         do j=1,nffrq
+             gk = dual_g(:,j,i)
+             gr = czero
+             call df_fft2d(+1, nkp_x, nkp_y, gk, gr) ! gk -> gr
+             gr = gr * gr
+             call df_fft2d(-1, nkp_x, nkp_y, gr, gk) ! gr -> gk
+             bubble(:,j,i) = -gk
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+     bubble = bubble / real(nkpts * nkpts * beta)
+
+     return
+  end subroutine df_static_bubble
+
+  subroutine df_bubble(bubble, w)
+     use constants
+
+     use df_control
+     use df_context
+
+     implicit none
+
+! external arguments
+     real(dp), intent(in) :: w
+     complex(dp), intent(out) :: bubble(nkpts,nffrq,norbs)
+
+! local variables
+     integer :: i
+     integer :: j
+     integer :: k
+
+     real(dp) :: fw
+
+     complex(dp) :: gk(nkpts)
+     complex(dp) :: gr(nkpts), gr1(nkpts), gr2(nkpts)
+     complex(dp) :: gs(nkpts,nffrq,norbs)
+
+     do i=1,norbs
+         do j=1,nffrq
+             fw = fmesh(j) + w
+             k = floor( (fw * beta / pi + nffrq + one) / two + 0.5 )
+             if ( k >= 1 .and. k <= nffrq ) then
+                 gs(:,j,i) = dual_g(:,k,i)
+             else
+                 gs(:,j,i) = czero
+             endif
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+
+     do i=1,norbs
+         do j=1,nffrq
+             gk = dual_g(:,j,i)
+             gr1 = czero
+             call df_fft2d(+1, nkp_x, nkp_y, gk, gr1) ! gk -> gr
+
+             gk = gs(:,j,i)
+             gr2 = czero
+             call df_fft2d(+1, nkp_x, nkp_y, gk, gr2) ! gk -> gr
+
+             gr = gr1 * gr2
+             call df_fft2d(-1, nkp_x, nkp_y, gr, gk) ! gr -> gk
+             bubble(:,j,i) = -gk
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+     bubble = bubble / real(nkpts * nkpts * beta)
+
+     return
+  end subroutine df_bubble
+
+  subroutine df_bse_solver(bubbleM, vertexM, gammaM)
+     use constants
+
+     use df_control
+     use df_context
+
+     implicit none
+
+! external arguments
+     complex(dp), intent(in) :: bubbleM(nffrq,nffrq)
+     complex(dp), intent(in) :: vertexM(nffrq,nffrq)
+     complex(dp), intent(out) :: gammaM(nffrq,nffrq)
+
+! local variables
+     complex(dp) :: Imat(nffrq,nffrq)
+     complex(dp) :: v4chi(nffrq,nffrq)
+     complex(dp) :: zdet
+
+     call s_identity_z(nffrq, Imat)
+     v4chi = Imat - matmul(vertexM,bubbleM)
+     gammaM = v4chi
+     call s_det_z(nffrq, v4chi, zdet)
+     call s_inv_z(nffrq, gammaM)
+     gammaM = matmul(gammaM, vertexM) 
+
+     !print *, zdet
+     return
+  end subroutine df_bse_solver
+
+  subroutine df_bse_solver_iter(niter, mix, bubbleM, vertexM, gammaM)
+     use constants
+
+     use df_control
+     use df_context
+
+     implicit none
+
+! external arguments
+     integer, intent(in) :: niter
+     real(dp), intent(in) :: mix
+
+     complex(dp), intent(in) :: bubbleM(nffrq,nffrq)
+     complex(dp), intent(in) :: vertexM(nffrq,nffrq)
+     complex(dp), intent(out) :: gammaM(nffrq,nffrq)
+
+! local variables
+     integer :: it
+     complex(dp) :: V4old(nffrq,nffrq)
+     complex(dp) :: V4chi(nffrq,nffrq)
+     complex(dp) :: diff
+
+     gammaM = vertexM
+     V4old = vertexM
+     V4chi = matmul(vertexM, bubbleM)
+
+     do it=1,niter
+         if ( it == niter ) then
+             gammaM = ( vertexM + matmul(V4chi,V4old) ) * mix + (1.0 - mix) * V4old
+         else
+             gammaM = ( matmul(V4chi,V4old) ) * mix + (1.0 - mix) * V4old
+         endif
+         diff = abs(sum(gammaM - V4old)) / real(nffrq * nffrq)
+         V4old = gammaM
+     enddo
+
+     return
+  end subroutine df_bse_solver_iter
+! calculate resulting observables
+
+  subroutine df_spin_susc()
+     implicit none
+
+     return
+  end subroutine df_spin_susc
+
+  subroutine df_char_susc()
+     implicit none
+
+     return
+  end subroutine df_char_susc
