3b26f0cccb19513460b6bbd92b7eacd7ccbb5a22
Li Huang <lihuang.dmft@gmail.com>
2017-09-02 01:34:09 +0800

    in debug mode

--- src/app/azalea/dt_stream.f90
+++ src/app/azalea/dt_stream.f90
@@ -18,9 +18,14 @@
   end subroutine dt_setup_param
 
   subroutine dt_setup_model()
+     use context
+
      implicit none
 
      call dt_mesh_init()
+
+     call dt_dump_grnd(fmesh,dual_g)
+     STOP
      call dt_dmft_init()
      call dt_latt_init()
      call dt_dual_init()
