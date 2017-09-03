047901e72a87767dbafdfc7b1c7b613dffb7a2ed
Li Huang <lihuang.dmft@gmail.com>
2017-09-03 18:11:26 +0800

    try to add mpi support to dt_setup_vert_

--- src/app/azalea/dt_stream.f90
+++ src/app/azalea/dt_stream.f90
@@ -458,22 +458,22 @@
          exists = .false.
 
 ! inquire about file's existence
-         inquire (file = 'dt.dmft_g.in', exist = exists)
+         inquire (file = 'dt.vert_d.in', exist = exists)
 
-! find input file: dt.dmft_g.in, read it
+! find input file: dt.vert_d.in, read it
          if ( exists .eqv. .true. ) then
 
-     open(mytmp, file = 'dt.vert_d.in', form = 'formatted', status = 'unknown')
-     do i=1,nbfrq
-         do if1=1,nffrq
-             do if2=1,nffrq
-                 read(mytmp,*) r1, r2, c1, c2, d1, d2, v1, v2
-                 vert_d(if2,if1,i) = dcmplx(v1, v2)
-             enddo ! over if2={1,nffrq} loop
-             read(mytmp,*) ! skip one line
-         enddo ! over if1={1,nffrq} loop
-     enddo ! over i={1,nbfrq} loop
-     close(mytmp)
+             open(mytmp, file = 'dt.vert_d.in', form = 'formatted', status = 'unknown')
+             do i=1,nbfrq
+                 do if1=1,nffrq
+                     do if2=1,nffrq
+                         read(mytmp,*) r1, r2, c1, c2, d1, d2, v1, v2
+                         vert_d(if2,if1,i) = dcmplx(v1, v2)
+                     enddo ! over if2={1,nffrq} loop
+                     read(mytmp,*) ! skip one line
+                 enddo ! over if1={1,nffrq} loop
+             enddo ! over i={1,nbfrq} loop
+             close(mytmp)
 
          endif ! back if ( exists .eqv. .true. ) block
      endif ! back if ( myid == master ) block
