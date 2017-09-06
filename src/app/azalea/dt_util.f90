bbb271edf876372e0c3d594d59ad2716c0cdfabf
Li Huang <lihuang.dmft@gmail.com>
2017-09-06 10:16:12 +0800

    add cat_dia_1d

--- src/app/azalea/dt_util.f90
+++ src/app/azalea/dt_util.f90
@@ -281,6 +281,64 @@
 !!========================================================================
 
 !!
+!! @cat_dia_1d
+!!
+!! calculate the two-particle bubble diagram, 1d version
+!!
+  subroutine cat_dia_1d(gin, ginp, chiq)
+     use constants, only : dp
+     use constants, only : czero
+
+     use control, only : norbs
+     use control, only : nffrq
+     use control, only : nkpts, nkp_x, nkp_y
+     use control, only : beta
+
+     implicit none
+
+! external arguments
+! G(\nu, K)
+     complex(dp), intent(in)  :: gin(nffrq,norbs,nkpts)
+
+! G(\nu + \omega, K + Q)
+     complex(dp), intent(in)  :: ginp(nffrq,norbs,nkpts)
+
+! two-particle bubble diagram
+     complex(dp), intent(out) :: chiq(nffrq,norbs,nkpts)
+
+! local variables
+! loop index
+     integer :: i
+     integer :: j
+
+! dummy complex(dp) variables
+     complex(dp) :: gk(nkpts)
+     complex(dp) :: gr(nkpts)
+     complex(dp) :: g1(nkpts)
+     complex(dp) :: g2(nkpts)
+
+     do i=1,norbs
+         do j=1,nffrq
+             gk = gin(j,i,:)
+             g1 = czero
+             call cat_fft_2d(+1, nkp_x, nkp_y, gk, g1) ! gk -> gr
+
+             gk = ginp(j,i,:)
+             g2 = czero
+             call cat_fft_2d(+1, nkp_x, nkp_y, gk, g2) ! gk -> gr
+
+             gr = g1 * g2
+             gk = czero
+             call cat_fft_2d(-1, nkp_x, nkp_y, gr, gk) ! gr -> gk
+             chiq(j,i,:) = -gk
+         enddo ! over j={1,nffrq} loop
+     enddo ! over i={1,norbs} loop
+     chiq = chiq / real(nkpts * nkpts * beta)
+
+     return
+  end subroutine cat_dia_1d
+
+!!
 !! @cat_dia_2d
 !!
 !! calculate the two-particle bubble diagram, 2d version
