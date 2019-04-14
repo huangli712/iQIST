22175451baf20b12f34e01e297854fdc770b55d5
Li Huang <lihuang.dmft@gmail.com>
2019-04-15 00:50:32 +0800

    fix compatible problem with gfortran

--- src/ct_hyb1/ctqmc_print.f90
+++ src/ct_hyb1/ctqmc_print.f90
@@ -206,7 +206,7 @@
      integer :: i
 
 ! predefined strings for control parameters
-     character (len = 4) :: scf(2) = ['nscf', 'scf']
+     character (len = 4) :: scf(2) = ['nscf', 'scf ']
      character (len = 7) :: scr(4) = ['static', 'dyn_ppm', 'dyn_om', 'dyn_rm']
      character (len = 3) :: bnd(2) = ['no', 'yes']
      character (len = 3) :: spn(2) = ['no', 'yes']
