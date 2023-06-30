c1166e11eaaa3681e48bf0645be7e23d6f82c265
Li Huang <lihuang.dmft@gmail.com>
2023-07-01 00:20:45 +0800

    improve code style

--- src/ct_hyb1/ctqmc_print.f90
+++ src/ct_hyb1/ctqmc_print.f90
@@ -98,17 +98,19 @@
 
      implicit none
 
-! local variables
-! string for current date and time
+!! local variables
+     ! string for current date and time
      character (len = 20) :: date_time_string
 
-! used to record the time usage information
+     ! used to record the time usage information
      real(dp) :: tot_time
 
-! obtain time usage information
+!! [body
+
+     ! obtain time usage information
      call cpu_time(tot_time)
 
-! obtain current date and time
+     ! obtain current date and time
      call s_time_builder(date_time_string)
 
      write(mystd,'(2X,a,f10.2,a)') cname//' >>> total time spent:', tot_time, 's'
@@ -116,6 +118,8 @@
 
      write(mystd,'(2X,a)') cname//' >>> I am tired and want to go to bed. Bye!'
      write(mystd,'(2X,a)') cname//' >>> happy ending at '//date_time_string
+
+!! body]
 
      return
   end subroutine ctqmc_print_footer
