e1f623a29dedca08212dacfbdeacefca38786c1d
Li Huang <lihuang.dmft@gmail.com>
2023-07-01 00:46:28 +0800

    improve code style

--- src/ct_hyb1/ctqmc_update.f90
+++ src/ct_hyb1/ctqmc_update.f90
@@ -1,19 +1,19 @@
 !!!-----------------------------------------------------------------------
-!!! project : narcissus
+!!! project : iqist @ narcissus
 !!! program : ctqmc_try_warming
 !!!           ctqmc_try_walking
-!!!           ctqmc_try_warning <<<---
+!!!           ctqmc_try_warning
 !!!           ctqmc_insert_kink
 !!!           ctqmc_remove_kink
 !!!           ctqmc_lshift_kink
 !!!           ctqmc_rshift_kink
 !!!           ctqmc_reflip_kink
-!!!           ctqmc_reload_kink <<<---
+!!!           ctqmc_reload_kink
 !!! source  : ctqmc_update.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 09/16/2009 by li huang (created)
-!!!           08/14/2017 by li huang (last modified)
+!!!           07/01/2023 by li huang (last modified)
 !!! purpose : basic update actions for the hybridization expansion version
 !!!           continuous time quantum Monte Carlo (CTQMC) quantum impurity
 !!!           solver. they are called by ctqmc_impurity_solver().
