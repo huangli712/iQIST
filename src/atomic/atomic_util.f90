3552a583ffabad16745ce63a9fa210cda4ec4a49
Li Huang <lihuang.dmft@gmail.com>
2024-01-08 10:46:39 +0800

    fix atomic_make_hund()

--- src/atomic/atomic_util.f90
+++ src/atomic/atomic_util.f90
@@ -407,23 +407,28 @@
 !!>>> determine Coulomb interaction matrix                             <<<
 !!========================================================================
 
-!!>>> atomic_make_hund: make the Hund's rule coupling matrix
+!!
+!! @sub atomic_make_hund
+!!
+!! make the Hund's rule coupling matrix
+!!
   subroutine atomic_make_hund(hund)
-     use constants, only : dp, zero
+     use constants, only : dp
+     use constants, only : zero
 
      use control, only : icu
      use control, only : nband
      use control, only : Jz, Js, Jp
 
-! external arguments
-! Hund's rule coupling matrix
+!! external arguments
+     ! Hund's rule coupling matrix
      real(dp), intent(out) :: hund(nband,nband,3)
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer  :: i
 
-! dummy variables
+     ! dummy variables
      real(dp) :: ff2(3)
      real(dp) :: ff4(3)
      real(dp) :: jj1(3)
@@ -432,15 +437,17 @@
      real(dp) :: jj4(3)
      real(dp) :: jzsp(3)
 
-! initialize hund to be zero
+!! [body
+
+     ! initialize hund to be zero
      hund = zero
 
-! set jzsp
+     ! set jzsp
      jzsp(1) = Jz
      jzsp(2) = Js
      jzsp(3) = Jp
 
-! for isotropic Hund's rule coupling
+     ! for isotropic Hund's rule coupling
      if ( icu == 1 ) then
          hund(:,:,1) = Jz
          hund(:,:,2) = Js
@@ -450,15 +457,32 @@
          enddo ! over i={1,nband} loop
      endif ! back if ( icu == 1 ) block
 
-! for anisotropic Hund's rule coupling
+     ! for anisotropic Hund's rule coupling
      if ( icu == 3 ) then
          if ( nband == 5 ) then
-! J(dxy,dxz) = J(dxy,dyz) = J(dxz,dyz) = J(dxz,dx2) = J(dyz,dx2) = 3/49 * F^2 + 20/441 * F^4
-! J(dxy,dz2) = J(dx2,dz2) = 4/49 * F^2 + 15/441 * F^4
-! J(dxz,dz2) = J(dyz,dz2) = 1/49 * F^2 + 30/441 * F^4
-! J(dxy,dx2) = 35/441 * F^4
-! the averaged Hund's rule coupling is J_{ave} = 5/98 * (F^2 + F^4)
-! and F^4 = 0.625 * F^2.
+             !
+             ! J(dxy,dxz) = J(dxy,dyz)
+             !            = J(dxz,dyz)
+             !            = J(dxz,dx2)
+             !            = J(dyz,dx2)
+             !            = 3/49 * F^2 + 20/441 * F^4
+             !
+             ! J(dxy,dz2) = J(dx2,dz2)
+             !            = 4/49 * F^2 + 15/441 * F^4
+             !
+             ! J(dxz,dz2) = J(dyz,dz2)
+             !            = 1/49 * F^2 + 30/441 * F^4
+             !
+             ! J(dxy,dx2) = 35/441 * F^4
+             !
+             ! the averaged Hund's rule coupling is
+             !
+             !     J_{ave} = 5/98 * (F^2 + F^4)
+             !
+             ! and
+             !
+             !     F^4 = 0.625 * F^2.
+             !
              ff2 = (98.0 * jzsp) / (1.625 * 5.0)
              ff4 = 0.625 * ff2
              jj1 = 3.0 / 49.0 * ff2 + 20.0 / 441.0 * ff4
@@ -466,7 +490,9 @@
              jj3 = 1.0 / 49.0 * ff2 + 30.0 / 441.0 * ff4
              jj4 = 35.0 / 441.0 * ff4
 
-! orbital order is: (1) dz2, (2) dxz, (3) dyz, (4) dx2, (5) dxy
+             ! orbital order is:
+             !     (1) dz2, (2) dxz,
+             !     (3) dyz, (4) dx2, (5) dxy
              hund(2,3,:) = jj1;   hund(3,2,:) = jj1
              hund(2,5,:) = jj1;   hund(5,2,:) = jj1
              hund(3,5,:) = jj1;   hund(5,3,:) = jj1
@@ -485,6 +511,9 @@
          endif ! back if ( nband == 5 ) block
      endif ! back if ( icu == 3 ) block
 
+!! body]
+
+     return
   end subroutine atomic_make_hund
 
 !!>>> atomic_make_umatK: make Coulomb interaction U according to Kanamori
