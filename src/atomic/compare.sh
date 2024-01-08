b80fd396895aa7e30405e57a81ff3834a958b1f9
Li Huang <lihuang.dmft@gmail.com>
2024-01-08 22:37:22 +0800

    remove unused codes

--- src/atomic/compare.sh
+++
@@ -1,7 +0,0 @@
-diff atom.eigvec.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.cix ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.eigval.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.tmat.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.umat.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.emat.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
-diff atom.fock.dat ~/Working/working/saving/iQIST-0.6.7/working/ctqmc/validation/3a/
