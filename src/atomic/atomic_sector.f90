c7e62a091fba67226ae16cdec210f02f3542e7f5
Li Huang <lihuang.dmft@gmail.com>
2024-05-17 10:53:06 +0800

    improve the code style

--- src/atomic/atomic_sector.f90
+++ src/atomic/atomic_sector.f90
@@ -597,34 +597,52 @@
          ! loop over the Fock states in the isec-th subspace
          do jbas=1,sectors(isec)%ndim
              ! get Fock state |jold>
-             jold = dec_basis(sectors(isec)%basis(jbas))
+             jold = dec_basis( sectors(isec)%basis(jbas) )
 
+             ! simulate f^+ |jold> = isgn |jnew> and
+             !          f   |jold> = isgn |jnew>
+             ! notice that the new state is |jnew>
+             !
              ! apply creation fermion operator
              if ( ityp == 1 .and. ( btest(jold, iorb-1) .eqv. .false. ) ) then
                  call atomic_make_cdagger(iorb, jold, jnew, isgn)
+             !
              ! apply annihilation fermion operator
              else if ( ityp == 0 .and. ( btest(jold, iorb-1) .eqv. .true. ) ) then
                  call atomic_make_c(iorb, jold, jnew, isgn)
+             !
              else
                  CYCLE
-             endif ! back if ( ityp == 1 .and. ( btest(jold, iorb-1) .eqv. .false. ) ) block
+             !
+             endif ! back if block
 
-             ! loop over the basis for the jsec-th sector
+             ! loop over the Fock states in the jsec-th subspace
              do ibas=1,sectors(jsec)%ndim
+                 ! well, the matrix element exists
                  if ( sectors(jsec)%basis(ibas) == ind_basis(jnew) ) then
-                     ! build matrix element for F-matrix
                      sectors(isec)%fmat(iorb,ityp)%val(ibas,jbas) = dble(isgn)
                      EXIT
-                 endif ! back if ( sectors(jsec)%basis(ibas) == ind_basis(jnew) ) block
+                 endif ! back if block
              enddo ! over ibas={1,sectors(jsec)%ndim} loop
          enddo ! over jbas={1,sectors(isec)%ndim} loop
 
-         ! roate fmat to atomic eigenstates basis
+         ! (D) rotate fmat from Fock basis to atomic eigenbasis
          call atomic_tran_fmat(sectors(jsec)%ndim, &
                                sectors(isec)%ndim, &
                                sectors(jsec)%evec, &
                                sectors(isec)%fmat(iorb,ityp)%val, &
                                sectors(isec)%evec)
+
+         !
+         if (ityp == 1) then
+             write(mystd,'(4X,a,i2,a)', advance = 'no') 'f^+(alpha = ', iorb, ')'
+             write(mystd,'(2X,a)', advance = 'no') 'size: '
+             write(mystd,'(i4,a,i4)') sectors(jsec)%ndim, ' X ', sectors(isec)%ndim
+         else
+             write(mystd,'(4X,a,i2,a)', advance = 'no') 'f  (alpha = ', iorb, ')'
+             write(mystd,'(2X,a)', advance = 'no') 'size: '
+             write(mystd,'(i4,a,i4)') sectors(jsec)%ndim, ' X ', sectors(isec)%ndim
+         endif
 
              enddo ! over ityp={0,1} loop
          enddo ! over iorb={1,norbs} loop
