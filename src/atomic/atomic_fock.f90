6e99496130e6a917d7d3d2b973de0e879e13ec57
Li Huang <lihuang.dmft@gmail.com>
2024-05-21 16:30:50 +0800

    fix atomic_fock.f90

--- src/atomic/atomic_fock.f90
+++ src/atomic/atomic_fock.f90
@@ -155,7 +155,7 @@
      use constants, only: zero, half
      use constants, only : mystd
 
-     use control, only : norbs, ncfgs
+     use control, only : nband, norbs, ncfgs
 
      use m_fock, only : bin_basis
      use m_fock, only : evec
@@ -177,11 +177,12 @@
      do ibas=1,ncfgs
          do iorb=1,norbs
              if ( bin_basis(iorb,ibas) == 1 ) then
-                 if ( mod(iorb,2) /= 0 ) then ! spin up
+                 ! the spin order is always: up up up down down down
+                 if ( iorb <= nband ) then ! spin up
                      spin(ibas,ibas) = spin(ibas,ibas) + half
-                 else                         ! spin down
+                 else                      ! spin down
                      spin(ibas,ibas) = spin(ibas,ibas) - half
-                 endif ! back if ( mod(iorb,2) /= 0 ) block
+                 endif ! back if ( iorb <= nband ) block
              endif ! back if ( bin_basis(iorb,ibas ) == 1) block
          enddo ! over iorb={1,norbs} loop
          !
