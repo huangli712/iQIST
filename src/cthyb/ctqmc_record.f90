11d8d4199151f33006a5eccdedd8a48bf1b9626b
Li Huang <lihuang.dmft@gmail.com>
2024-06-20 21:29:45 +0800

    fix cthyb/ctqmc_record.f90

--- src/cthyb/ctqmc_record.f90
+++ src/cthyb/ctqmc_record.f90
@@ -388,23 +388,23 @@
                  endif STD_BLOCK ! back if ( isort == 1 ) block
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-!-------------------------------------------------------------------------
-! using legendre orthogonal polynomial representation
-!-------------------------------------------------------------------------
+                 !--------------------------------------------------------
+                 ! using legendre orthogonal polynomial representation
+                 !--------------------------------------------------------
                  LEG_BLOCK: if ( isort == 2 ) then
 
-! convert dtau in [0,\beta] to daux in [0,2]
+                     ! convert dtau in [0,\beta] to daux in [0,2]
                      daux = two * dtau / beta
 
-! determine index for legendre orthogonal polynomial interval
+                     ! determine index for legendre orthogonal polynomial interval
                      curr = nint( daux * step ) + 1
 
-! special tricks for the first point and the last point
+                     ! special tricks for the first point and the last point
                      if ( curr == 1 .or. curr == legrd ) then
                          maux = two * maux
                      endif ! back if ( curr == 1 .or. curr == legrd ) block
 
-! record gtau, we normalize gtau in ctqmc_tran_gtau() subroutine
+                     ! record gtau, we normalize gtau in ctqmc_tran_gtau() subroutine
                      LEG_CYCLE: do fleg=1,lemax
                          dtau = sqrt(two * fleg - 1) * rep_l(curr,fleg)
                          gtau(fleg, flvr, flvr) = gtau(fleg, flvr, flvr) - maux * dtau
