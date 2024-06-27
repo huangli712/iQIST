e5ee39c87dc51d4adf3abe53595329b0d04cfdc3
Li Huang <lihuang.dmft@gmail.com>
2024-06-27 15:44:40 +0800

    fix cthyb/ctqmc_stream.f90

--- src/cthyb/ctqmc_stream.f90
+++ src/cthyb/ctqmc_stream.f90
@@ -49,20 +49,21 @@
      ! used to check whether the input file (solver.ctqmc.in) exists
      logical :: exists
 
-! setup general control flags
-!-------------------------------------------------------------------------
+!! [body
+
+     ! setup general control flags
+     !--------------------------------------------------------------------
      isscf  = 1         ! self-consistent scheme
      isbnd  = 1         ! symmetry (band part)
      isspn  = 1         ! symmetry (spin part)
      isopt  = 1         ! fast trace evaluation
      iscut  = 1         ! dynamic truncation
-     !isbin  = 1         ! data binning
      iswor  = 1         ! worm algorithm
      isort  = 1         ! advanced basis
      isobs  = 1         ! various physical observables
      issus  = 1         ! charge/spin susceptibility
-     isvrt  = 1         ! two-particle green's function
-!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     isvrt  = 1         ! two-particle green's functions
+     !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 ! setup common variables for dynamical mean field theory
 !-------------------------------------------------------------------------
