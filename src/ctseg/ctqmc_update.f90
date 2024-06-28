e1b811af37ef1f8fd08714e0dc8fe60f26cfd5af
Li Huang <lihuang.dmft@gmail.com>
2024-06-28 11:11:30 +0800

    fix ctseg/ctqmc_update.f90

--- src/ctseg/ctqmc_update.f90
+++ src/ctseg/ctqmc_update.f90
@@ -247,15 +247,15 @@
              if ( stts(i) == 0 ) then
                  if ( rank(i) /= 0 ) cflag = 99
              endif ! back if ( stts(i) == 0 ) block
-
+             !
              if ( stts(i) == 1 ) then
                  if ( rank(i) == 0 ) cflag = 99
              endif ! back if ( stts(i) == 1 ) block
-
+             !
              if ( stts(i) == 2 ) then
                  if ( rank(i) == 0 ) cflag = 99
              endif ! back if ( stts(i) == 2 ) block
-
+             !
              if ( stts(i) == 3 ) then
                  if ( rank(i) /= 0 ) cflag = 99
              endif ! back if ( stts(i) == 3 ) block
@@ -266,10 +266,11 @@
              do j=1,rank(i)-1
                  if ( time_s( index_s(j, i), i ) > time_s( index_s(j+1, i), i ) ) then
                      cflag = 99
-                 endif ! back if ( time_s( index_s(j, i), i ) > time_s( index_s(j+1, i), i ) ) block
+                 endif ! back if block
+                 !
                  if ( time_e( index_e(j, i), i ) > time_e( index_e(j+1, i), i ) ) then
                      cflag = 99
-                 endif ! back if ( time_e( index_e(j, i), i ) > time_e( index_e(j+1, i), i ) ) block
+                 endif ! back if block
              enddo ! over j={1,rank(i)-1} loop
          enddo ! over i={1,norbs} loop
 
@@ -278,13 +279,13 @@
              if ( stts(i) == 1 ) then
                  if ( time_s( index_s(1, i), i ) > time_e( index_e(1, i), i ) ) then
                      cflag = 99
-                 endif ! back if ( time_s( index_s(1, i), i ) > time_e( index_e(1, i), i ) ) block
+                 endif ! back if block
              endif ! back if ( stts(i) == 1 ) block
-
+             !
              if ( stts(i) == 2 ) then
                  if ( time_s( index_s(1, i), i ) < time_e( index_e(1, i), i ) ) then
                      cflag = 99
-                 endif ! back if ( time_s( index_s(1, i), i ) < time_e( index_e(1, i), i ) ) block
+                 endif ! back if block
              endif ! back if ( stts(i) == 2 ) block
          enddo ! over i={1,norbs} loop
 
